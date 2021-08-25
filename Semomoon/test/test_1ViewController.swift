3e6bb3586699425891728a12e6b768fa21fc89c3
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-08-25 21:53:52 +0900

    8/25 [FIX] 세가지 테스트형식창 디자인 수정

--- Semomoon/test/test_1ViewController.swift
+++ Semomoon/test/test_1ViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class test_1ViewController: UIViewController {
 
+    @IBOutlet weak var solvInputFrame: UIView!
+    @IBOutlet weak var sol_1: UIButton!
+    @IBOutlet weak var sol_2: UIButton!
+    @IBOutlet weak var sol_3: UIButton!
+    @IBOutlet weak var sol_4: UIButton!
+    @IBOutlet weak var sol_5: UIButton!
+    
+    @IBOutlet var star: UIButton!
+    @IBOutlet var bookmark: UIButton!
+    var buttons: [UIButton] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttons = [sol_1, sol_2, sol_3, sol_4, sol_5]
 
+        setRadius()
+        setBorderWidth()
+        setBorderColor()
+        setShadowFrame()
         // Do any additional setup after loading the view.
     }
     
@@ -19,6 +35,67 @@ class test_1ViewController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         print("1 : disappear")
+    }
+    
+    // 객관식 1~5 클릭 부분
+    @IBAction func sol_click(_ sender: UIButton) {
+        let num: Int = sender.tag
+        for bt in buttons {
+            if(bt.tag == num) {
+                bt.backgroundColor = UIColor(named: "mint")
+                bt.setTitleColor(UIColor.white, for: .normal)
+            } else {
+                bt.backgroundColor = UIColor.white
+                bt.setTitleColor(UIColor(named: "mint"), for: .normal)
+            }
+        }
+    }
+    
+}
+
+
+extension test_1ViewController {
+    // 뷰의 라운드 설정 부분
+    func setRadius() {
+        solvInputFrame.layer.cornerRadius = 20
+        for bt in buttons {
+            bt.layer.cornerRadius = 20
+        }
+        
+        star.layer.cornerRadius = 17.5
+        star.clipsToBounds = true
+        
+        bookmark.layer.cornerRadius = 17.5
+        bookmark.clipsToBounds = true
+    }
+    
+    // 객관식 1~5의 두께 설정 부분
+    func setBorderWidth() {
+        for bt in buttons {
+            bt.layer.borderWidth = 0.5
+        }
+    }
+    
+    // 객관식 1~5의 두께 색설정 부분
+    func setBorderColor() {
+        for bt in buttons {
+            bt.layer.borderColor = UIColor.black.cgColor
+        }
+    }
+    
+    // 객관식 입력창의 그림자 설정 부분
+    func setShadowFrame() {
+        solvInputFrame.layer.shadowColor = UIColor.lightGray.cgColor
+        solvInputFrame.layer.shadowOpacity = 0.3
+        solvInputFrame.layer.shadowOffset = CGSize(width: 3, height: 3)
+        solvInputFrame.layer.shadowRadius = 5
+        solvInputFrame.layer.masksToBounds = false
+        
+        star.layer.shadowColor = UIColor.lightGray.cgColor
+        star.layer.shadowOpacity = 0.3
+        star.layer.shadowOffset = CGSize(width: 2, height: 2)
+        star.layer.shadowRadius = 3
+        star.layer.masksToBounds = false
     }
     
 }
--- Semomoon/test/test_2ViewController.swift
+++ Semomoon/test/test_2ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class test_2ViewController: UIViewController {
 
+    @IBOutlet weak var solvInputFrame: UIView!
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBOutlet var star: UIButton!
+    @IBOutlet var bookmark: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setRadius()
+        setShadowFrame()
         // Do any additional setup after loading the view.
     }
     
@@ -19,5 +28,34 @@ class test_2ViewController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         print("2 : disappear")
+    }
+}
+
+
+extension test_2ViewController {
+    // 뷰의 라운드 설정 부분
+    func setRadius() {
+        solvInputFrame.layer.cornerRadius = 20
+        
+        star.layer.cornerRadius = 17.5
+        star.clipsToBounds = true
+        
+        bookmark.layer.cornerRadius = 17.5
+        bookmark.clipsToBounds = true
+    }
+    
+    // 객관식 입력창의 그림자 설정 부분
+    func setShadowFrame() {
+        solvInputFrame.layer.shadowColor = UIColor.lightGray.cgColor
+        solvInputFrame.layer.shadowOpacity = 0.3
+        solvInputFrame.layer.shadowOffset = CGSize(width: 3, height: 3)
+        solvInputFrame.layer.shadowRadius = 5
+        solvInputFrame.layer.masksToBounds = false
+        
+        star.layer.shadowColor = UIColor.lightGray.cgColor
+        star.layer.shadowOpacity = 0.3
+        star.layer.shadowOffset = CGSize(width: 2, height: 2)
+        star.layer.shadowRadius = 3
+        star.layer.masksToBounds = false
     }
 }
