804e58e222554f3934e8631a5d8c0e380aea3968
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-09-04 00:22:20 +0900

    9/4 [fix] PKCanvas 넣기 전 ImageView를 통한 테스트버전

--- Semomoon/test/test_1ViewController.swift
+++ Semomoon/test/test_1ViewController.swift
@@ -8,68 +8,40 @@
 import UIKit
 import PencilKit
 
-class test_1ViewController: UIViewController, CALayerDelegate {
+class test_1ViewController: UIViewController {
 
     @IBOutlet weak var solvInputFrame: UIView!
-    @IBOutlet weak var sol_1: UIButton!
-    @IBOutlet weak var sol_2: UIButton!
-    @IBOutlet weak var sol_3: UIButton!
-    @IBOutlet weak var sol_4: UIButton!
-    @IBOutlet weak var sol_5: UIButton!
-    
+    @IBOutlet var checkNumbers: [UIButton]!
     @IBOutlet var star: UIButton!
     @IBOutlet var bookmark: UIButton!
     
     @IBOutlet weak var scrollView: UIScrollView!
 //    @IBOutlet weak var canvasView: PKCanvasView!
     @IBOutlet weak var underImage: UIImageView!
+    @IBOutlet weak var contentView: UIView!
     
     @IBOutlet weak var imageWidth: NSLayoutConstraint!
     @IBOutlet weak var imageHeight: NSLayoutConstraint!
     
-//    @IBOutlet weak var contentHeight: NSLayoutConstraint!
+    var width: CGFloat!
+    var height: CGFloat!
     
-    var buttons: [UIButton] = []
-    
-    
-    var height: CGFloat = 0
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        buttons = [sol_1, sol_2, sol_3, sol_4, sol_5]
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
         setRadius()
         setBorderWidth()
         setBorderColor()
         setShadowFrame()
         
-//        underImage.layer.delegate = self
-        let image = UIImage(named: "A-1")!
+        guard let image = UIImage(named: "A-1") else { return }
         underImage.image = image
-        height = image.size.height*(500/image.size.width)
-        underImage.frame = CGRect(x: 0, y: 0, width: 500, height: height)
-        imageWidth.constant = 500
-        imageHeight.constant = height
-//
-        addPinch()
         
-        scrollView.delegate = self
-//        scrollView.maximumZoomScale = 2.0
-        scrollView.zoomScale = 1.0
-    }
-    
-    func addPinch() {
-        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(test_1ViewController.didPinch(_:)))
-        self.view.addGestureRecognizer(pinch)
-    }
-    
-    @objc func didPinch(_ gesture: UIPinchGestureRecognizer) {
-        if gesture.state == .changed {
-            imageWidth.constant *= gesture.scale
-            imageHeight.constant *= gesture.scale
-//            contentHeight.constant *= gesture.scale
-            gesture.scale = 1
-        }
+        width = contentView.frame.width - 20
+        height = image.size.height*(width/image.size.width)
+        underImage.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        imageWidth.constant = width
+        imageHeight.constant = height
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -80,7 +52,7 @@ class test_1ViewController: UIViewController, CALayerDelegate {
     // 객관식 1~5 클릭 부분
     @IBAction func sol_click(_ sender: UIButton) {
         let num: Int = sender.tag
-        for bt in buttons {
+        for bt in checkNumbers {
             if(bt.tag == num) {
                 bt.backgroundColor = UIColor(named: "mint")
                 bt.setTitleColor(UIColor.white, for: .normal)
@@ -98,7 +70,7 @@ extension test_1ViewController {
     // 뷰의 라운드 설정 부분
     func setRadius() {
         solvInputFrame.layer.cornerRadius = 20
-        for bt in buttons {
+        for bt in checkNumbers {
             bt.layer.cornerRadius = 20
         }
         
@@ -111,14 +83,14 @@ extension test_1ViewController {
     
     // 객관식 1~5의 두께 설정 부분
     func setBorderWidth() {
-        for bt in buttons {
+        for bt in checkNumbers {
             bt.layer.borderWidth = 0.5
         }
     }
     
     // 객관식 1~5의 두께 색설정 부분
     func setBorderColor() {
-        for bt in buttons {
+        for bt in checkNumbers {
             bt.layer.borderColor = UIColor.black.cgColor
         }
     }
@@ -138,16 +110,4 @@ extension test_1ViewController {
         star.layer.masksToBounds = false
     }
     
-}
-
-
-extension test_1ViewController: UIScrollViewDelegate {
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return underImage
-    }
-
-    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        scrollView.center = view.center
-        underImage.center.x = scrollView.center.x
-    }
 }
