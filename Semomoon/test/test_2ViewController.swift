9cd4db7d799abb5a46a3095170758bc7a30e1489
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-09-04 10:44:47 +0900

    9/4 [feat] 주관식 화면 구성

--- Semomoon/test/test_2ViewController.swift
+++ Semomoon/test/test_2ViewController.swift
@@ -1,36 +1,68 @@
 //
-//  test_2ViewController.swift
-//  test_2ViewController
+//  test_1ViewController.swift
+//  test_1ViewController
 //
 //  Created by qwer on 2021/08/25.
 //
 
 import UIKit
+import PencilKit
 
-class test_2ViewController: UIViewController {
+class test_2ViewController: UIViewController, PKToolPickerObserver {
 
     @IBOutlet weak var solvInputFrame: UIView!
-    @IBOutlet weak var textField: UITextField!
-    
-    
+    @IBOutlet var checkInput: UITextField!
     @IBOutlet var star: UIButton!
     @IBOutlet var bookmark: UIButton!
     
-    @IBOutlet weak var height: NSLayoutConstraint!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var canvasView: PKCanvasView!
+    @IBOutlet weak var imageView: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var canvasHeight: NSLayoutConstraint!
+    @IBOutlet weak var imageHeight: NSLayoutConstraint!
+    
+    var width: CGFloat!
+    var height: CGFloat!
+    
+    lazy var toolPicker: PKToolPicker = {
+        let toolPicker = PKToolPicker()
+        toolPicker.addObserver(self)
+        return toolPicker
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
         setRadius()
         setShadowFrame()
-        // Do any additional setup after loading the view.
+        
+        canvasView.isOpaque = false
+        canvasView.backgroundColor = .clear
+        canvasView.becomeFirstResponder()
+        
+        guard let image = UIImage(named: "A-2") else { return }
+        
+        width = canvasView.frame.width
+        height = image.size.height*(width/image.size.width)
+        
+        imageView.image = image
+        imageView.clipsToBounds = true
+        imageView.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        imageHeight.constant = height
+        canvasView.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        canvasHeight.constant = height
+        
+        canvasView.subviews[0].addSubview(imageView)
+        canvasView.subviews[0].sendSubviewToBack(imageView)
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
     }
-    
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        print("2 : disappear")
+        print("1 : disappear")
     }
+    
 }
 
 
@@ -60,4 +92,5 @@ extension test_2ViewController {
         star.layer.shadowRadius = 3
         star.layer.masksToBounds = false
     }
+    
 }
