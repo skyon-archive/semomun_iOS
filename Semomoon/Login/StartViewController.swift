d54a99b44d626b9d2a1c6988b1ff24c0238c84fb
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-09-19 14:42:28 +0900

    9/19 [feat] appLogo 이미지 추가, 첫화면 제작

---
+++ Semomoon/Login/StartViewController.swift
@@ -0,0 +1,26 @@
+//
+//  StartViewController.swift
+//  StartViewController
+//
+//  Created by qwer on 2021/09/19.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func register(_ sender: Any) {
+    }
+    @IBAction func registerWithApple(_ sender: Any) {
+    }
+    @IBAction func login(_ sender: Any) {
+    }
+    @IBAction func nextView(_ sender: Any) {
+    }
+    
+}
