fff1c0f5a3874b746d8e7e44670c80f78f96433d
Yoonho Shin <yoonhoshin@Yoonhos-Mac-mini.local>
2021-09-22 15:15:50 +0900

    Section_core re push

--- Semomoon/Model/Problem.swift
+++ Semomoon/Model/Problem.swift
@@ -27,7 +27,7 @@ struct Problem: Codable {
 }
 
 // 실제 ipad 에서 지닐 문제 데이터, reference 타입이라 해당 객체를 ViewController 에서 지닌 채로 값을 수정하면 같이 수정되는 식
-class Problem_Real {
+public class Problem_Real: NSObject {
     enum ProblemType {
         case notProblem
         case oneToFive
