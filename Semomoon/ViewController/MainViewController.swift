2ad18fd9bfe4850d5f748d6762ecc5e266802fa5
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-09-25 18:02:21 +0900

    9/25 [feat] sideViewController 보이기 및 선택 구현 완료

--- Semomoon/ViewController/MainViewController.swift
+++ Semomoon/ViewController/MainViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 import CoreData
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, SideMenuViewControllerDelegate {
 
+    @IBOutlet weak var currentMode: UIButton!
     @IBOutlet weak var category: UICollectionView!
     @IBOutlet weak var preview: UICollectionView!
+//    @IBOutlet weak var sideMenuShadowView: UIView!
+    
     //임시적인 데이터
     let categoryButtons: [String] = ["전체", "국어", "수학"]
     var previews: [Preview_Core] = []
@@ -21,6 +24,14 @@ class MainViewController: UIViewController {
     let dumyImage = UIImage(named: "256img_2")!
     var queryDictionary: [String:NSPredicate] = [:]
     var currentFilter: String = "전체"
+    
+    private var sideMenuViewController: SideMenuViewController!
+    private var sideMenuTrailingConstraint: NSLayoutConstraint!
+    private var sideMenuShadowView: UIView!
+    private var revealSideMenuOnTop: Bool = true
+    private var isExpanded: Bool = false
+    private var sideMenuRevealWidth: CGFloat = 260
+    private let paddingForRotation: CGFloat = 150
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,12 +47,94 @@ class MainViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(refreshPreviews(_:)), name: ShowDetailOfWorkbookViewController.refresh, object: nil)
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Shadow Background View
+        self.sideMenuShadowView = UIView(frame: self.view.bounds)
+        self.sideMenuShadowView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.sideMenuShadowView.backgroundColor = .black
+        self.sideMenuShadowView.alpha = 0.0
+        view.insertSubview(self.sideMenuShadowView, at: 4)
+        
+        // MARK:- setting sidebar ViewController
+        self.sideMenuViewController = storyboard?.instantiateViewController(withIdentifier: "SideMenuViewController") as? SideMenuViewController
+        self.sideMenuViewController.defaultHighlightedCell = 0
+        self.sideMenuViewController.delegate = self
+        
+        view.insertSubview(self.sideMenuViewController!.view, at: 5)
+        addChild(self.sideMenuViewController!)
+        self.sideMenuViewController!.didMove(toParent: self)
+        
+        // MARK:- setting sidebar autolayout
+        self.sideMenuViewController.view.translatesAutoresizingMaskIntoConstraints = false
+
+        if self.revealSideMenuOnTop {
+            self.sideMenuTrailingConstraint = self.sideMenuViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -self.sideMenuRevealWidth - self.paddingForRotation)
+            self.sideMenuTrailingConstraint.isActive = true
+        }
+        NSLayoutConstraint.activate([
+            self.sideMenuViewController.view.widthAnchor.constraint(equalToConstant: self.sideMenuRevealWidth),
+            self.sideMenuViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            self.sideMenuViewController.view.topAnchor.constraint(equalTo: view.topAnchor)
+        ])
+    }
+    
     @IBAction func userInfo(_ sender: UIButton) {
         print("userInfo")
     }
     
     @objc func refreshPreviews(_ notification: Notification) {
         fetchPreviews(filter: "전체")
+    }
+    
+    @IBAction func showSidebar(_ sender: Any) {
+        self.sideMenuState(expanded: self.isExpanded ? false : true)
+    }
+    
+    func selectedCell(_ row: Int) {
+        print(sideMenuViewController.testTitles[row])
+        self.currentMode.setTitle(sideMenuViewController.testTitles[row], for: .normal)
+        DispatchQueue.main.async { self.sideMenuState(expanded: false) }
+    }
+}
+// MARK:- sidebar viewController codes
+extension MainViewController {
+    
+    func animateShadow(targetPosition: CGFloat) {
+        UIView.animate(withDuration: 0.5) {
+            // When targetPosition is 0, which means side menu is expanded, the shadow opacity is 0.6
+            self.sideMenuShadowView.alpha = (targetPosition == 0) ? 0.3 : 0.0
+        }
+    }
+    
+    func sideMenuState(expanded: Bool) {
+        if expanded {
+            self.animateSideMenu(targetPosition: self.revealSideMenuOnTop ? 0 : self.sideMenuRevealWidth) { _ in
+                self.isExpanded = true
+            }
+            // Animate Shadow (Fade In)
+            UIView.animate(withDuration: 0.5) { self.sideMenuShadowView.alpha = 0.3 }
+        }
+        else {
+            self.animateSideMenu(targetPosition: self.revealSideMenuOnTop ? (-self.sideMenuRevealWidth - self.paddingForRotation) : 0) { _ in
+                self.isExpanded = false
+            }
+            // Animate Shadow (Fade Out)
+            UIView.animate(withDuration: 0.5) { self.sideMenuShadowView.alpha = 0.0 }
+        }
+    }
+    
+    func animateSideMenu(targetPosition: CGFloat, completion: @escaping (Bool) -> ()) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0, options: .layoutSubviews, animations: {
+            if self.revealSideMenuOnTop {
+                self.sideMenuTrailingConstraint.constant = targetPosition
+                self.view.layoutIfNeeded()
+            }
+            else {
+                self.view.subviews[1].frame.origin.x = targetPosition
+            }
+        }, completion: completion)
     }
 }
 
--- Semomoon/ViewController/SideMenuViewController.swift
+++ Semomoon/ViewController/SideMenuViewController.swift
@@ -14,6 +14,7 @@ protocol SideMenuViewControllerDelegate {
 class SideMenuViewController: UIViewController {
     @IBOutlet weak var sideMenuTableView: UITableView!
     var delegate: SideMenuViewControllerDelegate?
+    var defaultHighlightedCell: Int = 0
     var testTitles: [String] = ["수능 및 모의고사", "LEET", "공인회계사", "공인중개사", "9급 공무원"]
     
     override func viewDidLoad() {
