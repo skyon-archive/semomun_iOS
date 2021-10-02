1a4d7c5dc3428e97d29b4bb187ac3e9a4bbfdad3
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-10-02 16:56:09 +0900

    10/2 [feat] DB 수정

--- Semomoon/Model/Problem.swift
+++ Semomoon/Model/Problem.swift
@@ -20,7 +20,7 @@ struct ProblemOfDB: Codable, CustomStringConvertible {
     let type: Int //0: 채점 불가, 1: 객관식, 2: 주관식
     let answer: String? //문제의 정답
     let content: String //문제 내용 이미지의 숫자값, content.png 식으로 접근되는 식
-    let explanation: Int? //문제의 해설 이미지의 숫자값
+    let explanation: String? //문제의 해설 이미지의 숫자값
     let attempt_total: Int? //문제에 답이 제출된 총 횟수
     let attempt_corrent: Int? //문제에 정답이 제출된 총 횟수
     let rate: Int? //문제의 외부 정답률
