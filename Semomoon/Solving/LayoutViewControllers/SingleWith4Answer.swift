b02a4cb26fd6e0d9355f754cf4aedae4f497eb8d
Yoonho Shin <yoonhoshin@Yoonhos-Mac-mini.local>
2021-10-30 14:07:19 +0900

    [Feat] Singlewith4answer Layout ViewController

--- Semomoon/Solving/LayoutViewControllers/SingleWith4Answer.swift
+++ Semomoon/Solving/LayoutViewControllers/SingleWith4Answer.swift
@@ -6,14 +6,97 @@
 //
 
 import UIKit
+import PencilKit
 
-class SingleWith4Answer: UIViewController {
+class SingleWith4Answer: UIViewController, PKToolPickerObserver, PKCanvasViewDelegate {
     static let identifier = "SingleWith4Answer" // form == 2 && type == 4
-
+    
+    // check numbers needed
+    @IBOutlet var checkNumbers: [UIButton]!
+    @IBOutlet var star: UIButton!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var canvasView: PKCanvasView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var canvasHeight: NSLayoutConstraint!
+    @IBOutlet weak var imageHeight: NSLayoutConstraint!
+    
+    var width: CGFloat!
+    var height: CGFloat!
+    var image: UIImage!
+    var pageData: PageData!
+    
+    lazy var toolPicker: PKToolPicker = {
+        let toolPicker = PKToolPicker()
+        toolPicker.addObserver(self)
+        return toolPicker
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setButtons()
+        
+        canvasView.isOpaque = false
+        canvasView.backgroundColor = .clear
+        canvasView.becomeFirstResponder()
+        
+        canvasView.subviews[0].addSubview(imageView)
+        canvasView.subviews[0].sendSubviewToBack(imageView)
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
+        
+        canvasView.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        width = canvasView.frame.width
+        height = image.size.height*(width/image.size.width)
+        
+        imageView.image = image
+        imageView.clipsToBounds = true
+        imageView.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        imageHeight.constant = height
+        canvasView.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        canvasHeight.constant = height
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("1 : disappear")
+    }
+    
+    @IBAction func sol_click(_ sender: UIButton) {
+        let num: Int = sender.tag
+        for bt in checkNumbers {
+            if(bt.tag == num) {
+                bt.backgroundColor = UIColor(named: "mint")
+                bt.setTitleColor(UIColor.white, for: .normal)
+            } else {
+                bt.backgroundColor = UIColor.white
+                bt.setTitleColor(UIColor(named: "mint"), for: .normal)
+            }
+        }
+    }
+    
 
+}
+
+extension SingleWith4Answer {
+    func setButtons() {
+        for bt in checkNumbers {
+            bt.layer.cornerRadius = 17.5
+        }
+    }
+}
+
+extension SingleWith4Answer {
+    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+        print("update!")
+    }
 }
