75d8f02f3aab2f176a51aa70108918fa606537be
Yoonho Shin <yoonhoshin@Yoonhos-Mac-mini.local>
2021-11-13 13:40:33 +0900

    [Feat] initial work with Apple/Google login

--- Semomoon/Login/StartViewController.swift
+++ Semomoon/Login/StartViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 import AuthenticationServices
+import GoogleSignIn
 
 class StartViewController: UIViewController, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding{
+    
+    let signInConfig = GIDConfiguration.init(clientID: "436503570920-07bqbk38ub6tauc97csf5uo1o2781lm1.apps.googleusercontent.com")
+    
     func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
         return self.view.window!
     }
@@ -18,6 +22,7 @@ class StartViewController: UIViewController, ASAuthorizationControllerDelegate, 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpSignInAppleButton()
+        setUpSignInGoogleButton()
         // Do any additional setup after loading the view.
     }
     
@@ -28,6 +33,7 @@ class StartViewController: UIViewController, ASAuthorizationControllerDelegate, 
     }
     
     @IBAction func login(_ sender: Any) {
+        print("login")
         guard let nextVC = self.storyboard?.instantiateViewController(identifier: "LoginViewController") else { return }
         self.title = ""
         self.navigationController?.pushViewController(nextVC, animated: true)
@@ -41,7 +47,7 @@ extension StartViewController{
     func setUpSignInAppleButton(){
         let authorizationButton = ASAuthorizationAppleIDButton()
         authorizationButton.addTarget(self, action: #selector(appleSignInButtonPress), for: .touchUpInside)
-        authorizationButton.cornerRadius = 10
+        authorizationButton.cornerRadius = 20
         //Add button on some view or stack
         self.signInButtonStack.addArrangedSubview(authorizationButton) // have to divide the stack view of signing in and signing out
     }
@@ -49,18 +55,11 @@ extension StartViewController{
     func setUpSignUpAppleButton(){
         let authorizationButton = ASAuthorizationAppleIDButton()
         authorizationButton.addTarget(self, action:#selector(appleSignUpButtonPress), for: .touchUpInside)
-        authorizationButton.cornerRadius = 10
+        authorizationButton.cornerRadius = 20
         self.signInButtonStack.addArrangedSubview(authorizationButton)
     }
-    
+
     @objc func appleSignInButtonPress() {
-//        let appleIDProvider = ASAuthorizationAppleIDProvider()
-//        let request = appleIDProvider.createRequest()
-//        request.requestedScopes = [.fullName, .email]
-//        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-//        authorizationController.delegate = self
-//        authorizationController.performRequests()
-        
         let appleIDProvider = ASAuthorizationAppleIDProvider()
         let request = appleIDProvider.createRequest()
         request.requestedScopes = [.fullName, .email]
@@ -94,4 +93,29 @@ extension StartViewController{
     }
     
     
+}
+
+extension StartViewController{
+    func setUpSignInGoogleButton(){
+        let googleSignInButton: GIDSignInButton! = GIDSignInButton()
+        googleSignInButton.style = .standard
+        googleSignInButton.layer.cornerRadius = 20
+        googleSignInButton.addTarget(self, action: #selector(googleSignInButtonPress), for: .touchUpInside)
+        self.signInButtonStack.addArrangedSubview(googleSignInButton)
+    }
+    
+    @IBAction func googleSignInButtonPress(sender: Any){
+        GIDSignIn.sharedInstance.signIn(with: signInConfig, presenting: self) { user, error in
+            guard error == nil else { return }
+            guard let user = user else { return }
+            // If sign in succeeded, display the app's main content View.
+            let emailAddress = user.profile?.email
+            
+            let fullName = user.profile?.name
+            let givenName = user.profile?.givenName
+            let familyName = user.profile?.familyName
+            
+            let profilePicUrl = user.profile?.imageURL(withDimension: 320)
+        }
+    }
 }
--- Semomoon/SceneDelegate.swift
+++ Semomoon/SceneDelegate.swift
@@ -17,6 +17,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+//        if let windowScene = scene as? UIWindowScene {
+//            let window = UIWindow(windowScene: windowScene)
+//            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//            window.rootViewController = storyboard.instantiateViewController(withIdentifier: "StartViewController")
+//// Your RootViewController in here
+//            self.window = window
+//            window.makeKeyAndVisible()
+//        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
