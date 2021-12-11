553056fcebab81cd6e5f18b1c7db8adfe377d2f5
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-12-11 12:39:02 +0900

    [Feat] 회원가입용 Category VC, Manager 로직 구현

--- Semomoon/Login/CollecitonViews/CategoryViewController.swift
+++ Semomoon/Login/CollecitonViews/CategoryViewController.swift
@@ -7,18 +7,67 @@
 
 import UIKit
 
+protocol CategorySetable: AnyObject {
+    func didSelectCategory(to: String)
+}
+
 final class CategoryViewController: UIViewController {
     enum Identifier {
         static let controller = "CategoryViewController"
         static let segue = "CategorySegue"
     }
-
+    weak var delegate: CategorySetable?
     @IBOutlet weak var categoryCollectionView: UICollectionView!
+    
+    var manager: CategoryManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.configureManager()
+        self.configureDelegate()
     }
+}
 
+//MARK: - Configure
+extension CategoryViewController {
+    private func configureManager() {
+        self.manager = CategoryManager()
+        manager?.fetch { [weak self] in
+            self?.categoryCollectionView.reloadData()
+        }
+    }
+    
+    private func configureDelegate() {
+        self.categoryCollectionView.delegate = self
+        self.categoryCollectionView.dataSource = self
+    }
+}
+
+//MARK: - CollectionView
+extension CategoryViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.manager?.selected(to: indexPath.item)
+        self.categoryCollectionView.reloadData()
+    }
+}
+
+extension CategoryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.manager?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FavoriteCategoryCell.identifier, for: indexPath) as? FavoriteCategoryCell else { return UICollectionViewCell() }
+        guard let manager = self.manager else { return cell }
+        let title = manager.item(at: indexPath.item)
+        cell.configure(title: title)
+        
+        if let selected = manager.selectedIndex {
+            if indexPath.item == selected {
+                cell.didSelected()
+            }
+        }
+        
+        return cell
+    }
 }
--- Semomoon/Login/CollectionViewManagers/CategoryManager.swift
+++ Semomoon/Login/CollectionViewManagers/CategoryManager.swift
@@ -9,14 +9,12 @@ import Foundation
 
 final class CategoryManager {
     private var items: [String] = []
+    private(set) var selectedIndex: Int?
     
-    init() {
-        self.configureItems()
-    }
-    
-    private func configureItems() {
+    func fetch(completion: @escaping(() -> Void)) {
         // TODO: 추후 DB 에서 수신하는 것으로 수정 예정
         self.items = ["수능 및 모의고사", "LEET", "공인회계사", "공인중개사", "9급 공무원"]
+        completion()
     }
     
     var count: Int {
@@ -25,5 +23,9 @@ final class CategoryManager {
     
     func item(at: Int) -> String {
         return self.items[at]
+    }
+    
+    func selected(to index: Int) {
+        self.selectedIndex = index
     }
 }
