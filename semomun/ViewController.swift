9c16bdb4953ad5733f5c7067aa3cfb3d39e3b26c
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-12-19 13:04:27 +0900

    [Add] create new project (modify project name)

---
+++ semomun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2021/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
