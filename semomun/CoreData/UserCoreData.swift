99e3ee385beb3296e3e0ba0e8b03ecaaa5f765df
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-12-22 11:58:06 +0900

    [Feat] UserCoreData 생성

---
+++ semomun/CoreData/UserCoreData.swift
@@ -0,0 +1,56 @@
+//
+//  UserCoreData.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2021/12/22.
+//
+//
+
+import Foundation
+import CoreData
+
+extension UserCoreData {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<UserCoreData> {
+        return NSFetchRequest<UserCoreData>(entityName: "UserCoreData")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var nickName: String?
+    @NSManaged public var phoneNumber: String?
+    @NSManaged public var favoriteCategory: String?
+    @NSManaged public var major: String?
+    @NSManaged public var majorDetail: String?
+    @NSManaged public var gender: String?
+    @NSManaged public var birthday: String?
+    @NSManaged public var schoolName: String?
+    @NSManaged public var graduationStatus: String?
+    @NSManaged public var userImage: Data?
+}
+
+extension UserCoreData : Identifiable {
+}
+
+
+@objc(UserCoreData)
+public class UserCoreData: NSManagedObject {
+    public override var description: String{
+        return "User(\(self.name!), \(self.nickName!), \(self.phoneNumber!), \(self.favoriteCategory!), \(self.major!), \(self.majorDetail!), \(self.gender!), \(self.birthday!), \(self.schoolName!), \(self.graduationStatus!)\n"
+    }
+    
+    func setValues(userInfo: SignUpInfo) {
+        self.setValue(userInfo.name, forKey: "name")
+        self.setValue(userInfo.nickName, forKey: "nickName")
+        self.setValue(userInfo.phoneNumber, forKey: "phoneNumber")
+        self.setValue(userInfo.favoriteCategory, forKey: "favoriteCategory")
+        self.setValue(userInfo.major, forKey: "major")
+        self.setValue(userInfo.majorDetail, forKey: "majorDetail")
+        self.setValue(userInfo.gender, forKey: "gender")
+        self.setValue(userInfo.birthday, forKey: "birthday")
+        self.setValue(userInfo.schoolName, forKey: "schoolName")
+        self.setValue(userInfo.graduationStatus, forKey: "graduationStatus")
+    }
+    
+    func setUserImage(imageData: Data) {
+        self.setValue(imageData, forKey: "userImage")
+    }
+}
