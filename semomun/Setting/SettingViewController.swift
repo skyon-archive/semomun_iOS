92657cb798fad69679f8e9cb9ee87397b28b5244
FreeDeveloper97 <freedeveloper97@gmail.com>
2021-12-31 14:25:41 +0900

    [Feat] 이용약관, 개인정보처리방침 구현

---
+++ semomun/Setting/SettingViewController.swift
@@ -0,0 +1,57 @@
+//
+//  SettingViewController.swift
+//  semomun
+//
+//  Created by SEONG YEOL YI on 2021/12/28.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    static let identifier = "SettingViewController"
+    
+    @IBOutlet weak var versionNum: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "설정"
+        self.versionNum.text =  Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "1.0.0"
+    }
+    
+    @IBAction func userLogout(_ sender: Any) {
+        self.showAlertWithOK(title: "미지원 기능입니다", text: "업데이트로 반영될 예정입니다.")
+    }
+    
+    @IBAction func userWithdrawal(_ sender: Any) {
+    }
+    
+    @IBAction func openCustomerService(_ sender: Any) {
+    }
+    
+    @IBAction func openTermsAndCondition(_ sender: Any) {
+        guard let filepath = Bundle.main.path(forResource: "termsAndConditions", ofType: "txt") else { return }
+        do {
+            let text = try String(contentsOfFile: filepath)
+            self.popupTextViewController(title: "이용약관", text: text)
+        } catch {
+            self.showAlertWithOK(title: "에러", text: "파일로딩에 실패하였습니다.")
+        }
+    }
+    
+    @IBAction func openPersonalInformationPolicy(_ sender: Any) {
+        guard let filepath = Bundle.main.path(forResource: "personalInformationProcessingPolicy", ofType: "txt") else { return }
+        do {
+            let text = try String(contentsOfFile: filepath)
+            self.popupTextViewController(title: "개인정보 처리방침", text: text)
+        } catch {
+            self.showAlertWithOK(title: "에러", text: "파일로딩에 실패하였습니다.")
+        }
+    }
+}
+
+extension SettingViewController {
+    private func popupTextViewController(title: String, text: String) {
+        let vc = LongTextPopupViewController(title: title, text: text)
+        present(vc, animated: true, completion: nil)
+    }
+}
