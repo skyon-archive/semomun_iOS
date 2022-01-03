d132c73bd50122dbad04c53b753e4018fb977a81
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-01-03 14:35:42 +0900

    [Refactor] Color 제거 및 SemomunColor 수정 완료

--- semomun/Global/SemomunColor/SemomunColor.swift
+++ semomun/Global/SemomunColor/SemomunColor.swift
@@ -9,4 +9,7 @@ import Foundation
 
 enum SemomunColor {
     static let mainColor = "mainColor"
+    static let redColor = "redColor"
+    static let yellowColor = "yellowColor"
+    static let selectCellColor = "selectCellColor"
 }
