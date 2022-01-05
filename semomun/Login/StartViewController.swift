1a79d4a52e76fa536d41f950c480b0f618179dd8
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-01-05 16:39:42 +0900

    [Feat] 회원가입 절차 1차 로직 구현, POST, GET 로직 필요

---
+++ semomun/Login/StartViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StartViewController.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2022/01/05.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    static let identifier = "StartViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func start(_ sender: Any) {
+        let signUpInfo = UserInfo()
+        guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: SurveyViewController.identifier) as? SurveyViewController else { return }
+        nextVC.signUpInfo = signUpInfo
+        
+        self.title = ""
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    @IBAction func login(_ sender: Any) {
+        guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: LoginViewController.identifier) else { return }
+        
+        self.title = ""
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+}
