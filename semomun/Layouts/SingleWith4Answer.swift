88e9c7913db6553f8495e6986961310fef23a930
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-01-07 14:13:47 +0900

    [Feat] 복수정답처리 로직 구현

--- semomun/Layouts/SingleWith4Answer.swift
+++ semomun/Layouts/SingleWith4Answer.swift
@@ -125,7 +125,7 @@ class SingleWith4Answer: UIViewController, PKToolPickerObserver, PKCanvasViewDel
     }
     
     @IBAction func showAnswer(_ sender: Any) {
-        guard let answer = self.viewModel?.problem?.answer else { return }
+        guard let answer = self.viewModel?.answer else { return }
         
         self.answer.isSelected.toggle()
         if self.answer.isSelected {
@@ -210,13 +210,14 @@ extension SingleWith4Answer {
             self.checkNumbers[targetIndex-1].setTitleColor(UIColor.white, for: .normal)
         }
         // 채점이 완료된 경우 && 틀린 경우 정답을 빨간색으로 표시
-        if let answer = problem.answer,
+        if let answer = self.viewModel?.answer,
            problem.terminated == true {
             self.answer.isHidden = true
-            guard let targetIndex = Int(answer) else { return }
-            // 체크 이미지 표시
-            self.createCheckImage(to: targetIndex-1)
-            self.configureTimerView()
+            if answer != "복수",
+               let targetIndex = Int(answer) {
+                self.createCheckImage(to: targetIndex-1)
+                self.configureTimerView()
+            }   
         }
     }
     
