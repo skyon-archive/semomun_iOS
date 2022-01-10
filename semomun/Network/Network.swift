858e2793ee424e0a7d66421dffdc06e52673102a
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-01-10 12:57:45 +0900

    [Refactor] toRequestResult 메소드 추가

--- semomun/Network/Network.swift
+++ semomun/Network/Network.swift
@@ -19,29 +19,29 @@ struct Network: NetworkFetchable {
         print(url)
         AF.request(url, method: .get, parameters: param)
             .responseDecodable(of: String.self) { response in
-            let result = RequestResult(statusCode: response.response?.statusCode, data: response.data)
-            print(response.response?.statusCode ?? 400)
-            completion(result)
-        }.resume()
+                Self.toRequestResult(with: response, completion: completion)
+            }.resume()
     }
-
-    static func post(url: String, param: [String: String], completion: @escaping(RequestResult) -> Void) {
+    
+    static func post(url: String, param: [String: String], completion: @escaping (RequestResult) -> Void) {
         print(url, param)
         AF.request(url, method: .post, parameters: param)
             .responseDecodable(of: String.self) { response in
-            let result = RequestResult(statusCode: response.response?.statusCode, data: response.data)
-            print(response.response?.statusCode ?? 400)
-            completion(result)
-        }.resume()
+                Self.toRequestResult(with: response, completion: completion)
+            }.resume()
     }
     
-    static func put(url: String, param: [String: String], completion: @escaping(RequestResult) -> Void) {
+    static func put(url: String, param: [String: String], completion: @escaping (RequestResult) -> Void) {
         print(url, param)
         AF.request(url, method: .put, parameters: param)
             .responseDecodable(of: String.self) { response in
-            let result = RequestResult(statusCode: response.response?.statusCode, data: response.data)
-            print(response.response?.statusCode ?? 400)
-            completion(result)
-        }.resume()
+                Self.toRequestResult(with: response, completion: completion)
+            }.resume()
+    }
+    
+    static func toRequestResult(with response: DataResponse<String, AFError>, completion: @escaping (RequestResult) -> Void) {
+        let result = RequestResult(statusCode: response.response?.statusCode, data: response.data)
+        print(response.response?.statusCode ?? 400)
+        completion(result)
     }
 }
