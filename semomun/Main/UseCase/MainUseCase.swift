8b08b6018440bfe0a07320e5825dba136d5a5d1a
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-01-13 16:56:09 +0900

    [Refactor] MainViewModel, MainUseCase 1차구현 및 Combine binding 구조 사용

--- semomun/Main/UseCase/MainUseCase.swift
+++ semomun/Main/UseCase/MainUseCase.swift
@@ -8,12 +8,44 @@
 import Foundation
 
 typealias MainFetchables = (PagesFetchable & VersionFetchable)
+
 protocol MainLogic {
+    func getVersion (completion: @escaping ((NetworkStatus, AppstoreVersion?) -> Void))
+    func getPages(sid: Int, completion: @escaping (([PageOfDB]) -> Void))
+    func updateVersion(with appstoreVersion: String) -> Bool
+    func savePages(sid: Int, pages: [PageOfDB], loading: loadingDelegate, completion: @escaping(Section_Core?) -> Void)
 }
 
 class MainUseCase: MainLogic {
     let networkUseCase: MainFetchables
     init(networkUseCase: MainFetchables) {
         self.networkUseCase = networkUseCase
+    }
+    
+    func getVersion(completion: @escaping ((NetworkStatus, AppstoreVersion?) -> Void)) {
+        self.networkUseCase.getAppstoreVersion { status, versionDTO in
+            completion(status, versionDTO)
+        }
+    }
+    
+    func getPages(sid: Int, completion: @escaping (([PageOfDB]) -> Void)) {
+        self.networkUseCase.getPages(sid: sid) { views in
+            completion(views)
+        }
+    }
+    
+    func updateVersion(with appstoreVersion: String) -> Bool {
+        guard let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else {
+            print("Error: can't read version")
+            return false
+        }
+        print(version, appstoreVersion)
+        return version != appstoreVersion
+    }
+    
+    func savePages(sid: Int, pages: [PageOfDB], loading: loadingDelegate, completion: @escaping(Section_Core?) -> Void) {
+        CoreUsecase.savePages(sid: sid, pages: pages, loading: loading) { section in
+            completion(section)
+        }
     }
 }
--- semomun/Main/ViewModel/MainViewModel.swift
+++ semomun/Main/ViewModel/MainViewModel.swift
@@ -6,10 +6,59 @@
 //
 
 import Foundation
+import Combine
 
 class MainViewModel {
-    let useCase: MainLogic
+    @Published private(set) var updateToVersion: String?
+    @Published private(set) var networkWarning: String?
+    @Published private(set) var createLoading: Bool = false
+    @Published private(set) var downloadedSection: Bool = false
+    private let useCase: MainLogic
+    private var downloadedPages: [PageOfDB] = []
+    private(set) var selectedSid: Int?
+    
     init(useCase: MainLogic) {
         self.useCase = useCase
+    }
+    
+    func getVersion() {
+        self.useCase.getVersion { status, versionDTO in
+            switch status {
+            case .SUCCESS:
+                print("get version success")
+                guard let versionDTO = versionDTO else { return }
+                if !versionDTO.results.isEmpty, let version = versionDTO.results.first?.version {
+                    if self.useCase.updateVersion(with: version) {
+                        self.updateToVersion = version
+                    }
+                }
+                print("version is empty list")
+            case .ERROR:
+                self.networkWarning = "네트워크 비정상"
+            default:
+                return
+            }
+        }
+    }
+    
+    func getPages(sid: Int) {
+        self.useCase.getPages(sid: sid) { views in
+            print("NETWORK RESULT")
+            print(views)
+            self.downloadedPages = views
+            self.createLoading = true
+        }
+    }
+    
+    func selectSection(to sid: Int) {
+        self.selectedSid = sid
+    }
+    
+    func savePages(loading: loadingDelegate) {
+        guard let sid = self.selectedSid else { return }
+        self.useCase.savePages(sid: sid, pages: self.downloadedPages, loading: loading) { section in
+            loading.terminate()
+            self.downloadedSection = true
+        }
     }
 }
