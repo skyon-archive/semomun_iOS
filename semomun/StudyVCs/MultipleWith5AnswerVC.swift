7ff2867a344f7918e64b209f707ecd17dfe148bb
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-01-21 12:41:32 +0900

    [Feat] Storyboard 분리, ViewController 파일들 생성

---
+++ semomun/StudyVCs/MultipleWith5AnswerVC.swift
@@ -0,0 +1,29 @@
+//
+//  MultipleWith5AnswerVC.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2022/01/21.
+//
+
+import UIKit
+
+class MultipleWith5AnswerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
