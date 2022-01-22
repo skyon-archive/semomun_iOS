4a39cf1d1da887020a7b1629017b17f5bf1fbf09
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-01-22 17:56:28 +0900

    [Feat] NavigationTitleView 생성

--- semomun/ProfileVCs/ProfileVC.swift
+++ semomun/ProfileVCs/ProfileVC.swift
@@ -8,22 +8,15 @@
 import UIKit
 
 class ProfileVC: UIViewController {
-
+    
+    @IBOutlet weak var navigationTitleView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.setShadow(with: navigationTitleView)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
