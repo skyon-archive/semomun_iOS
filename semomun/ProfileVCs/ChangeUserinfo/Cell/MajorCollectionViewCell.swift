5d90b02eadab082866735a48a6c85b76053ef762
Yeolyi <type101315@gmail.com>
2022-01-23 23:55:00 +0900

    [Feat] 계정 정보 변경하기 뷰 추가

---
+++ semomun/ProfileVCs/ChangeUserinfo/Cell/MajorCollectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  MajorCollectionViewCell.swift
+//  semomun
+//
+//  Created by SEONG YEOL YI on 2022/01/23.
+//
+
+import UIKit
+
+class MajorCollectionViewCell: UICollectionViewCell {
+    static let identifier = "MajorCollectionViewCell"
+    
+    @IBOutlet weak var majorName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        configureUI(major: "", isSelected: false)
+    }
+    
+    func configureUI(major: String, isSelected: Bool) {
+        self.majorName.text = major
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor(named: "mainColor")!.cgColor
+        self.layer.cornerRadius = 5
+        if isSelected {
+            self.backgroundColor = UIColor(named: "mainColor")
+            self.majorName.textColor = .white
+        } else {
+            self.backgroundColor = .white
+            self.majorName.textColor = UIColor(named: "mainColor")!
+        }
+    }
+}
