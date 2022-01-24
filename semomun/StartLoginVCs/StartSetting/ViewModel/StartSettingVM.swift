266651a3173b15b15b1ece7aab9bcae6b30b2ba8
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-01-24 13:33:06 +0900

    [Refactor] 기획 수정에 따른 tag 표시 및 로직 수정

--- semomun/StartLoginVCs/StartSetting/ViewModel/StartSettingVM.swift
+++ semomun/StartLoginVCs/StartSetting/ViewModel/StartSettingVM.swift
@@ -10,13 +10,11 @@ import Combine
 
 final class StartSettingVM {
     private let networkUsecase: NetworkUsecase
-    @Published private(set) var mediumTags: [String] = []
-    @Published private(set) var smallTags: [[String]] = [[]]
-    @Published private(set) var currentMediumIndex: Int = 0
-    @Published private(set) var currentSmallIndex: Int?
+    @Published private(set) var tags: [String] = []
     @Published private(set) var error: String?
-    private(set) var selectedMediumTag: String?
-    private(set) var selectedSmallTag: String?
+    @Published private(set) var warning: String?
+    private(set) var selectedTags: [String] = []
+    private(set) var selectedIndexes: [Int] = []
     
     init(networkUsecase: NetworkUsecase) {
         self.networkUsecase = networkUsecase
@@ -24,49 +22,35 @@ final class StartSettingVM {
     
     func fetchTags() {
         // TODO: Network 에서 받아오는 로직 필요
-        self.mediumTags = ["수험서/자격증","대학교재","외국어","고등","중등","초등"]
-        let small1 = ["국가 기술 자격", "국가 전문 자격", "귀화시험", "민간자격", "국가직 7급 공무원", "국가직 9급 공무원", "경찰공무원", "소방공무원", "기타공무원 시험", "교원임용", "한국사능력검정시험", "공사 공단 수험서", "기업 적성 검사", "공인중개사/주택관리사", "법학적성시험", "MEET/DEET/PEET", "공인회계사", "검정고시", "변호사 시험", "취업/상식", "PSAT", "컴퓨터 활용 능력", "운전면허"]
-        let small2 = ["대학1", "대학2", "대학3"]
-        let small3 = ["영어", "중국어", "프랑스어"]
-        let small4 = ["고등1", "고등2", "고등3"]
-        let small5 = ["중등1", "중등2", "중등3"]
-        let small6 = ["초등1", "초등2", "초등3"]
-        self.smallTags = [small1, small2, small3, small4, small5, small6]
+        self.tags = ["수험서/자격증","대학교재","외국어","고등","중등","초등", "국가 기술 자격", "국가 전문 자격", "귀화시험", "민간자격", "국가직 7급 공무원", "국가직 9급 공무원", "경찰공무원", "소방공무원", "기타공무원 시험", "교원임용", "한국사능력검정시험", "공사 공단 수험서", "기업 적성 검사", "공인중개사/주택관리사", "법학적성시험", "MEET/DEET/PEET", "공인회계사", "검정고시", "변호사 시험", "취업/상식", "PSAT", "컴퓨터 활용 능력", "운전면허"]
     }
     
-    func selectMedium(to index: Int) {
-        self.selectedMediumTag = self.mediumTag(index: index)
-        self.currentSmallIndex = nil
-        self.selectedSmallTag = nil
-        self.currentMediumIndex = index
+    func select(to index: Int) {
+        // 제거
+        if self.selectedIndexes.contains(index) {
+            self.selectedIndexes = self.selectedIndexes.filter { $0 != index }
+            self.selectedTags = self.selectedTags.filter { $0 != self.tags[index] }
+        }
+        // 추가
+        else {
+            if selectedTags.count >= 5 {
+                self.warning = "5개 이하만 선택해주세요"
+                return
+            }
+            self.selectedIndexes.append(index)
+            self.selectedTags.append(self.tags[index])
+        }
     }
     
-    func selectSmall(to index: Int) {
-        self.currentSmallIndex = index
-        self.selectedSmallTag = self.smallTag(index: index)
-    }
-    
-    func mediumTag(index: Int) -> String {
-        return self.mediumTags[index]
-    }
-    
-    func smallTag(index: Int) -> String {
-        return self.smallTags[currentMediumIndex][index]
+    func tag(index: Int) -> String {
+        return self.tags[index]
     }
     
     func saveUserDefaults() {
         // TODO: 중분류, 소분류를 저장해야 하는지 의논 후 구조 생성할 예정
     }
     
-    var smallTagsCount: Int {
-        if self.smallTags.isEmpty {
-            return 0
-        } else {
-            return self.smallTags[self.currentMediumIndex].count
-        }
-    }
-    
     var isSelectFinished: Bool {
-        return self.selectedMediumTag != nil && self.selectedSmallTag != nil
+        return self.selectedTags.count > 0 && self.selectedTags.count <= 5
     }
 }
