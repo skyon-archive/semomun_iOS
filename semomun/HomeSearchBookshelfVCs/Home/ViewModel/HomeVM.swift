1aefcd120a946bff995ca327c504a1dd2becb59b
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-01-24 19:24:13 +0900

    [Feat] bestSellers 구조 구현 및 표시 완료

--- semomun/HomeSearchBookshelfVCs/Home/ViewModel/HomeVM.swift
+++ semomun/HomeSearchBookshelfVCs/Home/ViewModel/HomeVM.swift
@@ -10,7 +10,14 @@ import Combine
 
 final class HomeVM {
     private let networkUsecase: NetworkUsecase
+    @Published private(set) var ads: [String] = [] // DTO로 타입이 변경될 부분
+    @Published private(set) var bestSellers: [PreviewOfDB] = []
+    @Published private(set) var workbooksWithTags: [PreviewOfDB] = []
+    @Published private(set) var workbooksWithRecent: [PreviewOfDB] = []
+    @Published private(set) var workbooksWithNewest: [PreviewOfDB] = []
     @Published private(set) var tags: [String] = []
+    @Published private(set) var error: String?
+    @Published private(set) var warning: (String, String)?
     
     init(networkUsecase: NetworkUsecase) {
         self.networkUsecase = networkUsecase
@@ -19,5 +26,27 @@ final class HomeVM {
     func fetchTags() {
         guard let tags = UserDefaultsManager.get(forKey: UserDefaultsManager.Keys.favoriteTags) as? [String] else { return }
         self.tags = tags
+    }
+    
+    func fetchAds() {
+        self.ads = Array(repeating: "https://forms.gle/suXByYKEied6RcSd8", count: 5)
+    }
+    
+    func fetchBestSellers() {
+        self.networkUsecase.getBestSellers { [weak self] status, workbooks in
+            switch status {
+            case .SUCCESS:
+                print(Array(workbooks.prefix(upTo: 10)))
+                self?.bestSellers = Array(workbooks.prefix(upTo: 10))
+            case .DECODEERROR:
+                self?.warning = ("올바르지 않는 형식", "최신 버전으로 업데이트 해주세요")
+            default:
+                self?.error = "네트워크 연결을 확인 후 다시 시도하세요"
+            }
+        }
+    }
+    
+    func bestSeller(index: Int) -> PreviewOfDB {
+        return self.bestSellers[index]
     }
 }
