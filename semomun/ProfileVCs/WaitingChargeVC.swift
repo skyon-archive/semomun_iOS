9846340c14ab382603a0cbef30232c651cdc4c54
Yeolyi <type101315@gmail.com>
2022-01-25 00:55:43 +0900

    [Feat] WaitingChargeVC 틀 추가

--- semomun/ProfileVCs/WaitingChargeVC.swift
+++ semomun/ProfileVCs/WaitingChargeVC.swift
@@ -13,23 +13,14 @@ class WaitingChargeVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func chargeComplete(_ sender: Any) {
+        
     }
-    */
-
 }
