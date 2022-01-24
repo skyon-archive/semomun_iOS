e753927c49218bf7e40c1ef5e14ca080280c5605
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-01-24 16:49:28 +0900

    [Feat] tag 표시 구현

---
+++ semomun/HomeSearchBookshelfVCs/Home/HomeVC.swift
@@ -0,0 +1,87 @@
+//
+//  HomeVC.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2022/01/21.
+//
+
+import UIKit
+import Combine
+
+final class HomeVC: UIViewController {
+    @IBOutlet weak var navigationTitleView: UIView!
+    @IBOutlet weak var bannerAds: UICollectionView!
+    @IBOutlet weak var bestSellers: UICollectionView!
+    @IBOutlet weak var workbooksWithTags: UICollectionView!
+    @IBOutlet weak var workbooksWithRecent: UICollectionView!
+    @IBOutlet weak var workbooksWithNewest: UICollectionView!
+    @IBOutlet weak var tagsStackView: UIStackView!
+    private var viewModel: HomeVM?
+    private var cancellables: Set<AnyCancellable> = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setShadow(with: navigationTitleView)
+        self.configureViewModel()
+        self.bindAll()
+        self.viewModel?.fetchTags()
+    }
+    
+    @IBAction func appendTags(_ sender: Any) {
+        
+    }
+}
+
+// MARK: - Configure
+extension HomeVC {
+    private func configureViewModel() {
+        let network = Network()
+        let networkUsecase = NetworkUsecase(network: network)
+        self.viewModel = HomeVM(networkUsecase: networkUsecase)
+    }
+    
+    private func configureTags(with tags: [String]) {
+        self.tagsStackView.translatesAutoresizingMaskIntoConstraints = false
+        tags.forEach { tag in
+            let tagView = UIView()
+            tagView.layer.borderWidth = 1
+            tagView.layer.cornerRadius = 15
+            tagView.clipsToBounds = true
+            tagView.layer.borderColor = UIColor(named: SemomunColor.mainColor)?.cgColor
+            let tagLabel = UILabel()
+            tagLabel.backgroundColor = .clear
+            tagLabel.textColor = UIColor(named: SemomunColor.mainColor)
+            tagLabel.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+            tagLabel.text = "#\(tag)"
+            
+            tagView.translatesAutoresizingMaskIntoConstraints = false
+            tagLabel.translatesAutoresizingMaskIntoConstraints = false
+            tagView.addSubview(tagLabel)
+            NSLayoutConstraint.activate([
+                tagView.heightAnchor.constraint(equalToConstant: 30),
+                tagLabel.centerXAnchor.constraint(equalTo: tagView.centerXAnchor),
+                tagLabel.centerYAnchor.constraint(equalTo: tagView.centerYAnchor),
+                tagLabel.leadingAnchor.constraint(equalTo: tagView.leadingAnchor, constant: 10)
+            ])
+            
+            self.tagsStackView.addArrangedSubview(tagView)
+        }
+    }
+}
+
+// MARK: - Binding
+extension HomeVC {
+    private func bindAll() {
+        self.bindTags()
+    }
+    
+    private func bindTags() {
+        self.viewModel?.$tags
+            .receive(on: DispatchQueue.main)
+            .dropFirst()
+            .sink(receiveValue: { [weak self] tags in
+                self?.configureTags(with: tags)
+            })
+            .store(in: &self.cancellables)
+    }
+}
