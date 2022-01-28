14d795f365d8fd95efb4b6c7136af23445346a03
이성열 <type101315@gmail.com>
2022-01-28 18:04:37 +0900

    [Refactor] txt파일을 LongTextVC로 표시하는 과정을 extension으로 간소화

---
+++ semomun/ProfileVCs/Setting/UnloginedSettingTableVC.swift
@@ -0,0 +1,35 @@
+//
+//  UnloginedSettingTableVC.swift
+//  semomun
+//
+//  Created by SEONG YEOL YI on 2022/01/24.
+//
+
+import UIKit
+
+class UnloginedSettingTableVC: UITableViewController {
+    static let storyboardName = "Profile"
+    static let identifier = "UnloginedSettingTableVC"
+    
+    @IBOutlet weak var versionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 109, bottom: 0, trailing: 109)
+        self.versionLabel.text =  Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "버전 정보 없음"
+    }
+}
+
+extension UnloginedSettingTableVC {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch (indexPath.section, indexPath.row) {
+        case (0, 1):
+            self.popupLongTextVC(title: "서비스이용약관", txtResourceName: "termsAndConditions")
+        case (0, 2):
+            self.popupLongTextVC(title: "개인정보 처리방침", txtResourceName: "personalInformationProcessingPolicy")
+        default:
+            break
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
