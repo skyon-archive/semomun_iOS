e8d359462c973cd439fae6bd658281dde70dd7fd
이성열 <type101315@gmail.com>
2022-01-28 18:04:37 +0900

    [Refactor] txt파일을 LongTextVC로 표시하는 과정을 extension으로 간소화

---
+++ semomun/ProfileVCs/Setting/LoginedSettingTableVC.swift
@@ -0,0 +1,70 @@
+//
+//  LoginedSettingTableVC.swift
+//  semomun
+//
+//  Created by SEONG YEOL YI on 2022/01/24.
+//
+
+import UIKit
+
+class LoginedSettingTableVC: UITableViewController {
+    static let storyboardName = "Profile"
+    static let identifier = "LoginedSettingTableVC"
+    
+    @IBOutlet weak var versionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.setHorizontalMargin(to: 109)
+        self.versionLabel.text =  Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "버전 정보 없음"
+    }
+    
+    @IBAction func logout(_ sender: Any) {
+        CoreUsecase.deleteAllCoreData()
+        self.deleteKeychain()
+        self.deleteUserDefaults()
+        self.showLogoutedAlert()
+    }
+}
+
+// MARK: - 로그아웃 로직
+extension LoginedSettingTableVC {
+    private func deleteKeychain() {
+        KeychainItem.deleteUserIdentifierFromKeychain()
+        print("keychain delete complete")
+    }
+    
+    private func deleteUserDefaults() {
+        let dictionary = UserDefaults.standard.dictionaryRepresentation()
+        dictionary.keys.forEach { key in
+            UserDefaults.standard.removeObject(forKey: key)
+        }
+        print("userDefaults delete complete")
+    }
+    
+    private func showLogoutedAlert() {
+        self.showAlertWithOK(title: "로그아웃 되었습니다", text: "") { [weak self] in
+            let startVC = UIStoryboard(name: StartVC.storyboardName, bundle: nil).instantiateViewController(withIdentifier: StartVC.identifier)
+            let navigationController = UINavigationController(rootViewController: startVC)
+            navigationController.modalPresentationStyle = .fullScreen
+            self?.present(navigationController, animated: true)
+        }
+    }
+}
+
+// MARK: - TableView 선택 
+extension LoginedSettingTableVC {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch (indexPath.section, indexPath.row) {
+        case (1, 1):
+            self.popupLongTextVC(title: "서비스이용약관", txtResourceName: "termsAndConditions")
+        case (1, 2):
+            self.popupLongTextVC(title: "개인정보 처리방침", txtResourceName: "personalInformationProcessingPolicy")
+        case (1, 3):
+            self.popupLongTextVC(title: "마케팅 정보 수신", txtResourceName: "receiveMarketingInfo", marketingInfo: true)
+        default:
+            break
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
--- semomun/ProfileVCs/Setting/SettingVC.swift
+++ semomun/ProfileVCs/Setting/SettingVC.swift
@@ -7,48 +7,60 @@
 
 import UIKit
 
-class SettingVC: UIViewController {
+final class SettingVC: UIViewController {
     static let storyboardName = "Profile"
     static let identifier = "SettingVC"
 
+    private var isLogin = UserDefaultsManager.get(forKey: UserDefaultsManager.Keys.logined) as? Bool ?? false
+    private lazy var tableViewAfterLogin: UIViewController = {
+        let storyboard = UIStoryboard(name: LoginedSettingTableVC.storyboardName, bundle: nil)
+        return storyboard.instantiateViewController(withIdentifier: LoginedSettingTableVC.identifier)
+    }()
+    private lazy var tableViewBeforeLogin: UIViewController = {
+        let storyboard = UIStoryboard(name: UnloginedSettingTableVC.storyboardName, bundle: nil)
+        return storyboard.instantiateViewController(withIdentifier: UnloginedSettingTableVC.identifier)
+    }()
+    
     @IBOutlet weak var containerView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "설정"
-        configureTableView()
+        self.addTableView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: true)
+        self.updateIfLoginStatusChanged()
+    }
+}
+
+extension SettingVC {
+    private func updateIfLoginStatusChanged() {
         let currentLogin = UserDefaultsManager.get(forKey: UserDefaultsManager.Keys.logined) as? Bool ?? false
         if self.isLogin != currentLogin {
             self.isLogin = currentLogin
-            configureTableView()
+            self.configureTableView()
         }
     }
-    
     private func configureTableView() {
+        self.removeTableView()
+        self.addTableView()
+    }
+    
+    private func removeTableView() {
         let toRemove = self.isLogin ? tableViewBeforeLogin : tableViewAfterLogin
         toRemove.willMove(toParent: nil)
         toRemove.view.removeFromSuperview()
         toRemove.removeFromParent()
-        
+    }
+    
+    private func addTableView() {
         let target = self.isLogin ? tableViewAfterLogin : tableViewBeforeLogin
         target.view.frame = self.containerView.bounds
         self.containerView.addSubview(target.view)
         self.addChild(target)
         target.didMove(toParent: self)
     }
-    
-    private var isLogin = UserDefaultsManager.get(forKey: UserDefaultsManager.Keys.logined) as? Bool ?? false
-    private lazy var tableViewAfterLogin: UIViewController = {
-        let storyboard = UIStoryboard(name: SettingTableVC.storyboardName, bundle: nil)
-        return storyboard.instantiateViewController(withIdentifier: SettingTableVC.identifier)
-    }()
-    private lazy var tableViewBeforeLogin: UIViewController = {
-        let storyboard = UIStoryboard(name: BeforeLoginSettingTableVC.storyboardName, bundle: nil)
-        return storyboard.instantiateViewController(withIdentifier: BeforeLoginSettingTableVC.identifier)
-    }()
 }
