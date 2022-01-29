f7c12da470428e0b38d6dce8626fe4951f7ac86f
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-01-29 18:54:23 +0900

    [Feat] WorkbookDetailVC popupVC UI 구현

---
+++ semomun/HomeSearchBookshelfVCs/Purchase/PurchaseWarningPopupVC.swift
@@ -0,0 +1,23 @@
+//
+//  PurchaseWarningPopupVC.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2022/01/29.
+//
+
+import UIKit
+
+final class PurchaseWarningPopupVC: UIViewController {
+    static let identifier = "PurchaseWarningPopupVC"
+    static let storyboardName = "HomeSearchBookshelf"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
