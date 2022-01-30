816f9497723a3799bc3bcacb350c4113e0fe7b7f
Yeolyi <type101315@gmail.com>
2022-01-30 22:22:23 +0900

    [Feat] shadowPath와 rasterize를 활용해 addShadow 함수 최적화

--- semomun/ProfileVCs/Semopay/SemopayCell.swift
+++ semomun/ProfileVCs/Semopay/SemopayCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class SemopayCell: UITableViewCell {
     static let identifier = "SemopayCell"
+    static let dividerSublayerName = "SemopayDivider"
     
     @IBOutlet private weak var date: UILabel!
     @IBOutlet private weak var workbookName: UILabel!
@@ -16,10 +17,15 @@ final class SemopayCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.addShadow(direction: .bottom, shouldRasterize: true)
+        self.clipsToBounds = false
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        self.contentView.layer.cornerRadius = 0
+        self.contentView.layer.mask = nil
+        self.contentView.layer.sublayers?.removeAll(where: { $0.name == Self.dividerSublayerName})
     }
 }
 
@@ -40,7 +46,7 @@ extension SemopayCell {
         let attrString = NSMutableAttributedString(string: costToString)
         
         let costRange = NSRange(location: 0, length: costToString.count-1)
-        let wonRange = NSRange(location: costToString.count-1, length: 1)
+        // let wonRange = NSRange(location: costToString.count-1, length: 1)
         
         let costColor: UIColor = cost < 0 ? .red : .blue
         let costAttribute = [NSAttributedString.Key.foregroundColor: costColor]
@@ -51,3 +57,78 @@ extension SemopayCell {
     }
 }
 
+extension SemopayCell {
+    func configureCellUI(at position: CellPosition) {
+        switch position {
+        case .oneAndOnly:
+            self.makeCornerRadius(at: .all)
+        case .top:
+            self.makeCornerRadius(at: .top)
+            self.addBottomDivider()
+            self.clipShadow(at: .bottom)
+        case .bottom:
+            self.makeCornerRadius(at: .bottom)
+            self.clipShadow(at: .top)
+        case .middle:
+            self.addBottomDivider()
+            self.clipShadow(at: .both)
+            self.changeShadowOffset(to: CGSize())
+        }
+    }
+    
+    enum CellPosition {
+        case oneAndOnly, top, bottom, middle
+    }
+    
+    private enum ShadowClipDirection {
+        case top, bottom, both
+    }
+    
+    private enum CornerRadiusDirection {
+        case top, bottom, all
+    }
+    
+    private func addBottomDivider() {
+        guard let dividerColor = UIColor(named: "grayLineColor") else { return }
+        let dividerHeight: CGFloat = 0.25
+        let dividerMargin: CGFloat = 39
+        let border = CALayer()
+        border.name = Self.dividerSublayerName
+        border.backgroundColor = dividerColor.cgColor
+        border.frame = CGRect(x: dividerMargin, y: self.contentView.frame.size.height - dividerHeight, width: self.contentView.frame.size.width - 2*dividerMargin, height: dividerHeight)
+        self.contentView.layer.addSublayer(border)
+    }
+    
+    private func clipShadow(at direction: ShadowClipDirection) {
+        let shadowRadius: CGFloat = 10
+        let layer = CALayer()
+        layer.backgroundColor = UIColor.white.cgColor
+        switch direction {
+        case .top:
+            layer.frame = .init(-shadowRadius, 0, self.layer.frame.width+2*shadowRadius, self.layer.frame.height+shadowRadius)
+        case .bottom:
+            layer.frame = .init(-shadowRadius, -shadowRadius, self.layer.frame.width+2*shadowRadius, self.layer.frame.height+shadowRadius)
+        case .both:
+            layer.frame = .init(-shadowRadius, 0, self.layer.frame.width+2*shadowRadius, self.layer.frame.height)
+        }
+        self.layer.mask = layer
+    }
+    
+    private func makeCornerRadius(at direction: CornerRadiusDirection) {
+        let cornerRadius: CGFloat = 10
+        switch direction {
+        case .top:
+            let path = UIBezierPath(roundedRect: self.contentView.bounds, byRoundingCorners: [.topLeft, .topRight], cornerRadii: CGSize(width: cornerRadius, height:  cornerRadius))
+            let maskLayer = CAShapeLayer()
+            maskLayer.path = path.cgPath
+            self.contentView.layer.mask = maskLayer
+        case .bottom:
+            let path = UIBezierPath(roundedRect: self.contentView.bounds, byRoundingCorners: [.bottomLeft, .bottomRight], cornerRadii: CGSize(width: cornerRadius, height:  cornerRadius))
+            let maskLayer = CAShapeLayer()
+            maskLayer.path = path.cgPath
+            self.contentView.layer.mask = maskLayer
+        case .all:
+            self.contentView.layer.cornerRadius = cornerRadius
+        }
+    }
+}
