ecdc4f51a6a46ff23eb05faed4a9eca251cf8899
Yeolyi <type101315@gmail.com>
2022-01-31 00:15:14 +0900

    [Feat] SemopayVC의 남은 금액 관련 NetworkUsecase와 binding 추가

--- semomun/Network/NetworkUsecase.swift
+++ semomun/Network/NetworkUsecase.swift
@@ -499,7 +499,7 @@ extension NetworkUsecase: SemopayHistoryFetchable {
             let wids = searchPreview.workbooks.map(\.wid)
             let testData: [SemopayHistory] = Array(1...20).map { _ in
                 let cost = makeRandomCost()
-                let wid = cost < 0 ? nil : wids.randomElement()
+                let wid = cost > 0 ? nil : wids.randomElement()
                 return SemopayHistory(wid: wid, date: makeRandomPastDate(), cost: cost)
             }.sorted(by: { $0.date > $1.date })
             // 정렬은 프론트에서? 백에서?
@@ -524,5 +524,11 @@ extension NetworkUsecase: WorkbookFetchable {
             }
             handler(searchWorkbook)
         }
+    }
+}
+
+extension NetworkUsecase: RemainingSemopayFetchable {
+    func getRemainingSemopay(completion: @escaping ((NetworkStatus, Int)) -> Void) {
+        completion((.SUCCESS, 1000000))
     }
 }
