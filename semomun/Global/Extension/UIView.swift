0fd2426f08154871f4dd5769282a16514ae2c737
Yeolyi <type101315@gmail.com>
2022-01-31 21:39:45 +0900

    [Feat] UIView에 특정 면의 그림자를 자르는 함수 추가

--- semomun/Global/Extension/UIView.swift
+++ semomun/Global/Extension/UIView.swift
@@ -23,7 +23,6 @@ extension UIView {
     }
     
     func addShadow(direction: ShadowDirection = .center, offset: CGSize? = nil, shouldRasterize: Bool = false) {
-        
         let shadowLayer = self.layer.sublayers?.first(where: { $0.name == Self.shadowLayerName }) ?? CAShapeLayer()
         shadowLayer.name = Self.shadowLayerName
         shadowLayer.shadowOpacity = 0.3
@@ -58,5 +57,40 @@ extension UIView {
             return
         }
         shadowLayer.shadowOffset = offset
+    }
+    
+    enum ShadowClipDirection {
+        case top, bottom, both
+    }
+    
+    func clipShadow(at direction: ShadowClipDirection) {
+        guard let shadowLayer = self.layer.sublayers?.first(where: { $0.name == Self.shadowLayerName }) else { return }
+        let shadowRadius: CGFloat = shadowLayer.shadowRadius * 2
+        let shadowLayerHeight = shadowLayer.frame.height
+        let layer = CALayer()
+        layer.backgroundColor = UIColor.white.cgColor
+        // 왼쪽부분 그림자는 항상 남음
+        let x = -shadowRadius
+        // 마찬가지로 좌우 그림자는 항상 남으므로 셀의 너비 + 양쪽 그림자의 넓이
+        let w = shadowLayer.frame.width+2*shadowRadius
+        let y, h: CGFloat
+        switch direction {
+        case .top:
+            y = 0
+            h = shadowLayerHeight+shadowRadius
+        case .bottom:
+            y = -shadowRadius
+            h = shadowLayerHeight+shadowRadius
+        case .both:
+            y = 0
+            h = shadowLayerHeight
+        }
+        layer.frame = CGRect(x, y, w, h)
+        shadowLayer.mask = layer
+    }
+    
+    func undoClipShadow() {
+        guard let shadowLayer = self.layer.sublayers?.first(where: { $0.name == Self.shadowLayerName }) else { return }
+        shadowLayer.mask = nil
     }
 }
