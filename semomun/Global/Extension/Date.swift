e7ab4e39eee95fa208ef9cb4e7452d8422886697
Yeolyi <type101315@gmail.com>
2022-02-03 00:36:48 +0900

    [Refactor] 날짜 포매팅 관련 코드 extension으로 이동

--- semomun/Global/Extension/Date.swift
+++ semomun/Global/Extension/Date.swift
@@ -13,4 +13,9 @@ extension Date {
         guard let year = calendarDate.year, let month = calendarDate.month else { return "20xx.xx" }
         return String(format: "%d. %02d", year, month)
     }
+    var yearMonthDayText: String {
+        let calendarDate = Calendar.current.dateComponents([.year, .month, .day], from: self)
+        guard let year = calendarDate.year, let month = calendarDate.month, let day = calendarDate.day else { return "20xx.xx.xx" }
+        return String(format: "%d.%02d.%02d", year, month, day)
+    }
 }
