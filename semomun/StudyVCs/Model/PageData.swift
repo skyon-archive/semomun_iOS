d3b195a787c6b5ae5da23599d720df1d4902a397
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-02-04 16:53:47 +0900

    [Refactor] 1.0 기준 VC identifier Convert 로직 구현

--- semomun/StudyVCs/Model/PageData.swift
+++ semomun/StudyVCs/Model/PageData.swift
@@ -10,6 +10,15 @@ import CoreData
 import UIKit
 
 final class PageData {
+    enum PastLayout {
+        static let Concept = "Concept"
+        static let SingleWithNoAnswer = "SingleWithNoAnswer"
+        static let SingleWithTextAnswer = "SingleWithTextAnswer"
+        static let SingleWith4Answer = "SingleWith4Answer"
+        static let SingleWith5Answer = "SingleWith5Answer"
+        static let MultipleWithNoAnswer = "MultipleWithNoAnswer"
+        static let MultipleWith5Answer = "MultipleWith5Answer"
+    }
     let vid: Int
     var layoutType: String!
     var pageCore: Page_Core!
@@ -25,7 +34,7 @@ final class PageData {
     private func fetchPageCore() {
         if let pageData = CoreUsecase.fetchPage(vid: self.vid) {
             self.pageCore = pageData
-            self.layoutType = pageData.layoutType
+            self.layoutType = self.layoutType(from: pageData.layoutType)
         }
     }
     
@@ -38,6 +47,35 @@ final class PageData {
                 let problem = Problem_Core(context: CoreDataManager.shared.context)
                 problem.setValue(UIImage(named: SemomunImage.warning)!.pngData, forKey: "contentImage")
             }
+        }
+    }
+    
+    private func layoutType(from type: String) -> String {
+        switch type {
+        // Version 2.0
+        case ConceptVC.identifier,
+            SingleWithNoAnswerVC.identifier,
+            SingleWithTextAnswerVC.identifier,
+            SingleWith4AnswerVC.identifier,
+            SingleWith5AnswerVC.identifier,
+            MultipleWithNoAnswerVC.identifier,
+            MultipleWith5AnswerVC.identifier:
+            return type
+        // Version 1.0
+        case PastLayout.Concept:
+            return ConceptVC.identifier
+        case PastLayout.SingleWithNoAnswer:
+            return SingleWithNoAnswerVC.identifier
+        case PastLayout.SingleWith4Answer:
+            return SingleWith4AnswerVC.identifier
+        case PastLayout.SingleWith5Answer:
+            return SingleWith5AnswerVC.identifier
+        case PastLayout.MultipleWithNoAnswer:
+            return MultipleWithNoAnswerVC.identifier
+        case PastLayout.MultipleWith5Answer:
+            return MultipleWith5AnswerVC.identifier
+        default:
+            return MultipleWith5AnswerVC.identifier
         }
     }
 }
