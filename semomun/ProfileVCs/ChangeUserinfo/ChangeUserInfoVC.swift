e04b8f42bba417142bcf1b456a73179673008635
이성열 <type101315@gmail.com>
2022-02-10 18:17:42 +0900

    [Fix] ChangeUserInfoVM이 새로운 버전에서 필요한 추가적인 정보가 기입되었음을 저장할  수 있도록 수정

--- semomun/ProfileVCs/ChangeUserinfo/ChangeUserInfoVC.swift
+++ semomun/ProfileVCs/ChangeUserinfo/ChangeUserInfoVC.swift
@@ -66,7 +66,6 @@ final class ChangeUserInfoVC: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: true)
-        UserDefaultsManager.set(to: "2.0", forKey: UserDefaultsManager.Keys.userVersion)
     }
     
     @IBAction func checkNickname(_ sender: Any) {
