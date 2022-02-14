55d710199aa91242413140a59bb27127bd6f56d7
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-02-14 15:04:58 +0900

    [Feat] SearchTagVM 생성, tag 표시 및 제거로직 구현

--- semomun/Global/CollectionViewFlowLayout/TagsLayout.swift
+++ semomun/Global/CollectionViewFlowLayout/TagsLayout.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TagsLayout: UICollectionViewFlowLayout {
+final class TagsLayout: UICollectionViewFlowLayout {
     
     required override init() {super.init(); common()}
     required init?(coder aDecoder: NSCoder) {super.init(coder: aDecoder); common()}
@@ -36,26 +36,3 @@ class TagsLayout: UICollectionViewFlowLayout {
         return att
     }
 }
-
-//class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
-//
-//    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-//        let attributes = super.layoutAttributesForElements(in: rect)
-//
-//        var leftMargin = sectionInset.left
-//        var maxY: CGFloat = -1.0
-//        attributes?.forEach { layoutAttribute in
-//            if layoutAttribute.frame.origin.y >= maxY {
-//                leftMargin = sectionInset.left
-//            }
-//
-//            layoutAttribute.frame.origin.x = leftMargin
-//
-//            leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
-//            maxY = max(layoutAttribute.frame.maxY , maxY)
-//        }
-//
-//        return attributes
-//    }
-//}
-
--- semomun/HomeSearchBookshelfVCs/Home/SearchTagVC.swift
+++ semomun/HomeSearchBookshelfVCs/Home/SearchTagVC.swift
@@ -15,9 +15,15 @@ final class SearchTagVC: UIViewController {
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var selectedTags: UICollectionView!
     @IBOutlet weak var searchTagResults: UITableView!
+    private var cancellables: Set<AnyCancellable> = []
+    private var viewModel: SearchTagVM?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureCollectionView()
+        self.configureTableView()
+        self.configureViewModel()
+        self.bindAll()
     }
     
     @IBAction func close(_ sender: Any) {
@@ -26,5 +32,59 @@ final class SearchTagVC: UIViewController {
     
     @IBAction func searchTag(_ sender: Any) {
         
+    }
+}
+
+extension SearchTagVC {
+    private func configureCollectionView() {
+        self.selectedTags.collectionViewLayout = TagsLayout()
+        self.selectedTags.dataSource = self
+        self.selectedTags.delegate = self
+    }
+    
+    private func configureTableView() {
+        
+    }
+    
+    private func configureViewModel() {
+        let network = Network()
+        let networkUsecase = NetworkUsecase(network: network)
+        self.viewModel = SearchTagVM(networkUsecase: networkUsecase)
+    }
+}
+
+extension SearchTagVC {
+    private func bindAll() {
+        self.bindTags()
+    }
+    
+    private func bindTags() {
+        self.viewModel?.$tags
+            .receive(on: DispatchQueue.main)
+            .dropFirst()
+            .sink(receiveValue: { [weak self] _ in
+                self?.selectedTags.reloadData()
+            })
+            .store(in: &self.cancellables)
+    }
+}
+
+extension SearchTagVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.viewModel?.tags.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SmallTagCell.identifier, for: indexPath) as? SmallTagCell else { return UICollectionViewCell() }
+        guard let tag = self.viewModel?.tags[indexPath.item] else { return cell }
+        cell.configure(tag: tag)
+        
+        return cell
+    }
+}
+
+extension SearchTagVC: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.viewModel?.removeTag(index: indexPath.item)
     }
 }
