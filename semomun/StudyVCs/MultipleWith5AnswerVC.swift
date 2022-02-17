f6e07596dbe8e135e5e3e9c3cef27eddeba6b491
이성열 <type101315@gmail.com>
2022-02-17 16:54:08 +0900

    [Fix] CanvasView 하단에 드로잉이 안되는 문제 수정

--- semomun/StudyVCs/MultipleWith5AnswerVC.swift
+++ semomun/StudyVCs/MultipleWith5AnswerVC.swift
@@ -63,7 +63,7 @@ class MultipleWith5AnswerVC: UIViewController, PKToolPickerObserver, PKCanvasVie
         self.collectionView.reloadData()
         self.configureCanvasView()
         self.configureCanvasViewData()
-        self.canvasView.zoomScale = 1.0
+        self.scrollView.zoomScale = 1.0
     }
     
     override func viewDidAppear(_ animated: Bool) {
