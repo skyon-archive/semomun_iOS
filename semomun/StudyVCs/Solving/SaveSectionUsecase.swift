96d33cd703025fa8f13a56cc55b61ca79c2044c7
Yeolyi <type101315@gmail.com>
2022-02-21 18:57:28 +0900

    [Refactor] 채점 UI가 나타나게 하기 위한 임시 코드 추가

--- semomun/StudyVCs/Solving/SaveSectionUsecase.swift
+++ semomun/StudyVCs/Solving/SaveSectionUsecase.swift
@@ -19,44 +19,41 @@ class SaveSectionUsecase {
     
     init(section: Section_Core) {
         self.section = section
-        self.fetchVids()
-        self.calculateSectionResult()
-    }
-    
-    private func fetchVids() {
-        let dumlicatedVids = section.dictionaryOfProblem.values.map() { $0 }
-        self.vids = Array(Set(dumlicatedVids)).sorted(by: < )
-    }
-    
-    private func calculateSectionResult() {
-        self.vids.forEach { vid in
-            self.calculateProblems(vid: vid)
+        self.section.problemCores?.forEach { problemCore in
+            self.calculateProblem(problemCore)
         }
     }
     
-    private func calculateProblems(vid: Int) {
-        guard let page = CoreUsecase.fetchPage(vid: vid) else { return }
-        page.problems.forEach { pid in
-            guard let problem = CoreUsecase.fetchProblem(pid: pid) else { return }
-            problem.setValue(true, forKey: "terminated")
+//    private func fetchVids() {
+//        let dumlicatedVids = section.dictionaryOfProblem.values.map() { $0 }
+//        self.vids = Array(Set(dumlicatedVids)).sorted(by: < )
+//    }
+//
+//    private func calculateSectionResult() {
+//        self.vids.forEach { vid in
+//            self.calculateProblems(vid: vid)
+//        }
+//    }
+    
+    private func calculateProblem(_ problem: Problem_Core) {
+        problem.setValue(true, forKey: "terminated")
+        
+        let submission = Submission(problem: problem)
+        self.submissions.append(submission)
+        
+        // 원점수 합산, 틀린문제 합산
+        if let correct = submission.correct,
+           let pName = problem.pName {
+            self.perfectScore += problem.point
+            self.wrongs.append(correct != 1)
             
-            let submission = Submission(problem: problem)
-            self.submissions.append(submission)
-            
-            // 원점수 합산, 틀린문제 합산
-            if let correct = submission.correct,
-               let pName = problem.pName {
-                self.perfectScore += problem.point
-                self.wrongs.append(correct != 1)
-                
-                if correct == 0 {
-                    self.wrongProblems.append(pName)
-                } else {
-                    self.totalScore += problem.point
-                }
+            if correct == 0 {
+                self.wrongProblems.append(pName)
             } else {
-                self.wrongs.append(false)
+                self.totalScore += problem.point
             }
+        } else {
+            self.wrongs.append(false)
         }
     }
 }
