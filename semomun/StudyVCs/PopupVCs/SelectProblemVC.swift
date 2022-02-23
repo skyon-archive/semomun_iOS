de76e2788d7bcdf86f64b47f17d5a7070afa8902
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-02-23 12:58:57 +0900

    [Refactor] StudyVCs 폴더 리펙토링

---
+++ semomun/StudyVCs/PopupVCs/SelectProblemVC.swift
@@ -0,0 +1,29 @@
+//
+//  SelectProblemVC.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2022/02/23.
+//
+
+import UIKit
+
+class SelectProblemVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
