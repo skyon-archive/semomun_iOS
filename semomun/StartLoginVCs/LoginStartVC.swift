45bfb3512ca214e29dfba9c3994382125cb7ee1e
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-02-23 11:58:25 +0900

    [UI] Login UI 수정 반영, 로그인시 animation 추가

--- semomun/StartLoginVCs/LoginStartVC.swift
+++ semomun/StartLoginVCs/LoginStartVC.swift
@@ -7,26 +7,63 @@
 
 import UIKit
 
-final class LoginStartVC: UIViewController {
+final class LoginStartVC: UIViewController, UINavigationControllerDelegate {
     static let identifier = "LoginStartVC"
     static let storyboardName = "StartLogin"
+    var isAnimation: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.delegate = self
     }
     
     @IBAction func login(_ sender: Any) {
         guard let nextVC = UIStoryboard(name: LoginSelectVC.storyboardName, bundle: nil).instantiateViewController(withIdentifier: LoginSelectVC.identifier) as? LoginSelectVC else { return }
         nextVC.configurePopup(isNeeded: false)
+        
+        self.isAnimation = true
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
     
     @IBAction func signin(_ sender: Any) {
         guard let nextVC = UIStoryboard(name: LoginSignupVC.storyboardName, bundle: nil).instantiateViewController(withIdentifier: LoginSignupVC.identifier) as? LoginSignupVC else { return }
+        
+        self.isAnimation = false
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
     
     @IBAction func cancel(_ sender: Any) {
         self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+}
+
+/// https://stackoverflow.com/questions/26569488/navigation-controller-custom-transition-animation
+
+extension LoginStartVC: UINavigationBarDelegate {
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        class FadeAnimation: NSObject, UIViewControllerAnimatedTransitioning {
+            func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+                return 0.3
+            }
+            
+            func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+                let toViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)
+                if let vc = toViewController {
+                    transitionContext.finalFrame(for: vc)
+                    transitionContext.containerView.addSubview(vc.view)
+                    vc.view.alpha = 0.0
+                    UIView.animate(withDuration: self.transitionDuration(using: transitionContext),
+                                   animations: {
+                        vc.view.alpha = 1.0
+                    },
+                                   completion: { finished in
+                        transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+                    })
+                } else {
+                    NSLog("Oops! Something went wrong! 'ToView' controller is nill")
+                }
+            }
+        }
+        return self.isAnimation == true ? FadeAnimation() : nil
     }
 }
