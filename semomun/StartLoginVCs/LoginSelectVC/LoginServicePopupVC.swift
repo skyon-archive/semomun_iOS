c01202529f4d39b0f47b61709ec606635a164e10
이성열 <type101315@gmail.com>
2022-02-24 15:11:46 +0900

    [Feat] LoginServicePopupVC에서 회원가입 버튼의 활성 유무에 따라 투명도를 조절

--- semomun/StartLoginVCs/LoginSelectVC/LoginServicePopupVC.swift
+++ semomun/StartLoginVCs/LoginSelectVC/LoginServicePopupVC.swift
@@ -16,16 +16,28 @@ class LoginServicePopupVC: UIViewController {
     
     @IBOutlet var checkButtons: [UIButton]!
     @IBOutlet var longTextButtons: [UIButton]!
+    @IBOutlet weak var registerButton: UIButton!
     
     private lazy var isChecked = [Bool](repeating: false, count: checkButtons.count)
-    private var action: (() -> ())?
     
+    private var action: (() -> ())?
     private var networkUsecase: LoginServicePopupNetworkUsecase? = NetworkUsecase(network: Network())
+    private var canRegister: Bool {
+        return isChecked[0] && isChecked[1]
+    }
+    private var normalCheckButtons: ArraySlice<UIButton> {
+        return checkButtons[0..<3]
+    }
+    private var agreeAllButton: UIButton {
+        return checkButtons[3]
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configureCheckButtons()
+        self.configureNormalButtons()
+        self.configureAgreeAllButton()
         self.configureLongTextButtons()
+        self.updateUIOfRegisterButton()
     }
     
     @IBAction func closeWindow(_ sender: Any) {
@@ -33,20 +45,16 @@ class LoginServicePopupVC: UIViewController {
     }
     
     @IBAction func continueRegister(_ sender: Any) {
-        let canSubmit = isChecked[0] && isChecked[1]
-        if canSubmit {
-            let marketingAgreed = isChecked[2]
-            self.networkUsecase?.postMarketingConsent(isConsent: marketingAgreed) { status in
-                switch status {
-                case .SUCCESS:
-                    self.dismiss(animated: true)
-                    self.action?()
-                default:
-                    self.showAlertWithOK(title: "네트워크 오류", text: "네트워크 연결 확인 후 다시 시도해주세요.")
-                }
+        guard self.canRegister else { return }
+        let marketingAgreed = isChecked[2]
+        self.networkUsecase?.postMarketingConsent(isConsent: marketingAgreed) { status in
+            switch status {
+            case .SUCCESS:
+                self.dismiss(animated: true)
+                self.action?()
+            default:
+                self.showAlertWithOK(title: "네트워크 오류", text: "네트워크 연결 확인 후 다시 시도해주세요.")
             }
-        } else {
-            self.showAlertWithOK(title: "필수 항목에 동의가 필요합니다.", text: "")
         }
     }
 }
@@ -58,51 +66,29 @@ extension LoginServicePopupVC {
     }
 }
 
-// MARK: Private configures
+// MARK: Configure buttons
 extension LoginServicePopupVC {
-    private func configureCheckButtons() {
-        self.configureNormalButtons()
-        self.configureAgreeAllButton()
-    }
-    
     private func configureNormalButtons() {
-        self.checkButtons[0..<3].forEach { button in
+        self.normalCheckButtons.forEach { button in
             let action = UIAction { [weak self] _ in
                 guard let self = self else { return }
                 self.isChecked[button.tag].toggle()
-                self.configureButtonUI(button)
                 let agreeAllButtonStatus = self.isChecked[0..<3].allSatisfy({$0})
                 self.isChecked[3] = agreeAllButtonStatus
-                let agreeAllButton = self.checkButtons[3]
-                self.configureButtonUI(agreeAllButton)
+                self.updateUIOfAllButtons()
             }
             button.addAction(action, for: .touchUpInside)
         }
     }
     
     private func configureAgreeAllButton() {
-        guard let agreeAllButton = self.checkButtons.last else { return }
         let action = UIAction { [weak self] _ in
-            guard let self = self else { return }
-            guard let agreeAllButtonState = self.isChecked.last else { return }
+            guard let self = self,
+            let agreeAllButtonState = self.isChecked.last else { return }
             self.isChecked = self.isChecked.map { _ in !agreeAllButtonState }
-            self.checkButtons.forEach {
-                self.configureButtonUI($0)
-            }
+            self.updateUIOfAllButtons()
         }
-        agreeAllButton.addAction(action, for: .touchUpInside)
-    }
-    
-    private func configureButtonUI(_ button: UIButton) {
-        let check = UIImage(.circleCheckmark)
-        let checkFilled = UIImage(.circleCheckmarkFilled) 
-        if self.isChecked[button.tag] {
-            button.setImage(checkFilled, for: .normal)
-            button.tintColor = UIColor(.mainColor)
-        } else {
-            button.setImage(check, for: .normal)
-            button.tintColor = UIColor(.grayDefaultColor)
-        }
+        self.agreeAllButton.addAction(action, for: .touchUpInside)
     }
     
     private func configureLongTextButtons() {
@@ -117,5 +103,31 @@ extension LoginServicePopupVC {
             }
             button.addAction(action, for: .touchUpInside)
         }
+    }
+}
+
+// MARK: Update button UI
+extension LoginServicePopupVC {
+    private func updateUIOfAllButtons() {
+        self.checkButtons.forEach {
+            self.updateUI(of: $0)
+        }
+        self.updateUIOfRegisterButton()
+    }
+    // isChecked를 버튼들에 반영
+    private func updateUI(of button: UIButton) {
+        let check = UIImage(.circleCheckmark)
+        let checkFilled = UIImage(.circleCheckmarkFilled) 
+        if self.isChecked[button.tag] {
+            button.setImage(checkFilled, for: .normal)
+            button.tintColor = UIColor(.mainColor)
+        } else {
+            button.setImage(check, for: .normal)
+            button.tintColor = UIColor(.grayDefaultColor)
+        }
+    }
+    
+    private func updateUIOfRegisterButton() {
+        self.registerButton.layer.opacity = self.canRegister ? 1 : 0.3
     }
 }
