922d417a4b371064b004524b3a34750f2f433d75
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-02-27 22:40:36 +0900

    [Refactor] HomeWorkbookCell: configureNetworkUsecase 추가

--- semomun/HomeSearchBookshelfVCs/Home/Cell/HomeWorkbookCell.swift
+++ semomun/HomeSearchBookshelfVCs/Home/Cell/HomeWorkbookCell.swift
@@ -12,6 +12,7 @@ class HomeWorkbookCell: UICollectionViewCell {
     static let identifier = "HomeWorkbookCell"
     @IBOutlet weak var bookcover: UIImageView!
     @IBOutlet weak var title: UILabel!
+    private var networkUsecase: S3ImageFetchable?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,16 +23,18 @@ class HomeWorkbookCell: UICollectionViewCell {
         self.bookcover.image = UIImage(.loadingBookcover)
     }
     
+    func configureNetworkUsecase(to usecase: S3ImageFetchable?) {
+        self.networkUsecase = usecase
+    }
+    
     func configure(with preview: PreviewOfDB) {
         self.title.text = preview.title
-        let networkUsecase = NetworkUsecase(network: Network())
-        networkUsecase.getImageFromS3(uuid: preview.bookcover, type: .bookcover) { [weak self] status, stringUrl in
+        self.networkUsecase?.getImageFromS3(uuid: preview.bookcover, type: .bookcover) { [weak self] status, stringUrl in
             DispatchQueue.main.async { [weak self] in
                 switch status {
                 case .SUCCESS:
                     guard let stringUrl = stringUrl,
                           let url = URL(string: stringUrl) else { return }
-                    print(stringUrl)
                     self?.bookcover.kf.setImage(with: url)
                 default:
                     print("get image Error")
