bb2768553a4b221688c877b7c76c1071d17bedef
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-02-28 00:55:28 +0900

    [Refactor] 리뷰 반영, 개인정보 수정 로직 버그로 인해 확인못한상태

--- semomun/StudyVCs/PopupVCs/SectionResult.swift
+++ semomun/StudyVCs/PopupVCs/SectionResult.swift
@@ -12,11 +12,4 @@ struct SectionResult {
     let score: Double
     let time: Int64
     let wrongProblems: [String]
-    
-    init(perfactScore: Double, score: Double, time: Int64, wrongProblems: [String]) {
-        self.time = time
-        self.perfectScore = perfactScore
-        self.score = score
-        self.wrongProblems = wrongProblems
-    }
 }
