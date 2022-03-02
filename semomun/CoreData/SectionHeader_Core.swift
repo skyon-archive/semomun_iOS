12b0ff8d96b7b15520f0477512dcea47e763d0e0
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-03-02 11:06:35 +0900

    [Refactor] WorkbookOfDB, SectionOfDB CodingKeys 적용 및 변수명 수정 반영

--- semomun/CoreData/SectionHeader_Core.swift
+++ semomun/CoreData/SectionHeader_Core.swift
@@ -56,14 +56,14 @@ public class SectionHeader_Core: NSManagedObject {
     func setValues(section: SectionOfDB) {
         self.setValue(Int64(section.wid), forKey: Attribute.wid.rawValue)
         self.setValue(Int64(section.sid), forKey: Attribute.sid.rawValue)
-        self.setValue(Int64(section.index), forKey: Attribute.index.rawValue)
+        self.setValue(Int64(section.sectionNum), forKey: Attribute.index.rawValue)
         self.setValue(section.title, forKey: Attribute.title.rawValue)
         self.setValue(section.detail, forKey: Attribute.detail.rawValue)
         self.setValue(nil, forKey: Attribute.cutoff.rawValue)
-        self.setValue(Int64(section.size), forKey: Attribute.size.rawValue)
+        self.setValue(Int64(section.fileSize), forKey: Attribute.size.rawValue)
         self.setValue(nil, forKey: Attribute.audio.rawValue) // TODO: 추후 Data 형식으로 가져오는 로직이 필요
         self.setValue(nil, forKey: Attribute.audioDetail.rawValue) // TODO: 추후 반영될 값
-        self.setValue(section.updatedAt, forKey: Attribute.updatedAt.rawValue)
+        self.setValue(section.updatedDate, forKey: Attribute.updatedAt.rawValue)
         self.setValue(false, forKey: Attribute.downloaded.rawValue)
         self.setValue(false, forKey: Attribute.terminated.rawValue)
         self.setValue(nil, forKey: Attribute.image.rawValue)
