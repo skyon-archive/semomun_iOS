3fcc7f696f52c0bc2810a19846172e922a45ab85
이성열 <type101315@gmail.com>
2022-03-03 23:50:44 +0900

    [Feat] NetworkTokens 추가

---
+++ semomun/Global/Secure/NetworkTokens.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkTokens.swift
+//  semomun
+//
+//  Created by SEONG YEOL YI on 2022/03/03.
+//
+
+import Foundation
+import Alamofire
+
+struct NetworkTokens: Decodable, Equatable {
+    let accessToken: String
+    let refreshToken: String
+
+    init?() {
+        let accessTokenKeyChain = KeychainItem(account: .accessToken)
+        let refreshTokenKeyChain = KeychainItem(account: .refreshToken)
+        do {
+            self.accessToken = try accessTokenKeyChain.readItem()
+            self.refreshToken = try refreshTokenKeyChain.readItem()
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+
+    func save() throws {
+        let accessTokenKeyChain = KeychainItem(account: .accessToken)
+        let refreshTokenKeyChain = KeychainItem(account: .refreshToken)
+        try accessTokenKeyChain.saveItem(self.accessToken)
+        try refreshTokenKeyChain.saveItem(self.refreshToken)
+    }
+}
