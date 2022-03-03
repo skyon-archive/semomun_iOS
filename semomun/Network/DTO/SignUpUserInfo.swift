cb7f425b00ec4a3a92a30ffe6fa76520cd4daa89
이성열 <type101315@gmail.com>
2022-03-03 23:51:26 +0900

    [Feat] SignUpUserInfo DTO 추가

---
+++ semomun/Network/DTO/SignUpUserInfo.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpUserInfo.swift
+//  semomun
+//
+//  Created by SEONG YEOL YI on 2022/03/03.
+//
+
+import Foundation
+
+struct SignUpParam: Encodable {
+    let info: SignUpUserInfo
+    let token: String
+    let type: String
+}
+
+struct SignUpUserInfo: Encodable {
+    let nickname: String
+    let phone: String
+    let school: String
+    let major: String
+    let majorDetail: String
+    let favoriteTags: [Int]
+    let graduationStatus: String
+}
