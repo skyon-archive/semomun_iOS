9a6a6043c17824cf230773d50105abd1ba2796dc
이성열 <type101315@gmail.com>
2022-03-03 23:52:07 +0900

    [Refactor] UserInfo 리팩터링

--- semomun/Network/DTO/UserInfo.swift
+++ semomun/Network/DTO/UserInfo.swift
@@ -7,22 +7,29 @@
 
 import Foundation
 
-class UserInfo: Codable, CustomStringConvertible {
-    var description: String {
-        return "User(\(optional: self.uid), \(optional: self.name), \(optional: self.nickName), \(optional: self.phoneNumberWithCountryCode), \(optional: self.favoriteCategory), \(optional: self.major), \(optional: self.majorDetail), \(optional: self.gender), \(optional: self.birthday), \(optional: self.school), \(optional: self.graduationStatus))"
+struct UserInfo: Codable {
+    enum CodingKeys: String, CodingKey {
+        case uid, name, nickname, email, gender, birth, major, majorDetail, school, graduationStatus, credit
+        case phoneNumberWithCountryCode = "phone"
+        case createdDate = "createdAt"
+        case updatedDate = "updatedAt"
     }
+    
+    var uid: Int
     var name: String?
-    var nickName: String?
-    private(set) var phoneNumberWithCountryCode: String?
-    var favoriteCategory: String?
+    var nickname: String?
+    var email: String?
+    var gender: String?
+    var birth: String?
     var major: String?
     var majorDetail: String?
-    var gender: String?
-    var birthday: String?
     var school: String?
     var graduationStatus: String?
-    var uid: String?
-    var profileImage: Data?
+    var credit: Int?
+    var createdDate: Date?
+    var updatedDate: Date?
+    
+    private(set) var phoneNumberWithCountryCode: String?
     
     var phoneNumber: String? {
         get {
@@ -30,83 +37,24 @@ class UserInfo: Codable, CustomStringConvertible {
             return self.phoneNumberWithCountryCode?.replacingOccurrences(of: "^\\+82-(\\d{1,2})-(\\d{4})-(\\d{4})$", with: "0$1$2$3", options: .regularExpression, range: nil)
         }
         set {
-            if let phoneNumberWithCountryCode = newValue?.phoneNumberWithCountryCode {
-                self.phoneNumberWithCountryCode = phoneNumberWithCountryCode
-            }
+            self.phoneNumberWithCountryCode = newValue?.phoneNumberWithCountryCode
         }
-    }
-    
-    func configureName(to name: String?) {
-        if let name = name {
-            self.name = name
-        } else {
-            self.name = self.nickName
-        }
-    }
-    
-    func configureNickname(to nickName: String?) {
-        if let nickName = nickName {
-            self.nickName = nickName
-        } else {
-            self.nickName = "User" + String.randomUserNumber
-        }
-    }
-    
-    func configurePhone(to phone: String?) {
-        if let phone = phone {
-            self.phoneNumberWithCountryCode = phone
-        } else {
-            self.phoneNumberWithCountryCode = String.randomPhoneNumber
-        }
-    }
-    
-    func configureGender(to gender: String) {
-        self.gender = gender
-    }
-    
-    func configureCategory(to category: String) {
-        self.favoriteCategory = category
-    }
-    
-    func configureMajor(to major: String) {
-        self.major = major
-    }
-    
-    func configureMajorDetail(to majorDetail: String?) {
-        self.majorDetail = majorDetail
-    }
-    
-    func configureBirthday(to birthday: String) {
-        self.birthday = birthday
-    }
-    
-    func configureSchool(to school: String) {
-        self.school = school
-    }
-    
-    func configureGraduation(to status: String) {
-        self.graduationStatus = status
-    }
-    
-    func configureUid(to uid: String) {
-        self.uid = uid
     }
     
     var isValidSurvay: Bool {
-        return [self.nickName, self.phoneNumberWithCountryCode, self.major, self.majorDetail, self.school, self.graduationStatus].allSatisfy {
+        return [self.nickname, self.phoneNumberWithCountryCode, self.major, self.majorDetail, self.school, self.graduationStatus].allSatisfy {
             $0 != nil && $0 != ""
         }
     }
     
-    func setValues(userInfo: UserCoreData) {
+    mutating func setValues(userInfo: UserCoreData) {
         self.name = userInfo.name
-        self.nickName = userInfo.nickName
-        self.phoneNumberWithCountryCode = userInfo.phoneNumber
-        self.favoriteCategory = userInfo.favoriteCategory
+        self.nickname = userInfo.nickName
+        self.phoneNumber = userInfo.phoneNumber
         self.major = userInfo.major
         self.majorDetail = userInfo.majorDetail
         self.gender = userInfo.gender
-        self.birthday = userInfo.birthday
+        self.birth = userInfo.birthday
         self.school = userInfo.schoolName
         self.graduationStatus = userInfo.graduationStatus
     }
