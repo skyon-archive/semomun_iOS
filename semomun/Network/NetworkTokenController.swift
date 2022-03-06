40f47c9e07cf168b3cc7a554b974b9eb1dc006a4
Yeolyi <type101315@gmail.com>
2022-03-06 18:24:38 +0900

    [Refactor] NetworkTokenController에 주석 추가

--- semomun/Network/NetworkTokenController.swift
+++ semomun/Network/NetworkTokenController.swift
@@ -9,7 +9,10 @@ import Foundation
 import Alamofire
 
 struct NetworkTokenController: RequestInterceptor {
+    
+    /// 네트워크 요청 전 urlRequest에 관한 처리를 가로채서 적용
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Swift.Result<URLRequest, Error>) -> Void) {
+        // TODO: 토큰값이 필요한 요청에 대해서만 토큰을 추가하도록 수정하기
         guard urlRequest.url?.absoluteString.hasPrefix(NetworkURL.base) == true,
               let authToken = try? KeychainItem(account: .accessToken).readItem() else {
                   completion(.success(urlRequest))
@@ -17,39 +20,46 @@ struct NetworkTokenController: RequestInterceptor {
               }
         var urlRequest = urlRequest
         urlRequest.setValue("Bearer " + authToken, forHTTPHeaderField: "Authorization")
+        
         completion(.success(urlRequest))
     }
+    
+    ///  네트워크 요청 결과로 Error가 발생한 경우, 이를 처리한 다음 Error가 발생한 요청를 재시도할지 판단
+    ///  - 401 status code가 반환되었을 때 토큰 재발급 시도
     func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
         guard let response = request.task?.response as? HTTPURLResponse, response.statusCode == 401 else {
             completion(.doNotRetry)
             return
         }
-        print("토큰 재발급 시작")
-        self.refreshToken { result in
+        
+        guard let token = NetworkTokens() else {
+            print("refresh에 사용 가능한 토큰값 없음")
+            completion(.doNotRetry)
+            return
+        }
+        
+        self.requestTokenRefresh(using: token) { result in
             switch result.result {
             case .success(let token):
                 do {
                     try token.save()
-                    dump(token)
-                    print("토큰 재발급 완료")
+                    print("토큰 재발급 완료: \(token)")
                     completion(.retry)
                 } catch {
-                    print(error)
+                    print("토큰 저장 실패: \(error)")
                     completion(.doNotRetryWithError(error))
                 }
             case .failure(let error):
-                print(error)
+                print("토큰 재발급 실패: \(error)")
                 completion(.doNotRetryWithError(error))
             }
         }
     }
-    private func refreshToken(completion: @escaping (DataResponse<NetworkTokens, AFError>) -> Void) {
-        guard let token = NetworkTokens() else {
-            print("refresh에 사용 가능한 토큰값 없음")
-            return
-        }
-        print(token)
-        let headers: HTTPHeaders = [.authorization(bearerToken: token.accessToken), .refresh(token: token.refreshToken)]
+    
+    /// 전달받은 토큰값을 사용해 토큰 갱신을 서버로 요청
+    private func requestTokenRefresh(using networkTokens: NetworkTokens, completion: @escaping (DataResponse<NetworkTokens, AFError>) -> Void) {
+        let headers: HTTPHeaders = [.authorization(bearerToken: networkTokens.accessToken), .refresh(token: networkTokens.refreshToken)]
+        
         AF.request(NetworkURL.refreshToken, method: .get, headers: headers) { $0.timeoutInterval = .infinity }
         .responseDecodable(of: NetworkTokens.self) { requestResult in
             completion(requestResult)
