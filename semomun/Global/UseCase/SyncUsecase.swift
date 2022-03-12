bd0ee5d0538dddcc69a243f690d135d72c2f7819
이성열 <type101315@gmail.com>
2022-03-12 15:11:27 +0900

    [Fix] 리뷰 반영

--- semomun/Global/UseCase/SyncUsecase.swift
+++ semomun/Global/UseCase/SyncUsecase.swift
@@ -17,6 +17,31 @@ struct SyncUsecase {
     
     static private let networkUsecase: SyncFetchable = NetworkUsecase(network: Network())
     
+    // MARK: AppDelegate 에서 1.0 -> 2.0 업데이트 로직
+    static func getTokensForPastVersionUser(networkUsecase: LoginSignupPostable, completion: @escaping (Bool) -> Void) {
+        do {
+            let tokenString = try KeychainItem(account: .userIdentifier).readItem()
+            let userToken = NetworkURL.UserIDToken.apple(tokenString)
+            
+            // TODO: 타입 필요 없는 API로 대체하기
+            networkUsecase.postLogin(userToken: userToken) { status, userNotExist in
+                guard userNotExist == false else {
+                    assertionFailure()
+                    completion(false)
+                    return
+                }
+                switch status {
+                case .SUCCESS:
+                    completion(true)
+                default:
+                    completion(false)
+                }
+            }
+        } catch {
+            print("Auth/Refresh 토큰 발급 실패: \(error)")
+        }
+    }
+    
     static func syncUserDataFromDB(completion: @escaping (Result<UserInfo, SyncError>) -> Void) {
         self.networkUsecase.getUserInfo { status, userInfo in
             switch status {
