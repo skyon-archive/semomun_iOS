d6176602c6929d853e71e04017f4edccac62ba93
이성열 <type101315@gmail.com>
2022-03-15 21:44:02 +0900

    [Feat] 프로필 화면에 사용자 이름 정보 반영

--- semomun/ProfileVCs/ProfileVC/ProfileVC.swift
+++ semomun/ProfileVCs/ProfileVC/ProfileVC.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 final class ProfileVC: UIViewController {
-    
     @IBOutlet weak var navigationTitleView: UIView!
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
@@ -18,7 +17,15 @@ final class ProfileVC: UIViewController {
     @IBOutlet weak var nameToCircleLeading: NSLayoutConstraint!
     @IBOutlet weak var containerView: UIView!
     
-    private var isLogin = UserDefaultsManager.get(forKey: .logined) as? Bool ?? false
+    private var isLoginUI = false {
+        didSet {
+            if isLoginUI == true {
+                self.configureUIForLogined()
+            } else {
+                self.configureUIForNotLogined()
+            }
+        }
+    }
     
     private lazy var tableViewBeforeLogin: UIViewController = {
         let storyboard = UIStoryboard(name: UnloginedProfileTableVC.storyboardName, bundle: nil)
@@ -30,16 +37,11 @@ final class ProfileVC: UIViewController {
         return storyboard.instantiateViewController(withIdentifier: LoginedProfileTableVC.identifier)
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.configureUI()
-        self.configureTableView()
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(true, animated: true)
-        self.updateIfLoginStatusChanged()
+        self.isLoginUI = UserDefaultsManager.get(forKey: .logined) as? Bool ?? false
+        self.configureUserInfoUI()
     }
 
     @IBAction func openChangeAccountInfoView(_ sender: Any) {
@@ -61,39 +63,42 @@ final class ProfileVC: UIViewController {
 }
 
 extension ProfileVC {
+    private func configureUIForLogined() {
+        self.nameToCircleLeading.constant = 60
+        self.changeUserInfoButton.isHidden = false
+        self.loginButton.isHidden = true
+        self.profileImage.isHidden = false
+        self.configureTableView()
+    }
+    
+    private func configureUIForNotLogined() {
+        self.nameToCircleLeading.constant = 0
+        self.changeUserInfoButton.isHidden = true
+        self.loginButton.isHidden = false
+        self.profileImage.isHidden = true
+        self.configureTableView()
+    }
+    
     private func configureTableView() {
-        let toRemove = self.isLogin ? tableViewBeforeLogin : tableViewAfterLogin
+        let toRemove = self.isLoginUI ? tableViewBeforeLogin : tableViewAfterLogin
         toRemove.willMove(toParent: nil)
         toRemove.view.removeFromSuperview()
         toRemove.removeFromParent()
         
-        let target = self.isLogin ? tableViewAfterLogin : tableViewBeforeLogin
+        let target = self.isLoginUI ? tableViewAfterLogin : tableViewBeforeLogin
         target.view.frame = self.containerView.bounds
         self.containerView.addSubview(target.view)
         self.addChild(target)
         target.didMove(toParent: self)
     }
     
-    private func configureUI() {
-        self.nameToCircleLeading.constant = isLogin ? 60 : 0
-        self.changeUserInfoButton.isHidden = !isLogin
-        self.loginButton.isHidden = isLogin
-        self.profileImage.isHidden = !isLogin
-        if isLogin {
+    private func configureUserInfoUI() {
+        if isLoginUI {
             guard let userInfo = CoreUsecase.fetchUserInfo() else { return }
-            self.name.text = userInfo.name
+            self.name.text = userInfo.nickName
         } else {
             self.name.text = "로그인이 필요합니다"
             self.name.frame = self.name.frame.offsetBy(dx: -41, dy: 0)
-        }
-    }
-    
-    private func updateIfLoginStatusChanged() {
-        guard let isLoginNow = UserDefaultsManager.get(forKey: .logined) as? Bool else { return }
-        if self.isLogin != isLoginNow {
-            self.isLogin = isLoginNow
-            self.configureUI()
-            self.configureTableView()
         }
     }
 }
