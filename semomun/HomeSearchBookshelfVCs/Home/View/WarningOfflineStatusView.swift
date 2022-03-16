5e177e0a3162de9870ef40988b8543bc012b6b4c
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-03-16 14:00:13 +0900

    [Refactor] WarningOfflineStatusView 최종디자인 반영

--- semomun/HomeSearchBookshelfVCs/Home/View/WarningOfflineStatusView.swift
+++ semomun/HomeSearchBookshelfVCs/Home/View/WarningOfflineStatusView.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 final class WarningOfflineStatusView: UIView {
     private lazy var iconImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = UIImage(.offlineAlert)
+        imageView.image = UIImage(.cloud)
         return imageView
     }()
     private lazy var warningTitleLabel: UILabel = {
@@ -29,7 +30,16 @@ final class WarningOfflineStatusView: UIView {
         label.textColor = .black
         label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
         label.textAlignment = .center
-        label.text = "인터넷 연결을 확인한 후 다시 시도해 주세요."
+        label.numberOfLines = 2
+        
+        let string = "인터넷 연결을 확인한 후 다시 시도해 주세요.\n<책장>에서 구매 후 다운로드 받으신 콘텐츠는 이용 가능합니다."
+        let attributedString = NSMutableAttributedString(string: string)
+        var style = NSMutableParagraphStyle()
+        style.lineSpacing = 5
+        style.alignment = .center
+        attributedString.addAttribute(.paragraphStyle, value: style, range: NSRange(location: 0, length: string.count))
+        label.attributedText = attributedString
+        
         return label
     }()
     private lazy var refreshButton: UIButton = {
@@ -38,13 +48,28 @@ final class WarningOfflineStatusView: UIView {
         button.clipsToBounds = true
         button.layer.cornerRadius = 3
         button.backgroundColor = UIColor(.mainColor)
-        button.setTitle("  새로고침", for: .normal)
+        button.setTitle(" 다시시도", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
         button.setImage(UIImage(.refresh), for: .normal)
         
         button.addAction(UIAction(handler: { _ in
             NotificationCenter.default.post(name: .checkHomeNetworkFetchable, object: nil)
+        }), for: .touchUpInside)
+        return button
+    }()
+    private lazy var goBookshelfButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 3
+        button.backgroundColor = UIColor(.mainColor)
+        button.setTitle("책장으로 가기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        
+        button.addAction(UIAction(handler: { _ in
+            NotificationCenter.default.post(name: .refreshBookshelf, object: nil)
         }), for: .touchUpInside)
         return button
     }()
@@ -67,39 +92,43 @@ final class WarningOfflineStatusView: UIView {
             frameView.centerYAnchor.constraint(equalTo: self.centerYAnchor)
         ]
         
-        frameView.addSubviews(self.iconImageView, self.warningTitleLabel, self.warningTextLabel, self.refreshButton)
+        let buttons = UIStackView(arrangedSubviews: [self.refreshButton, self.goBookshelfButton])
+        buttons.distribution = .fillEqually
+        buttons.spacing = 10
+        frameView.addSubviews(self.iconImageView, self.warningTitleLabel, self.warningTextLabel, buttons)
         
         let iconImageViewLayouts = [
             self.iconImageView.topAnchor.constraint(equalTo: frameView.topAnchor, constant: 5),
             self.iconImageView.centerXAnchor.constraint(equalTo: frameView.centerXAnchor),
-            self.iconImageView.widthAnchor.constraint(equalToConstant: 74),
-            self.iconImageView.heightAnchor.constraint(equalToConstant: 38)
+            self.iconImageView.widthAnchor.constraint(equalToConstant: 96),
+            self.iconImageView.heightAnchor.constraint(equalToConstant: 58)
         ]
         
         let warningTitleLabelLayouts = [
-            self.warningTitleLabel.topAnchor.constraint(equalTo: self.iconImageView.bottomAnchor, constant: 23),
+            self.warningTitleLabel.topAnchor.constraint(equalTo: self.iconImageView.bottomAnchor, constant: 20),
             self.warningTitleLabel.centerXAnchor.constraint(equalTo: frameView.centerXAnchor)
         ]
         
         let warningTextLabelLayouts = [
-            self.warningTextLabel.topAnchor.constraint(equalTo: self.warningTitleLabel.bottomAnchor, constant: 6),
+            self.warningTextLabel.topAnchor.constraint(equalTo: self.warningTitleLabel.bottomAnchor, constant: 8),
             self.warningTextLabel.centerXAnchor.constraint(equalTo: frameView.centerXAnchor),
             self.warningTextLabel.leadingAnchor.constraint(equalTo: frameView.leadingAnchor, constant: 5),
             self.warningTextLabel.trailingAnchor.constraint(equalTo: frameView.trailingAnchor, constant: -5)
         ]
         
-        let refreshButtonLayouts = [
-            self.refreshButton.topAnchor.constraint(equalTo: self.warningTextLabel.bottomAnchor, constant: 31),
-            self.refreshButton.centerXAnchor.constraint(equalTo: frameView.centerXAnchor),
-            self.refreshButton.widthAnchor.constraint(equalToConstant: 110),
-            self.refreshButton.heightAnchor.constraint(equalToConstant: 33),
-            self.refreshButton.bottomAnchor.constraint(equalTo: frameView.bottomAnchor, constant: -5)
+        buttons.translatesAutoresizingMaskIntoConstraints = false
+        let buttonsLayout = [
+            buttons.topAnchor.constraint(equalTo: self.warningTextLabel.bottomAnchor, constant: 31),
+            buttons.centerXAnchor.constraint(equalTo: frameView.centerXAnchor),
+            buttons.widthAnchor.constraint(equalToConstant: 244),
+            buttons.heightAnchor.constraint(equalToConstant: 33),
+            buttons.bottomAnchor.constraint(equalTo: frameView.bottomAnchor, constant: -5)
         ]
-    
+        
         self.layoutConstraintActivates(frameViewLayouts,
                                        iconImageViewLayouts,
                                        warningTitleLabelLayouts,
                                        warningTextLabelLayouts,
-                                       refreshButtonLayouts)
+                                       buttonsLayout)
     }
 }
