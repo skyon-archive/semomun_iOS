5a86aec7a2e5c89979564dea8eef8759afd2871c
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-03-21 17:24:06 +0900

    [Refactor] 결제 -> 구매로 용어 수정, WorkbookDetailVC UI 반영

--- semomun/HomeSearchBookshelfVCs/Purchase/PurchaseWarningPopupVC.swift
+++ semomun/HomeSearchBookshelfVCs/Purchase/PurchaseWarningPopupVC.swift
@@ -54,11 +54,11 @@ extension PurchaseWarningPopupVC {
         switch type {
         case .login:
             self.configureTitle(to: "회원가입 또는 로그인이 필요합니다")
-            self.configureText(to: "문제집을 결제하기 위해서 로그인을 해주세요.")
+            self.configureText(to: "문제집을 구매하기 위해서 로그인을 해주세요.")
             self.configureActionTitle(to: "로그인 하기")
         case .updateUserinfo:
             self.configureTitle(to: "추가적인 정보가 필요합니다")
-            self.configureText(to: "결제를 진행하기 위해 닉네임, 전화번호를 수정해주세요.")
+            self.configureText(to: "구매를 진행하기 위해 닉네임, 전화번호를 수정해주세요.")
             self.configureActionTitle(to: "추가정보 작성하기")
         }
     }
