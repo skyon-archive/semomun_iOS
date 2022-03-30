d6735fcafb1e40edc63597e1f541f08ab62a5521
이성열 <type101315@gmail.com>
2022-03-30 21:45:51 +0900

    [Refactor] MyPurchasesVMTest 리팩터링

--- SemomunTests/ViewModels/PayHistoryVMTest.swift
+++ SemomunTests/ViewModels/PayHistoryVMTest.swift
@@ -25,15 +25,8 @@ class MyPurchasesVMTest: XCTestCase {
     
     func testInitPublished() {
         self.vm.initPublished()
+        self.checkMonthGroupValidity(group: self.vm.purchaseOfEachMonth, expectedSize: 25)
         XCTAssertEqual(vm.remainingSemopay, networkUsecase.remainingSemopay)
-        
-        let dataGroupedByVM = vm.purchaseOfEachMonth
-        let contentCount = dataGroupedByVM.reduce(0, { $0 + $1.content.count })
-        XCTAssertEqual(contentCount, networkUsecase.pageSize)
-        
-        for payHistory in networkUsecase.purchaseHistories[0..<networkUsecase.pageSize] {
-            XCTAssert(groupContainsItem(group: dataGroupedByVM, payHistoryOfDB: payHistory))
-        }
     }
     
     func testPagination() {
@@ -42,48 +35,46 @@ class MyPurchasesVMTest: XCTestCase {
         
         // Page 2 (25~49)
         self.vm.tryFetchMoreList()
-        var dataGroupedByVM = self.vm.purchaseOfEachMonth
+        self.checkMonthGroupValidity(group: self.vm.purchaseOfEachMonth, expectedSize: 50)
+        XCTAssertEqual(vm.remainingSemopay, networkUsecase.remainingSemopay)
         
+        // Page 3 (50~55)
+        self.vm.tryFetchMoreList()
+        self.checkMonthGroupValidity(group: self.vm.purchaseOfEachMonth, expectedSize: 55)
+        XCTAssertEqual(vm.remainingSemopay, networkUsecase.remainingSemopay)
+    }
+    
+    /// VM이 DB에서 전달받은 값을 월별로 올바르게 묶었는지 확인
+    private func checkMonthGroupValidity(group: [(section: String, content: [PurchasedItem])], expectedSize: Int) {
         // 정렬 확인
-        dataGroupedByVM.forEach { _, content in
+        group.forEach { _, content in
             XCTAssert(content == content.sorted(by: { $0.createdDate > $1.createdDate }))
         }
         
         // 개수 확인
-        var contentCount = dataGroupedByVM.reduce(0, { $0 + $1.content.count })
-        XCTAssertEqual(contentCount, 50)
+        let contentCount = group.reduce(0, { $0 + $1.content.count })
+        XCTAssertEqual(contentCount, expectedSize)
         
-        // DB값과 매칭여부 확인
-        for payHistory in networkUsecase.purchaseHistories[0..<50] {
-            XCTAssert(groupContainsItem(group: dataGroupedByVM, payHistoryOfDB: payHistory))
-        }
-        
-        // Page 3 (50~55)
-        self.vm.tryFetchMoreList()
-        dataGroupedByVM = self.vm.purchaseOfEachMonth
-        
-        dataGroupedByVM.forEach { _, content in
-            XCTAssert(content == content.sorted(by: { $0.createdDate > $1.createdDate }))
-        }
-        
-        contentCount = dataGroupedByVM.reduce(0, { $0 + $1.content.count })
-        XCTAssertEqual(contentCount, 55)
-        
-        for payHistory in networkUsecase.purchaseHistories[0..<55] {
-            XCTAssert(groupContainsItem(group: dataGroupedByVM, payHistoryOfDB: payHistory))
+        // DB값과 매칭 여부 확인
+        for payHistory in networkUsecase.purchaseHistories[0..<expectedSize] {
+            self.checkGroupContainsItem(group: group, payHistoryOfDB: payHistory)
         }
     }
     
-    private func groupContainsItem(group: [(section: String, content: [PurchasedItem])], payHistoryOfDB: PayHistoryofDB) -> Bool {
+    /// VM이 월별로 묶은 자료구조에 특정 PayHistoryofDB의 존재유무 확인
+    private func checkGroupContainsItem(group: [(section: String, content: [PurchasedItem])], payHistoryOfDB: PayHistoryofDB) {
         guard let section = group.first(where: { $0.section == payHistoryOfDB.createdDate.yearMonthText }) else {
-            return false
+            XCTFail()
+            return
         }
-        return section.content.contains(where: { purchasedItem in
-            purchasedItem.createdDate == payHistoryOfDB.createdDate &&
-            purchasedItem.descriptionImageID == payHistoryOfDB.item.workbook.bookcover &&
-            purchasedItem.title == payHistoryOfDB.item.workbook.title &&
-            purchasedItem.transaction == Transaction(amount: payHistoryOfDB.amount)
-        })
+        XCTAssert(
+            section.content.contains(where: { purchasedItem in
+                purchasedItem.createdDate == payHistoryOfDB.createdDate &&
+                purchasedItem.descriptionImageID == payHistoryOfDB.item.workbook.bookcover &&
+                purchasedItem.title == payHistoryOfDB.item.workbook.title &&
+                purchasedItem.transaction == Transaction(amount: payHistoryOfDB.amount)
+            })
+        )
     }
 }
 
