c1a65e028db976697f922d9c75ccfe9a226fbd42
이성열 <type101315@gmail.com>
2022-03-30 21:45:51 +0900

    [Refactor] MyPurchasesVMTest 리팩터링

--- SemomunTests/Network/MockPayNetworkUsecase.swift
+++ SemomunTests/Network/MockPayNetworkUsecase.swift
@@ -10,23 +10,24 @@ import Foundation
 
 /// 짝수번째 인덱스는 충전, 홀수번째는 구매인 PayHistory를 반환
 struct MockPayNetworkUsecase: PayNetworkUsecase {
-    let pageSize: Int
     let payHistoryCount: Int
+    let pageSize: Int
     let remainingSemopay = 123456789
     
     let wholePayHistories: [PayHistoryofDB]
     let purchaseHistories: [PayHistoryofDB]
     
     init(payHistoryCount: Int, pageSize: Int = 25) {
-        self.pageSize = pageSize
         self.payHistoryCount = payHistoryCount
+        self.pageSize = pageSize
         
         self.wholePayHistories = (0..<payHistoryCount).map { index in
             let purchaseWorkbookOfDB = PurchasedWorkbookofDB(title: "\(index)", bookcover: UUID())
             let purchasedItemOfDB = PurchasedItemofDB(workbook: purchaseWorkbookOfDB)
             
-            let date = Date().addingTimeInterval(TimeInterval(-1 * 60 * 60 * 24 * index))
-            let amount = index % 2 == 0 ? 10000 : -10000
+            let date = Date().addingTimeInterval(-1 * 60 * 60 * 24 * TimeInterval(index))
+            let amount = index.isMultiple(of: 2) ? 10000 : -10000
+            
             return PayHistoryofDB(item: purchasedItemOfDB, createdDate: date, amount: amount)
         }
         
@@ -36,19 +37,20 @@ struct MockPayNetworkUsecase: PayNetworkUsecase {
     func getPayHistory(onlyPurchaseHistory: Bool, page: Int, completion: @escaping (NetworkStatus, PayHistory?) -> Void) {
         let targetHistory = onlyPurchaseHistory ? self.purchaseHistories : self.wholePayHistories
         
-        let requestedRange = (page-1)*pageSize..<page*pageSize
-        let actualRange = 0..<targetHistory.count
+        let requestedRange = (page-1)*self.pageSize..<page*self.pageSize
+        let availableRange = 0..<targetHistory.count
         
-        if requestedRange.overlaps(actualRange) == false {
+        guard requestedRange.overlaps(availableRange) else {
             completion(.SUCCESS, nil)
-        } else {
-            let overlappedRange = requestedRange.clamped(to: actualRange)
-            let data = Array(targetHistory[overlappedRange])
-            
-            let payHistoryGroupOfDB = PayHistoryGroupOfDB(count: payHistoryCount, content: data)
-            let payHistory = PayHistory(networkDTO: payHistoryGroupOfDB)
-            completion(.SUCCESS, payHistory)
+            return
         }
+
+        let overlappedRange = requestedRange.clamped(to: availableRange)
+        let payHistoryOfDBInRange = Array(targetHistory[overlappedRange])
+        
+        let payHistoryGroupOfDB = PayHistoryGroupOfDB(count: payHistoryCount, content: payHistoryOfDBInRange)
+        let payHistory = PayHistory(networkDTO: payHistoryGroupOfDB)
+        completion(.SUCCESS, payHistory)
     }
     
     func getUserInfo(completion: @escaping (NetworkStatus, UserInfo?) -> Void) {
