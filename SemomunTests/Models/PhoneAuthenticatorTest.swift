82455f1380f3678e9b24ecc295424576383ad682
이성열 <type101315@gmail.com>
2022-04-01 21:13:35 +0900

    [Feat] PhoneAuthenticatorTest 추가

--- SemomunTests/Models/PhoneAuthenticatorTest.swift
+++ SemomunTests/Models/PhoneAuthenticatorTest.swift
@@ -6,30 +6,117 @@
 //
 
 import XCTest
+@testable import semomun
 
-class PhoneAuthenticatorTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+final class PhoneAuthenticatorTest: XCTestCase {
+    
+    private var networkUsecase: MockPhoneNumVerifiable!
+    private var phoneAuthenticator: PhoneAuthenticator!
+    
+    private let testPhoneNumber = "01012345678"
+    private let testPhoneNumberWithCountryCode = "+82-10-1234-5678"
+    
+    override func setUp() {
+        self.networkUsecase = MockPhoneNumVerifiable()
+        self.phoneAuthenticator = PhoneAuthenticator(networkUsecase: self.networkUsecase)
     }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+// sendSMSCode tests
+extension PhoneAuthenticatorTest {
+    func testNoNetworkSend() {
+        self.networkUsecase.networkStatus = .FAIL
+        self.phoneAuthenticator.sendSMSCode(to: testPhoneNumber) { result in
+            XCTAssertEqual(result, .failure(.noNetwork))
         }
     }
+    
+    func testWrongFormatSend() {
+        self.phoneAuthenticator.sendSMSCode(to: self.testPhoneNumberWithCountryCode) { result in
+            XCTAssertEqual(result, .failure(.invalidPhoneNumber))
+            XCTAssertNil(self.networkUsecase.inputParameter)
+        }
+    }
+    
+    func testToManyRequestOnSend() {
+        self.networkUsecase.networkStatus = .TOOMANYREQUESTS
+        self.phoneAuthenticator.sendSMSCode(to: self.testPhoneNumber) { result in
+            XCTAssertEqual(result, .failure(.smsSentTooMuch))
+        }
+    }
+    
+    func testSuccessfulSend() {
+        self.phoneAuthenticator.sendSMSCode(to: testPhoneNumber) { result in
+            XCTAssertEqual(result, .success(self.testPhoneNumber))
+            XCTAssertEqual(
+                self.networkUsecase.inputParameter,
+                self.testPhoneNumberWithCountryCode
+            )
+        }
+    }
+}
 
+// resendSMSCode tests
+extension PhoneAuthenticatorTest {
+    func testCodeResendNoNetwork() {
+        self.phoneAuthenticator.sendSMSCode(to: self.testPhoneNumber) { _ in
+            self.networkUsecase.networkStatus = .FAIL
+            self.phoneAuthenticator.resendSMSCode { result in
+                XCTAssertEqual(result, .failure(.noNetwork))
+            }
+        }
+    }
+    
+    func testToManyRequestOnResend() {
+        self.phoneAuthenticator.sendSMSCode(to: self.testPhoneNumber) { _ in
+            self.networkUsecase.networkStatus = .TOOMANYREQUESTS
+            self.phoneAuthenticator.resendSMSCode { result in
+                XCTAssertEqual(result, .failure(.smsSentTooMuch))
+            }
+        }
+    }
+    
+    func testSuccessfulResend() {
+        self.phoneAuthenticator.sendSMSCode(to: self.testPhoneNumber) { _ in
+            self.phoneAuthenticator.resendSMSCode { result in
+                XCTAssertEqual(result, .success(self.testPhoneNumber))
+            }
+        }
+    }
+}
+
+// verifySMSCode tests
+extension PhoneAuthenticatorTest {
+    func testNoNetworkVerify() {
+        self.phoneAuthenticator.sendSMSCode(to: self.testPhoneNumber) { _ in
+            self.networkUsecase.networkStatus = .FAIL
+            self.phoneAuthenticator.verifySMSCode(self.networkUsecase.testAuthCode) { result in
+                XCTAssertEqual(result, .failure(.noNetwork))
+            }
+        }
+    }
+    
+    func testCodeNotSent() {
+        self.phoneAuthenticator.verifySMSCode(self.networkUsecase.testAuthCode) { result in
+            XCTAssertEqual(result, .failure(.codeNotSent))
+        }
+    }
+    
+    func testWrongCodeVerify() {
+        self.phoneAuthenticator.sendSMSCode(to: self.testPhoneNumber) { _ in
+            self.phoneAuthenticator.verifySMSCode("000000") { result in
+                XCTAssertEqual(result, .failure(.wrongCode))
+            }
+        }
+    }
+    
+    func testVerifyAfterResend() {
+        self.phoneAuthenticator.sendSMSCode(to: self.testPhoneNumber) { _ in
+            self.phoneAuthenticator.resendSMSCode { _ in
+                self.phoneAuthenticator.verifySMSCode(self.networkUsecase.testAuthCode) { result in
+                    XCTAssertEqual(result, .success(self.testPhoneNumber))
+                }
+            }
+        }
+    }
 }
