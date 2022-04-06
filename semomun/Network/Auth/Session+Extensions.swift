f0df03d7e516bda11ed7afecc499451c4ee82d14
이성열 <type101315@gmail.com>
2022-04-07 00:26:29 +0900

    [Feat] sessionWithToken을 Network 객체들이 공유하도록 수정

---
+++ semomun/Network/Auth/Session+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  AuthenticatorInterceptor.swift
+//  semomun
+//
+//  Created by SEONG YEOL YI on 2022/04/07.
+//
+
+import Alamofire
+
+extension Session {
+    static var sessionWithToken: Session = {
+        guard let token = NetworkTokens() else {
+            assertionFailure()
+            return Session()
+        }
+        
+        let credential = OAuthCredential(accessToken: token.accessToken, refreshToken: token.refreshToken)
+        let authenticator = OAuthAuthenticator()
+        let interceptor = AuthenticationInterceptor(authenticator: authenticator, credential: credential)
+        
+        return Session(interceptor: interceptor)
+    }()
+}
