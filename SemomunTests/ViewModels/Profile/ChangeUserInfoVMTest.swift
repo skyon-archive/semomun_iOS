3bd8237d1bb268ab5bdb61685da7f34c840325cf
이성열 <type101315@gmail.com>
2022-04-06 20:49:38 +0900

    [Feat] MockUserInfoVMNetwork 추가

---
+++ SemomunTests/ViewModels/Profile/ChangeUserInfoVMTest.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeUserInfoVMTest.swift
+//  semomunTests
+//
+//  Created by SEONG YEOL YI on 2022/04/06.
+//
+
+@testable import semomun
+import XCTest
+
+class ChangeUserInfoVMTest: XCTest {
+    
+    
+    override func setUp() {
+        <#code#>
+    }
+}
