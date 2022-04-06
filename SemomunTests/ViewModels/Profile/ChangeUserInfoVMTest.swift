f3ec24a118647aa6788e6fda734a7c462df4eb32
이성열 <type101315@gmail.com>
2022-04-06 21:14:35 +0900

    [Feat] ChangeUserInfoVMTest 일부 구현

--- SemomunTests/ViewModels/Profile/ChangeUserInfoVMTest.swift
+++ SemomunTests/ViewModels/Profile/ChangeUserInfoVMTest.swift
@@ -9,9 +9,39 @@
 import XCTest
 
 class ChangeUserInfoVMTest: XCTest {
-    
+    let networkUsecase = MockUserInfoVMNetwork()
+    var vm: ChangeUserInfoVM!
     
     override func setUp() {
-        <#code#>
+        self.vm = ChangeUserInfoVM(networkUseCase: self.networkUsecase)
+    }
+    
+    func testInitialState() {
+        XCTAssertEqual(vm.status, nil)
+        XCTAssertEqual(vm.alert, nil)
+        XCTAssertEqual(vm.userInfo, nil)
+        XCTAssertEqual(vm.majors, [])
+        XCTAssertEqual(vm.majorDetails, [])
+        XCTAssertEqual(vm.configureUIForNicknamePhoneRequest, false)
+    }
+}
+
+extension LoginSignupAlert: Equatable {
+    public static func == (lhs: LoginSignupAlert, rhs: LoginSignupAlert) -> Bool {
+        switch (lhs, rhs) {
+        case let (.alertWithPop(t1, d1), .alertWithPop(t2, d2)):
+            return t1==t2 && d1==d2
+        case let (.alertWithoutPop(t1, d1), .alertWithoutPop(t2, d2)):
+            return t1==t2 && d1==d2
+        default:
+            return false
+        }
+    }
+}
+
+// nil과의 비교만 필요해 임시 구현
+extension UserInfo: Equatable {
+    public static func == (lhs: UserInfo, rhs: UserInfo) -> Bool {
+        return false
     }
 }
