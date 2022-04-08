8575bc5e0b2290762b5e8da28a597ab013c2c438
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-04-08 18:37:34 +0900

    [Refactor] Bookshelf 새디자인에 맞게 cell개수 및 위치 계산, UI 반영

--- semomun/HomeSearchBookshelfVCs/BookshelfVC/Cell/BookshelfCell.swift
+++ semomun/HomeSearchBookshelfVCs/BookshelfVC/Cell/BookshelfCell.swift
@@ -9,38 +9,22 @@ import UIKit
 
 class BookshelfCell: UICollectionViewCell {
     static let identifier = "BookshelfCell"
-    @IBOutlet weak var frameView: UIView!
+    @IBOutlet weak var bookcoverFrameView: UIView!
     @IBOutlet weak var bookcover: UIImageView!
     @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var authorAndPublisher: UILabel!
     @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var progressPercentLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.configureUI()
-        self.resetUI()
+        self.configureBookcoverShadow()
     }
     
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        self.resetUI()
-    }
-    
-    private func configureUI() {
-        self.frameView.layer.shadowOpacity = 0.3
-        self.frameView.layer.shadowColor = UIColor.lightGray.cgColor
-        self.frameView.layer.shadowOffset = CGSize(width: 0, height: 0)
-        self.frameView.layer.shadowRadius = 5
-    }
-    
-    private func resetUI() {
-        self.bookcover.image = nil
-        self.title.text = nil
-        self.authorAndPublisher.text = nil
-        self.progressPercentLabel.text = nil
-        self.progressView.isHidden = false
-        self.progressPercentLabel.text = nil
+    private func configureBookcoverShadow() {
+        self.bookcoverFrameView.layer.shadowOpacity = 0.25
+        self.bookcoverFrameView.layer.shadowColor = UIColor.lightGray.cgColor
+        self.bookcoverFrameView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.bookcoverFrameView.layer.shadowRadius = 5
     }
     
     func configure(with book: Preview_Core) {
@@ -48,15 +32,8 @@ class BookshelfCell: UICollectionViewCell {
         if let imageData = book.image {
             self.bookcover.image = UIImage(data: imageData)
         }
-        let author = book.author != "" ? (book.author ?? "저자 정보 없음") : "저자 정보 없음"
-        let publisher = book.publisher != "" ? (book.publisher ?? "출판사 정보 없음") : "출판사 정보 없음"
-        self.authorAndPublisher.text = "\(author) | \(publisher)"
         let percent = Float(book.progressCount)/Float(book.sids.count)
         self.progressView.setProgress(percent, animated: true)
         self.progressPercentLabel.text = "\(Int(percent*100))%"
-    }
-    
-    func configureShadow() {
-        self.progressView.isHidden = true
     }
 }
