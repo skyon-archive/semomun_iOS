8e49aea060baa2bb120bea25618e06caccfc27b7
이성열 <type101315@gmail.com>
2022-04-12 15:44:18 +0900

    [Feat] 문제 신고 내용 API 반영

---
+++ semomun/Network/DTO/Encodables/ErrorReport.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorReport.swift
+//  semomun
+//
+//  Created by SEONG YEOL YI on 2022/04/12.
+//
+
+import Foundation
+
+struct ErrorReport: Encodable {
+    let pid: Int
+    let content: String
+}
