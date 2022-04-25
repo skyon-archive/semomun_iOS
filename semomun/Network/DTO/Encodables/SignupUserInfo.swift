c78cf29c5a0e03c581c22cc6ebc6157427140e98
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-04-25 16:54:29 +0900

    [Fix] 회원가입 절차 통과여부 값 수정

--- semomun/Network/DTO/Encodables/SignupUserInfo.swift
+++ semomun/Network/DTO/Encodables/SignupUserInfo.swift
@@ -24,7 +24,7 @@ struct SignupUserInfo: Encodable {
     var marketing: Bool = false
     
     var isValid: Bool {
-        return [username, school, major, majorDetail, graduationStatus, marketing].allSatisfy { $0 != nil }
+        return [username, school, major, majorDetail, graduationStatus].allSatisfy { $0 != nil }
         && phone?.isValidPhoneNumberWithCountryCode == true
     }
 }
