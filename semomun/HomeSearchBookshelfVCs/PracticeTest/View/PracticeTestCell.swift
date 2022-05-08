9c10ee82cc0377fed48ae793798b26c34531db95
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-05-08 14:07:18 +0900

    [Feat] PracticeTestVC Cell 표시 기본로직 작성

---
+++ semomun/HomeSearchBookshelfVCs/PracticeTest/View/PracticeTestCell.swift
@@ -0,0 +1,31 @@
+//
+//  PracticeTestCell.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2022/05/08.
+//
+
+import UIKit
+
+final class PracticeTestCell: UICollectionViewCell {
+    static let identifer = "PracticeTestCell"
+    
+    @IBOutlet weak var bookcover: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    // 임시용 로직
+    func configure(title: String, price: String? = nil) {
+        self.titleLabel.text = title
+        self.priceLabel.text = price != nil ? price! : ""
+    }
+    
+}
