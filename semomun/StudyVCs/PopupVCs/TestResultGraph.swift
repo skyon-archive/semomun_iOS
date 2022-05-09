dcb5e27f7df5a9709c2e3270a9e859018c7a154b
이성열 <type101315@gmail.com>
2022-05-09 20:27:39 +0900

    [Refactor] 그래프 관련 코드 리팩터링

--- semomun/StudyVCs/PopupVCs/TestResultGraph.swift
+++ semomun/StudyVCs/PopupVCs/TestResultGraph.swift
@@ -7,16 +7,35 @@
 
 import SwiftUI
 
+extension View {
+    /// View의 frame 주위로 x축과 y축 직선을 추가
+    func xyAxis() -> some View {
+        return self
+            .overlay(
+                Rectangle()
+                    .frame(width: nil, height: 1, alignment: .bottom)
+                    .foregroundColor(.black),
+                alignment: .bottom
+            )
+            .overlay(
+                Rectangle()
+                    .frame(width: 1, height: nil, alignment: .leading)
+                    .foregroundColor(.black),
+                alignment: .leading
+            )
+    }
+}
+
 extension Path {
     /// 2개 이상의 점들을 이은 꺾은선 그래프의 Path를 반환
     /// - Parameters:
     ///   - yValue: 점들의 y값
     ///   - grid: 격자의 크기
-    static func lineChart(yValue: [Double?], grid: CGPoint) -> Path {
+    static func lineChart(yValue: [Double?], grid: CGSize) -> Path {
         var path = Path()
         
         for (idx, val) in yValue.enumerated() where val != nil {
-            let p = CGPoint(x: grid.x * CGFloat(idx), y: grid.y * CGFloat(val!))
+            let p = CGPoint(x: grid.width * CGFloat(idx), y: grid.height * CGFloat(val!))
             if idx == 0 {
                 path.move(to: p)
             } else {
@@ -27,11 +46,11 @@ extension Path {
         return path
     }
     
-    static func lineChartCircle(yValue: [Double?], grid: CGPoint) -> Path {
+    static func lineChartCircle(yValue: [Double?], grid: CGSize) -> Path {
         var path = Path()
         
         for (idx, val) in yValue.enumerated() where val != nil {
-            let p = CGPoint(x: grid.x * CGFloat(idx), y: grid.y * CGFloat(val!))
+            let p = CGPoint(x: grid.width * CGFloat(idx), y: grid.height * CGFloat(val!))
             var circle = Path()
             circle.addArc(center: p, radius: 5, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
             path.addPath(circle)
@@ -43,28 +62,18 @@ extension Path {
 
 struct Line: View {
     let data: [Double?]
-    let size: CGSize
     
     let lineColor: Color
     let circleColor: Color
     let strokeStyle: StrokeStyle
     
-    let xAxisSize: Int
-    let yAxisSize: Int
+    let gridSize: CGSize
     
-    var stepWidth: CGFloat {
-        return self.size.width / CGFloat(self.xAxisSize)
-    }
-    
-    var stepHeight: CGFloat {
-        return self.size.height / CGFloat(self.yAxisSize)
-    }
-    
-    public var body: some View {
-        Path.lineChart(yValue: self.data, grid: CGPoint(x: stepWidth, y: stepHeight))
+    var body: some View {
+        Path.lineChart(yValue: self.data, grid: self.gridSize) // 직선 차트
             .stroke(self.lineColor ,style: self.strokeStyle)
             .overlay(
-                Path.lineChartCircle(yValue: self.data, grid: CGPoint(x: stepWidth, y: stepHeight))
+                Path.lineChartCircle(yValue: self.data, grid: self.gridSize) // 차트 위 원
                     .fill(self.circleColor)
             )
             .rotationEffect(.degrees(180), anchor: .center)
@@ -75,6 +84,7 @@ struct Line: View {
 struct GradeLineGraph: View {
     let xLabelCount = 20
     let yLabelCount = 9
+    let yLabelWidth: CGFloat = 40
     
     let grades: [Double?]
     let average: [Double]
@@ -83,46 +93,54 @@ struct GradeLineGraph: View {
     var gradeYVal: [Double?] {
         return self.grades.map { $0 == nil ? nil : 10-$0! }
     }
-    
     var averageYVal: [Double] {
         return self.average.map { 10 - $0 }
     }
     
-    /// y축 레이블 간격
-    let yLabelPadding: CGFloat = 16
+    var xAxisLabels: some View {
+        HStack {
+            ForEach(1...xLabelCount, id: \.self) { grade in
+                Text("\(grade)회")
+                    .font(.system(size: 11, weight: .regular))
+                    .frame(height: 26)
+                    .frame(maxWidth: .infinity)
+            }
+        }
+        .padding(.leading, self.yLabelWidth)
+    }
     
-    public var body: some View {
+    var yAxisLabels: some View {
+        VStack {
+            ForEach(1...yLabelCount, id: \.self) { grade in
+                Text("\(grade)등급")
+                    .font(.system(size: 11, weight: .regular))
+                    .frame(width: self.yLabelWidth)
+                    .frame(maxHeight: .infinity)
+            }
+        }
+    }
+    
+    func getGridSize(frame: CGSize) -> CGSize {
+        return CGSize(width: frame.width / CGFloat(self.xLabelCount), height: frame.height / CGFloat(self.yLabelCount))
+    }
+    
+    var body: some View {
         VStack(spacing: 0) {
             HStack(spacing: 0) {
-                // y축 라벨
-                VStack {
-                    ForEach(1...yLabelCount, id: \.self) { grade in
-                        Text("\(grade)등급")
-                            .font(.system(size: 11, weight: .regular))
-                            .frame(width: 40)
-                            .frame(maxHeight: .infinity)
-                    }
-                }
+                self.yAxisLabels
                 GeometryReader{ reader in
                     ZStack {
                         Line(data: self.averageYVal,
-                             size: CGSize(width: reader.frame(in: .local).width , height: reader.frame(in: .local).height),
                              lineColor: Color(UIColor(SemomunColor.semoGray) ?? .gray),
                              circleColor: Color(UIColor(SemomunColor.yellowColor) ?? .black),
                              strokeStyle: .init(lineWidth: 1.5, dash: [5]),
-                             xAxisSize: self.xLabelCount,
-                             yAxisSize: self.yLabelCount
+                             gridSize: self.getGridSize(frame: reader.size)
                         )
                         Line(data: self.gradeYVal,
-                             size: CGSize(
-                                width: reader.frame(in: .local).width*CGFloat((1-1/xLabelCount)),
-                                height: reader.frame(in: .local).height*CGFloat((1-1/yLabelCount))
-                             ),
                              lineColor: Color(UIColor(SemomunColor.semoLightGray) ?? .gray),
                              circleColor: Color(UIColor(.mainColor) ?? .black),
                              strokeStyle: .init(lineWidth: 1.5),
-                             xAxisSize: self.xLabelCount,
-                             yAxisSize: self.yLabelCount
+                             gridSize: self.getGridSize(frame: reader.size)
                         )
                     }
                     .offset(
@@ -130,31 +148,9 @@ struct GradeLineGraph: View {
                         y: reader.frame(in: .local).height / CGFloat(yLabelCount*2)
                     )
                 }
-                // x축
-                .overlay(
-                    Rectangle()
-                        .frame(width: nil, height: 1, alignment: .bottom)
-                        .foregroundColor(.black),
-                    alignment: .bottom
-                )
-                // y축
-                .overlay(
-                    Rectangle()
-                        .frame(width: 1, height: nil, alignment: .leading)
-                        .foregroundColor(.black),
-                    alignment: .leading
-                )
+                .xyAxis()
             }
-            // x축 라벨
-            HStack {
-                ForEach(1...xLabelCount, id: \.self) { grade in
-                    Text("\(grade)회")
-                        .font(.system(size: 11, weight: .regular))
-                        .frame(height: 26)
-                        .frame(maxWidth: .infinity)
-                }
-            }
-            .padding(.leading, 40)
+            self.xAxisLabels
         }
         .frame(height: 315)
     }
