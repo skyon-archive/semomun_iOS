532bac3d853fd657732150238cbcea8c26e51ebe
이성열 <type101315@gmail.com>
2022-05-09 20:00:21 +0900

    [Refactor] 성적 그래프 관련 주석 추가

--- semomun/StudyVCs/PopupVCs/TestResultGraph.swift
+++ semomun/StudyVCs/PopupVCs/TestResultGraph.swift
@@ -8,18 +8,15 @@
 import SwiftUI
 
 extension Path {
-    /// 2개 이상의 점으로 이루어진 꺾은선 그래프의 Path를 반환
+    /// 2개 이상의 점들을 이은 꺾은선 그래프의 Path를 반환
     /// - Parameters:
-    ///   - points: 점들의 y값
-    ///   - step: 격자 크기
-    static func lineChart(points: [Double?], step: CGPoint) -> Path {
+    ///   - yValue: 점들의 y값
+    ///   - grid: 격자의 크기
+    static func lineChart(yValue: [Double?], grid: CGPoint) -> Path {
         var path = Path()
         
-        for idx in 0..<points.count {
-            guard let point = points[idx] else { continue }
-            
-            let y = step.y * CGFloat(10 - point)
-            let p = CGPoint(x: step.x * CGFloat(idx), y: y)
+        for (idx, val) in yValue.enumerated() where val != nil {
+            let p = CGPoint(x: grid.x * CGFloat(idx), y: grid.y * CGFloat(val!))
             if idx == 0 {
                 path.move(to: p)
             } else {
@@ -30,15 +27,11 @@ extension Path {
         return path
     }
     
-    static func lineChartCircle(points: [Double?], step: CGPoint) -> Path {
+    static func lineChartCircle(yValue: [Double?], grid: CGPoint) -> Path {
         var path = Path()
         
-        for idx in 0..<points.count {
-            guard let point = points[idx] else { continue }
-            
-            let y = step.y * CGFloat(10 - point)
-            let p = CGPoint(x: step.x * CGFloat(idx), y: y)
-            
+        for (idx, val) in yValue.enumerated() where val != nil {
+            let p = CGPoint(x: grid.x * CGFloat(idx), y: grid.y * CGFloat(val!))
             var circle = Path()
             circle.addArc(center: p, radius: 5, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
             path.addPath(circle)
@@ -51,27 +44,27 @@ extension Path {
 struct Line: View {
     let data: [Double?]
     let size: CGSize
+    
     let lineColor: Color
     let circleColor: Color
     let strokeStyle: StrokeStyle
-    // @Binding var frame: CGRect
     
-    @State private var end = CGFloat.zero
+    let xAxisSize: Int
+    let yAxisSize: Int
     
     var stepWidth: CGFloat {
-        return self.size.width / 20
+        return self.size.width / CGFloat(self.xAxisSize)
     }
     
     var stepHeight: CGFloat {
-        return self.size.height / 9
+        return self.size.height / CGFloat(self.yAxisSize)
     }
     
     public var body: some View {
-        
-        Path.lineChart(points: self.data, step: CGPoint(x: stepWidth, y: stepHeight))
+        Path.lineChart(yValue: self.data, grid: CGPoint(x: stepWidth, y: stepHeight))
             .stroke(self.lineColor ,style: self.strokeStyle)
             .overlay(
-                Path.lineChartCircle(points: self.data, step: CGPoint(x: stepWidth, y: stepHeight))
+                Path.lineChartCircle(yValue: self.data, grid: CGPoint(x: stepWidth, y: stepHeight))
                     .fill(self.circleColor)
             )
             .rotationEffect(.degrees(180), anchor: .center)
@@ -79,21 +72,29 @@ struct Line: View {
     }
 }
 
-struct LineView: View {
-    let data: [Double?]
-    let average: [Double]
-    
-    /// 그래프와 축간 간격
-    let padding: CGFloat = 30
-    /// y축 레이블 간격
-    let yLabelPadding: CGFloat = 16
-    
+struct GradeLineGraph: View {
     let xLabelCount = 20
     let yLabelCount = 9
+    
+    let grades: [Double?]
+    let average: [Double]
+    
+    // 등급이 낮아질수록 y값이 커지는 것 처리
+    var gradeYVal: [Double?] {
+        return self.grades.map { $0 == nil ? nil : 10-$0! }
+    }
+    
+    var averageYVal: [Double] {
+        return self.average.map { 10 - $0 }
+    }
+    
+    /// y축 레이블 간격
+    let yLabelPadding: CGFloat = 16
     
     public var body: some View {
         VStack(spacing: 0) {
             HStack(spacing: 0) {
+                // y축 라벨
                 VStack {
                     ForEach(1...yLabelCount, id: \.self) { grade in
                         Text("\(grade)등급")
@@ -104,33 +105,39 @@ struct LineView: View {
                 }
                 GeometryReader{ reader in
                     ZStack {
-                        Line(data: self.average,
+                        Line(data: self.averageYVal,
                              size: CGSize(width: reader.frame(in: .local).width , height: reader.frame(in: .local).height),
                              lineColor: Color(UIColor(SemomunColor.semoGray) ?? .gray),
                              circleColor: Color(UIColor(SemomunColor.yellowColor) ?? .black),
-                             strokeStyle: .init(lineWidth: 1.5, dash: [5])
+                             strokeStyle: .init(lineWidth: 1.5, dash: [5]),
+                             xAxisSize: self.xLabelCount,
+                             yAxisSize: self.yLabelCount
                         )
-                        Line(data: self.data,
+                        Line(data: self.gradeYVal,
                              size: CGSize(
-                                width: reader.frame(in: .local).width*CGFloat((1-1/self.xLabelCount)),
-                                height: reader.frame(in: .local).height*CGFloat((1-1/self.yLabelCount))
+                                width: reader.frame(in: .local).width*CGFloat((1-1/xLabelCount)),
+                                height: reader.frame(in: .local).height*CGFloat((1-1/yLabelCount))
                              ),
                              lineColor: Color(UIColor(SemomunColor.semoLightGray) ?? .gray),
                              circleColor: Color(UIColor(.mainColor) ?? .black),
-                             strokeStyle: .init(lineWidth: 1.5)
+                             strokeStyle: .init(lineWidth: 1.5),
+                             xAxisSize: self.xLabelCount,
+                             yAxisSize: self.yLabelCount
                         )
                     }
                     .offset(
-                        x: reader.frame(in: .local).width / CGFloat(self.xLabelCount*2),
-                        y: reader.frame(in: .local).height / CGFloat(self.yLabelCount*2)
+                        x: reader.frame(in: .local).width / CGFloat(xLabelCount*2),
+                        y: reader.frame(in: .local).height / CGFloat(yLabelCount*2)
                     )
                 }
+                // x축
                 .overlay(
                     Rectangle()
                         .frame(width: nil, height: 1, alignment: .bottom)
                         .foregroundColor(.black),
                     alignment: .bottom
                 )
+                // y축
                 .overlay(
                     Rectangle()
                         .frame(width: 1, height: nil, alignment: .leading)
@@ -138,6 +145,7 @@ struct LineView: View {
                     alignment: .leading
                 )
             }
+            // x축 라벨
             HStack {
                 ForEach(1...xLabelCount, id: \.self) { grade in
                     Text("\(grade)회")
@@ -148,9 +156,7 @@ struct LineView: View {
             }
             .padding(.leading, 40)
         }
-        
         .frame(height: 315)
-        
     }
 }
 
@@ -159,7 +165,7 @@ struct TestResultGraph: View {
     let average : [Double] = [3, 4, 3, 2, 5, 5, 3, 4, 5].map(Double.init)
     
     var body: some View {
-        LineView(data: self.result, average: average)
+        GradeLineGraph(grades: self.result, average: average)
     }
 }
 
