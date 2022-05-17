312ee19192d1f0ee76bf5ca759937e7cb4d497f2
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-05-17 21:33:59 +0900

    [Feat] 새로운 type에 대한 다운로드 로직 수정

---
+++ semomun/StudyVCs/ChildVCs/MultipleWithSubProblemsVC.swift
@@ -0,0 +1,30 @@
+//
+//  MultipleWithSubProblemsVC.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2022/05/17.
+//
+
+import UIKit
+
+final class MultipleWithSubProblemsVC: UIViewController {
+    static let identifier = "MultipleWithSubProblemsVC"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
