292f0dc74249db93356121968741614f37269e83
이성열 <type101315@gmail.com>
2022-05-23 14:53:02 +0900

    [Feat] FormCell 추가

--- semomun/StudyVCs/ChildVCs/Cell/MultipleWith5Cell.swift
+++ semomun/StudyVCs/ChildVCs/Cell/MultipleWith5Cell.swift
@@ -8,7 +8,7 @@
 import UIKit
 import PencilKit
 
-class MultipleWith5Cell: UICollectionViewCell, PKToolPickerObserver {
+class MultipleWith5Cell: FormCell {
     static let identifier = "MultipleWith5Cell"
     
     @IBOutlet weak var bookmarkBT: UIButton!
@@ -16,20 +16,6 @@ class MultipleWith5Cell: UICollectionViewCell, PKToolPickerObserver {
     @IBOutlet weak var answerBT: UIButton!
     @IBOutlet var checkNumbers: [UIButton]!
     
-    private let canvasView = PKCanvasView()
-    private let imageView = UIImageView()
-    
-    var contentImage: UIImage?
-    var problem: Problem_Core?
-    weak var delegate: CollectionCellDelegate?
-    
-    var toolPicker: PKToolPicker?
-    lazy var resultImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.backgroundColor = UIColor.clear
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
     lazy var checkImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.backgroundColor = UIColor.clear
@@ -42,44 +28,7 @@ class MultipleWith5Cell: UICollectionViewCell, PKToolPickerObserver {
         return answerView
     }()
     private lazy var timerView = ProblemTimerView()
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        self.configureUI()
-        self.configureScrollView()
-        
-        self.contentView.addSubview(canvasView)
-        self.canvasView.addDoubleTabGesture()
-        self.canvasView.addSubview(self.imageView)
-        self.canvasView.sendSubviewToBack(self.imageView)
-        
-        self.canvasView.borderColor = .red
-        self.canvasView.borderWidth = 5
-        self.imageView.borderColor = .blue
-        self.imageView.borderWidth = 5
-        
-        print("\(Self.identifier) awakeFromNib")
-    }
-    
-    override func prepareForReuse() {
-        self.configureUI()
-        self.canvasView.delegate = nil
-        
-        self.layoutIfNeeded()
-        let size = self.contentView.frame
-        self.canvasView.frame = .init(0, 51, size.width, size.height-51)
-        self.adjustLayout()
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        self.layoutIfNeeded()
-        let size = self.contentView.frame
-        self.canvasView.frame = .init(0, 51, size.width, size.height-51)
-        self.adjustLayout()
-    }
-    
+
     // 객관식 1~5 클릭 부분
     @IBAction func sol_click(_ sender: UIButton) {
         guard let problem = self.problem else { return }
@@ -128,52 +77,20 @@ class MultipleWith5Cell: UICollectionViewCell, PKToolPickerObserver {
         }
     }
     
+    override func configureReuse(_ contentImage: UIImage?, _ problem: Problem_Core?, _ toolPicker: PKToolPicker?) {
+        super.configureReuse(contentImage, problem, toolPicker)
+        self.configureUI()
+    }
+    
     // MARK: Configure
     private func configureUI() {
-        self.resultImageView.removeFromSuperview()
-        self.checkImageView.removeFromSuperview()
-        self.answerBT.isHidden = false
-        self.timerView.removeFromSuperview()
-        self.answerView.removeFromSuperview()
-    }
-    
-    private func configureScrollView() {
-        self.canvasView.minimumZoomScale = 0.5
-        self.canvasView.maximumZoomScale = 2.0
-        self.canvasView.delegate = self
-    }
-    
-    // MARK: - Configure Reuse
-    func configureReuse(_ contentImage: UIImage?, _ problem: Problem_Core?, _ toolPicker: PKToolPicker?) {
-        self.configureProblem(problem)
-        self.configureUI(contentImage)
-        self.toolPicker = toolPicker
-        self.configureCanvasView()
-    }
-    
-    func configureProblem(_ problem: Problem_Core?) {
-        self.problem = problem
-    }
-    
-    func configureUI(_ contentImage: UIImage?) {
-        self.configureImageView(contentImage)
         self.configureCheckButtons()
         self.configureStar()
         self.configureAnswer()
-        self.configureExplanation()
+        self.configureExplanationBT()
     }
     
-    func configureImageView(_ contentImage: UIImage?) {
-        guard let contentImage = contentImage else { return }
-        if contentImage.size.width > 0 && contentImage.size.height > 0 {
-            self.contentImage = contentImage
-        } else {
-            self.contentImage = UIImage(.warning)
-        }
-        self.imageView.image = self.contentImage
-    }
-    
-    func configureCheckButtons() {
+    private func configureCheckButtons() {
         guard let problem = self.problem else { return }
         
         // 일단 모든 버튼 표시 구현
@@ -200,7 +117,7 @@ class MultipleWith5Cell: UICollectionViewCell, PKToolPickerObserver {
         }
     }
     
-    func createCheckImage(to index: Int) {
+    private func createCheckImage(to index: Int) {
         self.checkImageView.image = UIImage(named: "check")
         self.contentView.addSubview(self.checkImageView)
         self.checkImageView.translatesAutoresizingMaskIntoConstraints = false
@@ -213,7 +130,7 @@ class MultipleWith5Cell: UICollectionViewCell, PKToolPickerObserver {
         ])
     }
     
-    func configureTimerView() {
+    private func configureTimerView() {
         guard let time = self.problem?.time else { return }
         
         self.contentView.addSubview(self.timerView)
@@ -227,17 +144,11 @@ class MultipleWith5Cell: UICollectionViewCell, PKToolPickerObserver {
         self.timerView.configureTime(to: time)
     }
     
-    func showResultImage(to: Bool) {
-        let imageName: String = to ? "correct" : "wrong"
-        self.resultImageView.image = UIImage(named: imageName)
-        self.imageView.addSubview(self.resultImageView)
-    }
-    
-    func configureStar() {
+    private func configureStar() {
         self.bookmarkBT.isSelected = self.problem?.star ?? false
     }
     
-    func configureAnswer() {
+    private func configureAnswer() {
         self.answerBT.isUserInteractionEnabled = true
         self.answerBT.setTitleColor(UIColor(.deepMint), for: .normal)
         if self.problem?.answer == nil {
@@ -246,7 +157,7 @@ class MultipleWith5Cell: UICollectionViewCell, PKToolPickerObserver {
         }
     }
     
-    func configureExplanation() {
+    private func configureExplanationBT() {
         self.explanationBT.isSelected = false
         self.explanationBT.isUserInteractionEnabled = true
         self.explanationBT.setTitleColor(UIColor(.deepMint), for: .normal)
@@ -254,84 +165,5 @@ class MultipleWith5Cell: UICollectionViewCell, PKToolPickerObserver {
             self.explanationBT.isUserInteractionEnabled = false
             self.explanationBT.setTitleColor(UIColor.gray, for: .normal)
         }
-    }
-    
-    func configureCanvasView() {
-        self.configureCanvasViewDataAndDelegate()
-        
-        canvasView.isOpaque = false
-        canvasView.backgroundColor = .clear
-//        canvasView.becomeFirstResponder()
-        canvasView.drawingPolicy = .pencilOnly
-        
-//        toolPicker?.setVisible(true, forFirstResponder: canvasView)
-        toolPicker?.addObserver(canvasView)
-    }
-    
-    func configureCanvasViewDataAndDelegate() {
-        if let pkData = self.problem?.drawing {
-            do {
-                try canvasView.drawing = PKDrawing.init(data: pkData)
-            } catch {
-                print("Error loading drawing object")
-            }
-        } else {
-            canvasView.drawing = PKDrawing()
-        }
-        canvasView.delegate = self
-    }
-    
-    func updateSolved(problem: Problem_Core, input: String) {
-        problem.setValue(input, forKey: "solved") // 사용자 입력 값 저장
-        
-        if let answer = problem.answer { // 정답이 있는 경우 정답여부 업데이트
-            let correct = input == answer
-            problem.setValue(correct, forKey: "correct")
-        }
-        self.delegate?.addScoring(pid: Int(problem.pid))
-    }
-    
-    /// action 전/후 레이아웃 변경을 저장해주는 편의 함수
-    private func adjustLayout(_ action: (() -> ())? = nil) {
-        let previousCanvasSize = self.canvasView.frame.size
-        let previousContentOffset = self.canvasView.contentOffset
-        action?()
-        self.adjustLayout(previousCanvasSize: previousCanvasSize, previousContentOffset: previousContentOffset)
-    }
-    
-    /// CanvasView의 크기가 바뀐 후 이에 맞게 필기/이미지 레이아웃을 수정
-    private func adjustLayout(previousCanvasSize: CGSize, previousContentOffset: CGPoint) {
-        guard let image = self.imageView.image else {
-            assertionFailure("CanvasView의 크기를 구할 이미지 정보 없음")
-            return
-        }
-        
-        let ratio = image.size.height/image.size.width
-        self.canvasView.adjustDrawingLayout(previousCanvasSize: previousCanvasSize, previousContentOffset: previousContentOffset, contentRatio: ratio)
-        
-        // 문제 이미지 크기 설정
-        self.imageView.frame.size = self.canvasView.contentSize
-        // 채점 이미지 크기 설정
-        let imageViewWidth = self.imageView.frame.width
-        self.resultImageView.frame = .init(imageViewWidth*65/834, 0, imageViewWidth*150/834, imageViewWidth*150/834)
-    }
-}
-
-extension MultipleWith5Cell: PKCanvasViewDelegate {
-    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
-        guard let problem = self.problem else { return }
-        let data = self.canvasView.drawing.dataRepresentation()
-        problem.setValue(data, forKey: "drawing")
-        self.delegate?.addUpload(pid: Int(problem.pid))
-    }
-}
-
-extension MultipleWith5Cell: UIScrollViewDelegate {
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return self.imageView
-    }
-    
-    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        self.adjustLayout()
     }
 }
