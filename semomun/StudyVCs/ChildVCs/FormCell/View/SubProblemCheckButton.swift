cafe6212fd081dde6b4d218f11d1c475c502fea5
이성열 <type101315@gmail.com>
2022-05-27 18:19:12 +0900

    [Refactor] SubProblemCheckButton가 UIStackView에서 가지는 크기를 변경할 수 있도록 수정

--- semomun/StudyVCs/ChildVCs/FormCell/View/SubProblemCheckButton.swift
+++ semomun/StudyVCs/ChildVCs/FormCell/View/SubProblemCheckButton.swift
@@ -14,19 +14,26 @@ class SubProblemCheckButton: UIButton {
     static let korLabels = ["ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㅊ"]
     static let EngLabels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
     static let engLabels = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    
+    private var size: CGFloat = 32
 
-    convenience init(index: Int, delegate: SubProblemCheckObservable) {
-        self.init(frame: CGRect(0, 0, 30, 30))
-        self.commonInit(index: index, delegate: delegate)
+    convenience init(size: CGFloat, index: Int, delegate: SubProblemCheckObservable) {
+        self.init(frame: CGRect(0, 0, 32, 32))
+        self.commonInit(size: size, index: index, delegate: delegate)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: self.size, height: self.size)
     }
 
-    private func commonInit(index: Int, delegate: SubProblemCheckObservable) {
+    private func commonInit(size: CGFloat, index: Int, delegate: SubProblemCheckObservable) {
+        self.size = size
+        
         self.backgroundColor = .clear
         
         self.borderWidth = 1
         self.layer.borderColor = UIColor(.deepMint)?.cgColor
-        self.layer.cornerRadius = 15
-        
+        self.layer.cornerRadius = size/2
         self.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .medium)
         self.setTitleColor(UIColor(.deepMint), for: .normal)
         self.setTitle(Self.korLabels[index], for: .normal)
