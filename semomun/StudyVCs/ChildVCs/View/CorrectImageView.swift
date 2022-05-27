5d9eb27b11af4d3cf5a7477071c96afb866fe752
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-05-28 00:27:26 +0900

    [Refactor] RotationableCanvasView 생성 및 FormCell lifecycle 에 따른 정리

--- semomun/StudyVCs/ChildVCs/View/CorrectImageView.swift
+++ semomun/StudyVCs/ChildVCs/View/CorrectImageView.swift
@@ -19,23 +19,25 @@ final class CorrectImageView: UIImageView {
         self.isHidden = true
     }
     
-    func show(isWrong: Bool) {
-        self.image = isWrong ? UIImage(.wrong) : UIImage(.correct)
+    func show(isCorrect: Bool) {
+        self.image = isCorrect ? UIImage(.correct) : UIImage(.wrong)
         self.isHidden = false
     }
     
     func adjustLayoutForCell(imageViewWidth: CGFloat) {
         guard self.isHidden == false else { return }
         let width = imageViewWidth/5
+        let xOffset = (imageViewWidth/10) - (width/2)
         let yOffset = (imageViewWidth/100) * 13.5 - (imageViewWidth/10)
-        self.frame = .init(0, yOffset, width, width)
+        self.frame = .init(xOffset, yOffset, width, width)
     }
     
     func adjustLayoutForZero(imageViewWidth: CGFloat) {
         guard self.isHidden == false else { return }
         let width = imageViewWidth/5
         let xOffset = (imageViewWidth/100)*19 - (imageViewWidth/10)
-        self.frame = .init(xOffset, 0, width, width)
+        let yOffset = (imageViewWidth/10) - (width/2)
+        self.frame = .init(xOffset, yOffset, width, width)
     }
 }
 
