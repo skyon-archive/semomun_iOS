4ab3712112827ba3d4950821715ea263a30435ac
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-05-31 17:51:22 +0900

    [Refactor] lifecycle 순서에 따른 정리

--- semomun/StudyVCs/ChildVCs/FormZero/FormZero.swift
+++ semomun/StudyVCs/ChildVCs/FormZero/FormZero.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import PencilKit
+import Alamofire
 
 class FormZero: UIViewController, PKToolPickerObserver {
     var canvasView = RotationableCanvasView()
@@ -51,6 +52,7 @@ class FormZero: UIViewController, PKToolPickerObserver {
     }()
     
     private(set) var shouldShowExplanation = false
+    private var isCanvasDrawingLoaded: Bool = false
     
     /* 외부에서 주입 가능한 property */
     var toolPicker: PKToolPicker?
@@ -67,6 +69,7 @@ class FormZero: UIViewController, PKToolPickerObserver {
         super.viewDidLoad()
         self.configureLoader()
         self.configureSubViews()
+        self.configureSwipeGesture()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -74,24 +77,22 @@ class FormZero: UIViewController, PKToolPickerObserver {
         self.updateImageView()
         self.updateToolPicker()
         self.updateTimerView()
-        self.showResultImage()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        self.configureUI()
-        self.configureCanvasViewData()
+        self.adjustLayouts(frameUpdate: true)
+        self.configureCanvasViewDataAndDelegate()
         self.stopLoader()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        self.canvasView.delegate = nil
+        self.canvasView.setDefaults()
         self.resultImageView.isHidden = true
         self.timerView.isHidden = true
-        
-        self.setViewToDefault()
+        self.isCanvasDrawingLoaded = false
+        self.closeExplanation()
     }
     
     // MARK: Rotation
@@ -100,34 +101,9 @@ class FormZero: UIViewController, PKToolPickerObserver {
         coordinator.animate { _ in
             UIView.performWithoutAnimation {
                 self.adjustLayouts(frameUpdate: true)
+                self.configureCanvasViewDataAndDelegate()
             }
         }
-    }
-    
-    /// 각 view들의 상태를 VC가 처음 보여졌을 때의 것으로 초기화
-    private func setViewToDefault() {
-        self.canvasView.setContentOffset(.zero, animated: false)
-        self.canvasView.zoomScale = 1.0
-        self.canvasView.contentInset = .zero
-        self.closeExplanation()
-
-        // 필기 남는 버그 우회, canvasView 객체를 다시 설정
-        self.canvasView.removeFromSuperview()
-        self.canvasView = PKCanvasView()
-        self.view.addSubview(self.canvasView)
-        self.canvasView.addSubview(self.imageView)
-        self.canvasView.sendSubviewToBack(self.imageView)
-        self.configureScrollView()
-        self.configureGesture()
-        
-        // 각종 subView들 제거
-        self.explanationView.removeFromSuperview()
-    }
-    
-    /// View의 frame이 정해진 후 UI를 구성
-    private func configureUI() {
-        self.canvasView.frame = .init(origin: .init(0, self.topHeight), size: self.contentSize)
-        self.adjustLayout()
     }
 }
 
@@ -149,8 +125,26 @@ extension FormZero {
         self.canvasView.addDoubleTabGesture()
         self.canvasView.addSubview(self.imageView)
         self.canvasView.sendSubviewToBack(self.imageView)
+    }
+    
+    private func configureSwipeGesture() {
+        let rightSwipeGesture: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(rightDragged))
+        rightSwipeGesture.direction = .right
+        rightSwipeGesture.numberOfTouchesRequired = 1
+        self.view.addGestureRecognizer(rightSwipeGesture)
         
-        self.configureSwipeGesture()
+        let leftSwipeGesture: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(leftDragged))
+        leftSwipeGesture.direction = .left
+        leftSwipeGesture.numberOfTouchesRequired = 1
+        self.view.addGestureRecognizer(leftSwipeGesture)
+    }
+    
+    @objc func rightDragged() {
+        NotificationCenter.default.post(name: .beforePage, object: nil)
+    }
+    
+    @objc func leftDragged() {
+        NotificationCenter.default.post(name: .nextPage, object: nil)
     }
     
     private func stopLoader() {
@@ -193,7 +187,6 @@ extension FormZero {
         if let showExplanation = showExplanation {
             self.shouldShowExplanation = showExplanation
         }
-
         // canvasView 크기 및 ratio 조절
         self.updateCanvasView(frameUpdate: frameUpdate)
         // explanation 크기 조절
@@ -222,55 +215,25 @@ extension FormZero {
         guard rotate && self.showExplanation else { return }
         self.explanationView.updateFrame(contentSize: self.view.frame.size, topHeight: self.internalTopViewHeight)
     }
-}
-
-extension FormZero {
-    /// 단 한 번만 필요한 UI 설정을 수행
-    private func showResultImage() {
-        guard let result = self.problemResult else {
-            self.resultImageView.isHidden = true
-            return
-        }
-        
-        let imageName = result ? "correct" : "wrong"
-        self.resultImageView.image = UIImage(named: imageName)
-        self.resultImageView.isHidden = false
-    }
     
-    private func configureCanvasViewData() {
+    private func configureCanvasViewDataAndDelegate() {
+        guard self.isCanvasDrawingLoaded == false else { return }
         // 설정 중에 delegate가 호출되지 않도록 마지막에 지정
         defer { self.canvasView.delegate = self }
         
-        guard let pkData = self.drawing,
-              let drawingWidth = self.drawingWidth else {
-            self.canvasView.drawing = PKDrawing()
-            return
-        }
-        
-        guard let drawing = try? PKDrawing(data: pkData) else {
-            print("Error loading drawing object")
-            self.canvasView.drawing = PKDrawing()
-            return
-        }
-        
-        
-        if drawingWidth > 0 {
-            let scale = self.canvasView.frame.width / CGFloat(drawingWidth)
-            let transform = CGAffineTransform(scaleX: scale, y: scale)
-            let drawingConverted = drawing.transformed(using: transform)
-            self.canvasView.drawing = drawingConverted
-        } else {
-            self.canvasView.drawing = drawing
-        }
+        let savedData = self.problem?.drawing
+        let lastWidth = self.problem?.drawingWidth
+        // 필기데이터 ratio 조절 후 표시
+        self.canvasView.loadDrawing(to: savedData, lastWidth: lastWidth)
+        self.isCanvasDrawingLoaded = true
     }
-    
-    
-    
-    
-    
-    
-    
-    
+}
+
+// MARK: Child Accessible
+extension FormZero {
+    func showResultImage(to: Bool) {
+        self.resultImageView.show(isCorrect: to)
+    }
 }
 
 // MARK: - 레이아웃 관련
@@ -291,45 +254,11 @@ extension FormZero {
             self.explanationView.alpha = 1
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    /// action 전/후 레이아웃 변경을 저장해주는 편의 함수
-    private func adjustLayout(_ action: (() -> ())? = nil) {
-        let previousCanvasSize = self.canvasView.frame.size
-        let previousContentOffset = self.canvasView.contentOffset
-        action?()
-        self.adjustLayout(previousCanvasSize: previousCanvasSize, previousContentOffset: previousContentOffset)
-    }
 }
 
 // MARK: - 제스쳐 설정
 extension FormZero {
     
-    
-    private func configureSwipeGesture() {
-        let rightSwipeGesture: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(rightDragged))
-        rightSwipeGesture.direction = .right
-        rightSwipeGesture.numberOfTouchesRequired = 1
-        self.view.addGestureRecognizer(rightSwipeGesture)
-        
-        let leftSwipeGesture: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(leftDragged))
-        leftSwipeGesture.direction = .left
-        leftSwipeGesture.numberOfTouchesRequired = 1
-        self.view.addGestureRecognizer(leftSwipeGesture)
-    }
-    
-    @objc func rightDragged() {
-        self.previousPage()
-    }
-    
-    @objc func leftDragged() {
-        self.nextPage()
-    }
 }
 
 // MARK: - Protocols
