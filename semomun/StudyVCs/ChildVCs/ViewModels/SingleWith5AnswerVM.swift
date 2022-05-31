1e237105463a3d02a8579afd1a70c515e51298c2
이성열 <type101315@gmail.com>
2022-05-31 14:53:28 +0900

    [Feat] 5다선지 다답형 처리 추가

--- semomun/StudyVCs/ChildVCs/ViewModels/SingleWith5AnswerVM.swift
+++ semomun/StudyVCs/ChildVCs/ViewModels/SingleWith5AnswerVM.swift
@@ -8,6 +8,11 @@
 import Foundation
 
 final class SingleWith5AnswerVM: PageVM {
+    override init(delegate: PageDelegate, pageData: PageData) {
+        self.shouldChooseMultipleAnswer = (pageData.problems.first?.answer?.contains(",") == true)
+        super.init(delegate: delegate, pageData: pageData)
+    }
+    
     override func answer(of problem: Problem_Core? = nil) -> String? {
         guard let answer = self.problem?.answer else { return nil }
         if answer.contains("|") {
@@ -16,8 +21,22 @@ final class SingleWith5AnswerVM: PageVM {
             return answer
         }
     }
+    
     override func isCorrect(input: String, answer: String) -> Bool {
         let answers = answer.split(separator: "|").map { String($0) }
         return answers.contains(input)
     }
+    
+    func updateSolved(withSelectedAnswers selectedAnswers: [Int], problem: Problem_Core? = nil) {
+        if self.shouldChooseMultipleAnswer {
+            let answersConverted = selectedAnswers.sorted().map { String($0) }.joined(separator: ",")
+            self.updateSolved(withSelectedAnswer: answersConverted, problem: problem)
+        } else {
+            if let selectedAnswer = selectedAnswers.first {
+                self.updateSolved(withSelectedAnswer: String(selectedAnswer), problem: problem)
+            }
+        }
+    }
+    
+    let shouldChooseMultipleAnswer: Bool
 }
