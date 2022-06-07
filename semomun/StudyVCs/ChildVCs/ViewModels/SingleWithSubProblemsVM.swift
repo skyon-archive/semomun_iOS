747fc2415d766298032a239d07435d7c04af2522
이성열 <type101315@gmail.com>
2022-06-07 17:25:26 +0900

    [Refactor] SingleWithSubProblemsVC의 문자열 관련 작업을 VM으로 이동

--- semomun/StudyVCs/ChildVCs/ViewModels/SingleWithSubProblemsVM.swift
+++ semomun/StudyVCs/ChildVCs/ViewModels/SingleWithSubProblemsVM.swift
@@ -8,10 +8,34 @@
 import Foundation
 
 final class SingleWithSubProblemsVM: PageVM {
-    override func answerStringForUser(_ problem: Problem_Core? = nil) -> String? {
-        return problem?.answer
+    /// 문제의 정답.
+    var answer: [String] {
+        guard let answer = self.problem?.answer else { return [] }
+        
+        return answer.split(separator: ",").map { String($0) }
     }
+    
+    override init(delegate: PageDelegate, pageData: PageData) {
+        super.init(delegate: delegate, pageData: pageData)
+    }
+    
+    override func answerStringForUser(_ problem: Problem_Core? = nil) -> String? {
+        guard let answer = self.problem?.answer else { return nil }
+        
+        return answer.split(separator: "$").joined(separator: ", ")
+    }
+    
     override func isCorrect(input: String, answer: String) -> Bool {
         return input == answer
+    }
+    
+    func updateSolved(withSelectedAnswer selectedAnswer: [String?]) {
+        guard selectedAnswer.count == self.answer.count else {
+            assertionFailure()
+            return
+        }
+        
+        let answerConverted = selectedAnswer.map({$0 ?? ""}).joined(separator: "$")
+        self.updateSolved(withSelectedAnswer: answerConverted)
     }
 }
