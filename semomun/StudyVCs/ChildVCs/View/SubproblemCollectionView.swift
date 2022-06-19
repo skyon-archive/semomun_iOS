5acfad3c1911ee32913401bb75fec98a9ae20c24
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-06-19 14:37:14 +0900

    [Fix] collectionView 우측 margin 10 설정 수정

--- semomun/StudyVCs/ChildVCs/View/SubproblemCollectionView.swift
+++ semomun/StudyVCs/ChildVCs/View/SubproblemCollectionView.swift
@@ -10,7 +10,9 @@ import UIKit
 /// Cell이 포함된 Form들에서 Cell들을 표시하는 역할
 class SubproblemCollectionView: UICollectionView {
     init() {
-        super.init(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10)
+        super.init(frame: .zero, collectionViewLayout: layout)
     }
     
     required init?(coder: NSCoder) {
@@ -25,7 +27,7 @@ class SubproblemCollectionView: UICollectionView {
      - FormOne exp 없을 때
      */
     func updateFrame(formOneContentSize contentSize: CGSize) {
-        self.frame = .init(contentSize.width/2, 0, contentSize.width/2 - 10, contentSize.height)
+        self.frame = .init(contentSize.width/2, 0, contentSize.width/2, contentSize.height)
         self.collectionViewLayout.invalidateLayout()
     }
     
@@ -34,9 +36,9 @@ class SubproblemCollectionView: UICollectionView {
      */
     func updateFrameWithExp(formOneContentSize contentSize: CGSize) {
         if UIWindow.isLandscape {
-            self.frame = .init(contentSize.width/2, 0, contentSize.width/2 - 10, contentSize.height)
+            self.frame = .init(contentSize.width/2, 0, contentSize.width/2, contentSize.height)
         } else {
-            self.frame = .init(contentSize.width/2, 0, contentSize.width/2 - 10, contentSize.height/2)
+            self.frame = .init(contentSize.width/2, 0, contentSize.width/2, contentSize.height/2)
         }
         self.collectionViewLayout.invalidateLayout()
     }
@@ -47,10 +49,10 @@ class SubproblemCollectionView: UICollectionView {
     func updateFrame(formTwoContentSize contentSize: CGSize) {
         if UIWindow.isLandscape {
             let marginBetweenView: CGFloat = 26
-            self.frame = .init(x: (contentSize.width - marginBetweenView)/2 + marginBetweenView, y: 0, width: (contentSize.width - marginBetweenView)/2 - 10, height: contentSize.height)
+            self.frame = .init(x: (contentSize.width - marginBetweenView)/2 + marginBetweenView, y: 0, width: (contentSize.width - marginBetweenView)/2, height: contentSize.height)
         } else {
             let marginBetweenView: CGFloat = 13
-            self.frame = .init(0, (contentSize.height - marginBetweenView)/2 + marginBetweenView, contentSize.width - 10, (contentSize.height - marginBetweenView)/2)
+            self.frame = .init(0, (contentSize.height - marginBetweenView)/2 + marginBetweenView, contentSize.width, (contentSize.height - marginBetweenView)/2)
         }
         self.collectionViewLayout.invalidateLayout()
     }
