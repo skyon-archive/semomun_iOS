0560f05636ef9cbea5182b7dd9ebedec1e5f60fa
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-06-20 14:19:17 +0900

    [Refactor] ComprehensiveReportButton customButton 으로 분리 반영

---
+++ semomun/HomeSearchBookshelfVCs/PracticeTest/View/ComprehensiveReportButton.swift
@@ -0,0 +1,26 @@
+//
+//  ComprehensiveReportButton.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2022/06/20.
+//
+
+import UIKit
+
+final class ComprehensiveReportButton: UIButton {
+    convenience init() {
+        self.init(frame: CGRect())
+        self.commonInit()
+    }
+    
+    private func commonInit() {
+        self.frame = .init(0, 0, 130, 42)
+        self.borderColor = UIColor(.mainColor)
+        self.borderWidth = 1
+        self.cornerRadius = 5
+        
+        self.titleLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
+        self.setTitleColor(UIColor(.mainColor), for: .normal)
+        self.setTitle("종합성적표 확인", for: .normal)
+    }
+}
