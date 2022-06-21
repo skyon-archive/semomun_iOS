cdfc9934f5ab6fed2ceace580f2e7e23b1214ed6
이성열 <type101315@gmail.com>
2022-06-21 16:08:57 +0900

    [Feat] PrivateTestResultInfoOfDB에 perfectScore 추가

---
+++ semomun/StudyZone/PopupVC/PracticeTestResult/PracticeTestResultVM.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeTestResultVM.swift
+//  semomun
+//
+//  Created by SEONG YEOL YI on 2022/06/21.
+//
+
+import Foundation
+
+class PracticeTestResultVM {
+    /* private */
+    private let networkUsecase: UserTestResultFetchable
+    
+    init(networkUsecase: UserTestResultFetchable) {
+        self.networkUsecase = networkUsecase
+    }
+}
