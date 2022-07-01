c78ae4c1c09ff4b62647344ffe75e84301ae0142
이성열 <type101315@gmail.com>
2022-07-01 14:06:51 +0900

    [Feat] groupStandardDeviation이 0이어도 올바른 평균값을 계산하도록 수정

--- semomun/StudyZone/PopupVC/PracticeTestResult/Model/TestResultCalculator.swift
+++ semomun/StudyZone/PopupVC/PracticeTestResult/Model/TestResultCalculator.swift
@@ -16,11 +16,11 @@ struct TestResultCalculator {
     ///   - rankCutoff: 1등급부터의 등급컷을 표현하는 길이 8의 배열. 9등급의 등급컷은 의미 없기 때문.
     ///   - perfectScore: 시험의 만점
     static func getScoreResult(rawScore: Int, groupAverage: Int, groupStandardDeviation: Int, area: String, rankCutoff: [Int], perfectScore: Int) -> ScoreResult {
-        guard groupStandardDeviation > 0 else {
-            return .init(rank: 0, rawScore: rawScore, standardScore: 0, percentile: 0, perfectScore: perfectScore)
-        }
-
         let rank = (rankCutoff.firstIndex(where: { rawScore >= $0 }) ?? 8) + 1
+        
+        guard groupStandardDeviation > 0 else {
+            return .init(rank: rank, rawScore: rawScore, standardScore: 0, percentile: 0, perfectScore: perfectScore)
+        }
         
         // 평균이 0인 표준점수
         let zeroAverageDeviation = Double(rawScore - groupAverage) / Double(groupStandardDeviation)
