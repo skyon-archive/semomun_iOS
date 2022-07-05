78231f11fbbf116d21e60072f98a401248eab79e
이성열 <type101315@gmail.com>
2022-07-05 14:47:11 +0900

    [Feat] UIFont extension에 heading별 static 프로퍼티 추가

---
+++ semomun/Global/Extension/UIFont.swift
@@ -0,0 +1,16 @@
+//
+//  UIFont.swift
+//  semomun
+//
+//  Created by SEONG YEOL YI on 2022/07/05.
+//
+
+import UIKit
+
+extension UIFont {
+    static let heading1 = UIFont(name: "PretendardVariable-Bold", size: 36)
+    static let heading2 = UIFont(name: "PretendardVariable-Bold", size: 24)
+    static let heading3 = UIFont(name: "PretendardVariable-Bold", size: 20)
+    static let heading4 = UIFont(name: "PretendardVariable-Bold", size: 16)
+    static let heading5 = UIFont(name: "PretendardVariable-SemiBold", size: 14)
+}
