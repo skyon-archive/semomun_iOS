e825059f8d7d5c3dd586f096a7f8ec22852a5498
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-07 18:24:03 +0900

    [Feat] configureCollectionView 로직 생성

--- semomun/Bookshelf/BookshelfVC.swift
+++ semomun/Bookshelf/BookshelfVC.swift
@@ -9,6 +9,11 @@ import UIKit
 import Combine
 
 final class BookshelfVC: UIViewController {
+    enum Tab: Int {
+        case home = 0
+        case workbook = 1
+        case practiceTest = 2
+    }
     /* public */
     static let identifier = "BookshelfVC"
     static let storyboardName = "HomeSearchBookshelf"
@@ -17,28 +22,76 @@ final class BookshelfVC: UIViewController {
     @IBOutlet var bookshelfTabUnderlines: [UIView]!
     @IBOutlet weak var collectionView: UICollectionView!
     
+    private var viewModel: BookshelfVM?
+    private var cancellables: Set<AnyCancellable> = []
+    private lazy var loadingView = LoadingView()
+    private var currentTab: Tab = .home {
+        didSet {
+            self.collectionView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.configureCollectionView()
+        self.configureViewModel()
+        self.bindAll()
+        self.checkSyncBookshelf()
+        self.configureObservation()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        guard UserDefaultsManager.isLogined else { return }
+        self.reloadCollectionView()
     }
     
     @IBAction func changeTab(_ sender: UIButton) {
         let index = sender.tag
         self.changeTabUI(index: index)
+        self.currentTab = Tab(rawValue: index) ?? .home
+    }
+}
+
+extension BookshelfVC {
+    private func configureCollectionView() {
+        let flowLayout = ScrollingBackgroundFlowLayout(sectionHeaderExist: true)
+        self.collectionView.collectionViewLayout = flowLayout
+        self.collectionView.configureDefaultDesign()
+        
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        
+        let homeHeaderNib = UINib(nibName: BookshelfHeaderView.identifier, bundle: nil)
+        let detailHeaderNib = UINib(nibName: BookshelfDetailHeaderView.identifier, bundle: nil)
+        self.collectionView.register(homeHeaderNib, forCellWithReuseIdentifier: BookshelfHeaderView.identifier)
+        self.collectionView.register(detailHeaderNib, forCellWithReuseIdentifier: BookshelfDetailHeaderView.identifier)
+    }
+}
+
+extension BookshelfVC: UICollectionViewDelegate {
+    
+}
+
+extension BookshelfVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
     }
 }
 
 extension BookshelfVC {
     private func changeTabUI(index: Int) {
-        UIView.animate(withDuration: 0.1) {
-            for idx in 0...2 {
-                if idx == index {
-                    self.bookshelfTabButtons[idx].setTitleColor(UIColor.getSemomunColor(.blueRegular), for: .normal)
-                    self.bookshelfTabUnderlines[idx].alpha = 1
-                } else {
-                    self.bookshelfTabButtons[idx].setTitleColor(UIColor.getSemomunColor(.black), for: .normal)
-                    self.bookshelfTabUnderlines[idx].alpha = 0
-                }
+        for (idx, button) in self.bookshelfTabButtons.enumerated() {
+            if button.tag == index {
+                button.setTitleColor(UIColor.getSemomunColor(.blueRegular), for: .normal)
+                self.bookshelfTabUnderlines[idx].alpha = 1
+            } else {
+                button.setTitleColor(UIColor.getSemomunColor(.black), for: .normal)
+                self.bookshelfTabUnderlines[idx].alpha = 0
             }
         }
     }
