0bb79acbb7fee26dc1496a8e52a42c589ebd9565
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-07 19:14:17 +0900

    [Feat] Bookshelf 용 CellInfo Struct 생성

---
+++ semomun/Bookshelf/Model/WorkbookCellInfo.swift
@@ -0,0 +1,17 @@
+//
+//  WorkbookCellInfo.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2022/07/07.
+//
+
+import Foundation
+
+struct WorkbookCellinfo {
+    let wid: Int
+    let title: String
+    let publishCompany: String
+    let imageData: Data
+    let recentDate: Date
+    let purchasedDate: Date
+}
---
+++ semomun/Bookshelf/Model/WorkbookGroupCellInfo.swift
@@ -0,0 +1,17 @@
+//
+//  WorkbookGroupCellInfo.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2022/07/07.
+//
+
+import Foundation
+
+struct WorkbookGroupCellInfo {
+    let wgid: Int
+    let title: String
+    let publishCompany: String
+    let imageData: Data
+    let recentDate: Date
+    let purchasedDate: Date
+}
