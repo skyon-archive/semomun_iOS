b38112e70a3835ca06aaeb3fc0bd991514754d4e
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-07 17:45:10 +0900

    [Feat] BookshelfHomeHeaderView 기본로직 구현

---
+++ semomun/Bookshelf/Cell/BookshelfDetailHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  BookshelfDetailHeaderView.swift
+//  semomun
+//
+//  Created by Kang Minsang on 2022/07/07.
+//
+
+import UIKit
+
+final class BookshelfDetailHeaderView: UICollectionReusableView {
+    static let identifier = "BookshelfDetailHeaderView"
+}
