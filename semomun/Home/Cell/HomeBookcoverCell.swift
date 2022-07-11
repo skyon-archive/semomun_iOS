3864b97eb927b74e43d9906ded1e357210b9ec8b
이성열 <type101315@gmail.com>
2022-07-11 15:55:52 +0900

    [Feat] HomeBookcoverConfigurable 추가

--- semomun/Home/Cell/HomeBookcoverCell.swift
+++ semomun/Home/Cell/HomeBookcoverCell.swift
@@ -9,6 +9,14 @@ import UIKit
 
 final class HomeBookcoverCell: BookcoverCell {
     static let identifier = "HomeBookcoverCell"
+    
+    func configure(_ configurable: HomeBookcoverConfigurable, networkUsecase: S3ImageFetchable) {
+        self.configureReuse(bookTitle: configurable.title, publishCompany: configurable.publishCompany)
+        configurable.fetchBookcoverUUID { [weak self] uuid in
+            self?.configureImage(uuid: uuid, networkUsecase: networkUsecase)
+        }
+    }
+    
     // 로그인 상태와 상관 없는 configure
     func configure(with preview: WorkbookPreviewOfDB, networkUsecase: S3ImageFetchable) {
         self.configureReuse(bookTitle: preview.title, publishCompany: preview.publishCompany)
@@ -19,6 +27,7 @@ final class HomeBookcoverCell: BookcoverCell {
         self.configureReuse(bookTitle: groupInfo.title, publishCompany: "실전 모의고사")
         self.configureImage(uuid: groupInfo.groupCover, networkUsecase: networkUsecase)
     }
+    
     // 로그인 상태에 구매내역을 통한 configure
     func configure(with info: BookshelfInfo, networkUsecase: (WorkbookSearchable & S3ImageFetchable)) {
         networkUsecase.getWorkbook(wid: info.wid) { workbookOfDB in
---
+++ semomun/Home/Cell/HomeBookcoverConfigurable.swift
@@ -0,0 +1,14 @@
+//
+//  HomeBookcoverConfigurable.swift
+//  semomun
+//
+//  Created by SEONG YEOL YI on 2022/07/11.
+//
+
+import Foundation
+
+protocol HomeBookcoverConfigurable {
+    var title: String { get }
+    var publishCompany: String { get }
+    var fetchBookcoverUUID: ((UUID) -> Void) -> Void { get }
+}
