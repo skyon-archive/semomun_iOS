26a80f24dbcecf8dc9cb9c2ccea74c9b5110cb5c
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-11 17:39:11 +0900

    [Feat] FavofiteTag 표시 로직 구현

--- semomun/Search/ViewModel/SearchVM.swift
+++ semomun/Search/ViewModel/SearchVM.swift
@@ -12,23 +12,27 @@ final class SearchVM {
     private let searchQueue = OperationQueue()
     private(set) var selectedWid: Int?
     private(set) var networkUsecase: NetworkUsecase
-    @Published private(set) var tags: [TagOfDB] = []
+    @Published private(set) var favoriteTags: [TagOfDB] = []
+    @Published private(set) var selectedTags: [TagOfDB] = []
+    @Published private(set) var searchResultWorkbooks: [WorkbookPreviewOfDB] = []
+    @Published private(set) var searchResultWorkbookGroups: [WorkbookGroupPreviewOfDB] = []
     @Published private(set) var workbook: WorkbookOfDB?
+    @Published private(set) var warning: (title: String, text: String)?
     
     init(networkUsecase: NetworkUsecase) {
         self.networkUsecase = networkUsecase
     }
     
     func append(tag: TagOfDB) {
-        self.tags.append(tag)
+        self.selectedTags.append(tag)
     }
     
     func removeTag(index: Int) {
-        self.tags.remove(at: index)
+        self.selectedTags.remove(at: index)
     }
     
     func removeAll() {
-        self.tags.removeAll()
+        self.selectedTags.removeAll()
     }
     
     func selectWorkbook(to wid: Int) {
@@ -38,6 +42,24 @@ final class SearchVM {
     func fetchWorkbook(wid: Int) {
         self.networkUsecase.getWorkbook(wid: wid) { [weak self] workbook in
             self?.workbook = workbook
+        }
+    }
+    
+    func fetchFavoriteTags() {
+        guard NetworkStatusManager.isConnectedToInternet() else {
+            self.warning = ("오프라인 상태입니다", "네트워크 연결을 확인 후 다시 시도하시기 바랍니다.")
+            return
+        }
+        
+        self.networkUsecase.getTags(order: .popularity) { [weak self] status, tags in
+            switch status {
+            case .SUCCESS:
+                self?.favoriteTags = Array(tags.prefix(10))
+            case .DECODEERROR:
+                self?.warning = ("올바르지 않는 형식", "최신 버전으로 업데이트 해주세요")
+            default:
+                self?.warning = ("네트워크 에러", "네트워크 연결을 확인 후 다시 시도하세요")
+            }
         }
     }
 }
