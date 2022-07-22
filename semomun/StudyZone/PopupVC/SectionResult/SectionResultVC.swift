b49bf72e50b25d7be636d746f36ffef168a83d22
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-22 12:18:25 +0900

    [Refactor] 결과창 로직 구현

--- semomun/StudyZone/PopupVC/SectionResult/SectionResultVC.swift
+++ semomun/StudyZone/PopupVC/SectionResult/SectionResultVC.swift
@@ -9,25 +9,24 @@ import UIKit
 import Combine
 
 final class SectionResultVC: UIViewController {
+    /* public */
     static let identifier = "SectionResultVC"
     static let storyboardName = "Study"
-    
-    @IBOutlet weak var frameView: UIView!
-    @IBOutlet weak var titleLabel: UILabel!
+    /* private */
+    @IBOutlet weak var workbookTitleLabel: UILabel!
+    @IBOutlet weak var sectionNumLabel: UILabel!
+    @IBOutlet weak var sectionTitleLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
-    @IBOutlet weak var progressView: CircularProgressView!
-    @IBOutlet weak var totalScoreLabel: UILabel!
-    @IBOutlet weak var totalTimeLabel: UILabel!
-    @IBOutlet weak var wrongProblems: UICollectionView!
-    @IBOutlet weak var wrongsHight: NSLayoutConstraint!
-    private var wrongs: [String] = []
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var problems: UICollectionView!
+    
     private var viewModel: SectionResultVM?
     private var cancellables: Set<AnyCancellable> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configureDataSource()
-        self.configureUI()
+        self.isModalInPresentation = true
+        self.configureCollectionView()
         self.bindAll()
         self.viewModel?.calculateResult()
     }
@@ -39,29 +38,50 @@ final class SectionResultVC: UIViewController {
     func configureViewModel(viewModel: SectionResultVM) {
         self.viewModel = viewModel
     }
-    
-    private func configureDataSource() {
-        self.wrongProblems.dataSource = self
+}
+
+extension SectionResultVC {
+    private func configureCollectionView() {
+        self.problems.dataSource = self
     }
     
-    private func configureUI() {
-        self.progressView.progressWidth = 30
-        self.progressView.trackColor = UIColor(.blueRegular) ?? .lightGray
-        self.progressView.progressColor = UIColor(.blueRegular) ?? .black
+    private func configureData(result: SectionResult) {
+        self.scoreLabel.text = "\(result.score.removeDecimalPoint)/\(result.perfectScore.removeDecimalPoint)"
+        self.timeLabel.text = result.time.toTimeString
     }
 }
 
 extension SectionResultVC {
     private func bindAll() {
-        self.bindTitle()
+        self.bindWorkbookTitle()
+        self.bindSectionNum()
+        self.bindSectionTitle()
         self.bindResult()
     }
     
-    private func bindTitle() {
+    private func bindWorkbookTitle() {
+        self.viewModel?.$workbookTitle
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] title in
+                self?.workbookTitleLabel.text = title
+            })
+            .store(in: &self.cancellables)
+    }
+    
+    private func bindSectionNum() {
+        self.viewModel?.$sectionNum
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] sectionNum in
+                self?.sectionNumLabel.text = String(format: "%02d", sectionNum)
+            })
+            .store(in: &self.cancellables)
+    }
+    
+    private func bindSectionTitle() {
         self.viewModel?.$sectionTitle
             .receive(on: DispatchQueue.main)
             .sink(receiveValue: { [weak self] title in
-                self?.titleLabel.text = title ?? ""
+                self?.sectionTitleLabel.text = title
             })
             .store(in: &self.cancellables)
     }
@@ -77,49 +97,16 @@ extension SectionResultVC {
     }
 }
 
-extension SectionResultVC {
-    private func configureData(result: SectionResult) {
-        self.scoreLabel.text = "\(result.score.removeDecimalPoint)점"
-        self.totalScoreLabel.text = "\(result.score.removeDecimalPoint) / \(result.perfectScore.removeDecimalPoint)점"
-        self.totalTimeLabel.text = result.time.toTimeString
-        self.configureWrongProblems(to: result.wrongProblems)
-        self.setProgress(total: result.perfectScore, to: result.score)
-    }
-    
-    private func configureWrongProblems(to problems: [String]) {
-        let cellHeight: CGFloat = 27
-        let verticalTerm: CGFloat = 5
-        self.wrongsHight.constant = cellHeight+(cellHeight+verticalTerm)*CGFloat(problems.count/6)
-        self.wrongs = problems
-        self.wrongProblems.reloadData()
-    }
-    
-    private func setProgress(total: Double, to: Double) {
-        let percent = Float(to/total)
-        self.progressView.setProgressWithAnimation(duration: 0.5, value: percent, from: 0)
-    }
-}
-
 extension SectionResultVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.wrongs.count == 0 ? 1 : self.wrongs.count
+        return self.viewModel?.problems.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WrongProblemCell.identifier, for: indexPath) as? WrongProblemCell else { return UICollectionViewCell() }
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProblemSelectCell.identifier, for: indexPath) as? ProblemSelectCell else { return UICollectionViewCell() }
+        guard let problem = self.viewModel?.problems[safe: indexPath.item] else { return cell }
+        cell.configure(problem: problem, isChecked: false)
         
-        if self.wrongs.count == 0 {
-            cell.configure(to: "없음")
-        } else {
-            cell.configure(to: self.wrongs[indexPath.item])
-        }
         return cell
     }
 }
-
-extension SectionResultVC: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 36, height: 27)
-    }
-}
-
