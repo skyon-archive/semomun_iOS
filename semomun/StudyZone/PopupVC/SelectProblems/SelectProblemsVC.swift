37983bbeb8e2beae58790070ff50810df9b968fb
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-22 13:51:04 +0900

    [Refactor] SelectProblemsVC PracticeTest 관련 로직 제거

--- semomun/StudyZone/PopupVC/SelectProblems/SelectProblemsVC.swift
+++ semomun/StudyZone/PopupVC/SelectProblems/SelectProblemsVC.swift
@@ -13,11 +13,6 @@ final class SelectProblemsVC: UIViewController {
     static let identifier = "SelectProblemsVC"
     static let storyboardName = "Study"
     /* private */
-    enum Mode {
-        case `default`, practiceTest
-    }
-    private var mode: Mode? // default, practiceTest
-    
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var workbookTitleLabel: UILabel!
     @IBOutlet weak var sectionNumLabel: UILabel!
@@ -28,7 +23,6 @@ final class SelectProblemsVC: UIViewController {
     @IBOutlet weak var scoringAllProblemsButton: UIButton!
     
     private var selectProblemsVM: SelectProblemsVM?
-    private var showSolvedProblemsVM: ShowSolvedProblemsVM?
     private var cancellables: Set<AnyCancellable> = []
     
     override func viewDidLoad() {
@@ -36,7 +30,6 @@ final class SelectProblemsVC: UIViewController {
         self.isModalInPresentation = true
         self.configureCollectionView()
         self.bindAll()
-        self.configurePracticeTestMode()
     }
     
     @IBAction func close(_ sender: Any) {
@@ -50,30 +43,20 @@ final class SelectProblemsVC: UIViewController {
     }
     
     @IBAction func scoringSelectedProblems(_ sender: Any) {
-        if self.mode == .default {
-            self.selectProblemsVM?.startSelectedScoring { [weak self] success in
-                guard success == true else { return }
-                self?.presentingViewController?.dismiss(animated: true, completion: {
-                    NotificationCenter.default.post(name: .showSectionResult, object: nil)
-                })
-            }
-        } else {
-            NotificationCenter.default.post(name: .sectionTerminated, object: nil)
-            self.presentingViewController?.dismiss(animated: true)
+        self.selectProblemsVM?.startSelectedScoring { [weak self] success in
+            guard success == true else { return }
+            self?.presentingViewController?.dismiss(animated: true, completion: {
+                NotificationCenter.default.post(name: .showSectionResult, object: nil)
+            })
         }
     }
     
     @IBAction func scoringAllProblems(_ sender: Any) {
-        if self.mode == .default {
-            self.selectProblemsVM?.startAllScoring { [weak self] success in
-                guard success == true else { return }
-                self?.presentingViewController?.dismiss(animated: true, completion: {
-                    NotificationCenter.default.post(name: .showSectionResult, object: nil)
-                })
-            }
-        } else {
-            NotificationCenter.default.post(name: .sectionTerminated, object: nil)
-            self.presentingViewController?.dismiss(animated: true)
+        self.selectProblemsVM?.startAllScoring { [weak self] success in
+            guard success == true else { return }
+            self?.presentingViewController?.dismiss(animated: true, completion: {
+                NotificationCenter.default.post(name: .showSectionResult, object: nil)
+            })
         }
     }
 }
@@ -82,12 +65,6 @@ final class SelectProblemsVC: UIViewController {
 extension SelectProblemsVC {
     func configureViewModel(viewModel: SelectProblemsVM) {
         self.selectProblemsVM = viewModel
-        self.mode = .default
-    }
-    
-    func configureViewModel(viewModel: ShowSolvedProblemsVM) {
-        self.showSolvedProblemsVM = viewModel
-        self.mode = .practiceTest
     }
 }
 
@@ -96,14 +73,6 @@ extension SelectProblemsVC {
     private func configureCollectionView() {
         self.problems.delegate = self
         self.problems.dataSource = self
-    }
-    
-    private func configurePracticeTestMode() {
-        if self.mode == .practiceTest {
-//            self.problemCountLabel.text = "푼 문제"
-//            self.allSelectIndicator.isHidden = true
-//            self.allSelectButton.isHidden = true
-        }
     }
 }
 
@@ -143,16 +112,9 @@ extension SelectProblemsVC {
                 self?.sectionTitleLabel.text = title
             })
             .store(in: &self.cancellables)
-        self.showSolvedProblemsVM?.$sectionTitle
-            .receive(on: DispatchQueue.main)
-            .sink(receiveValue: { [weak self] title in
-                self?.sectionTitleLabel.text = title
-            })
-            .store(in: &self.cancellables)
     }
     
     private func bindScoreingQueue() {
-        /// 채점할 문제 수
         self.selectProblemsVM?.$scoringQueue
             .receive(on: DispatchQueue.main)
             .sink(receiveValue: { [weak self] scoringQueue in
@@ -164,15 +126,6 @@ extension SelectProblemsVC {
                 }
                 
                 self?.problems.reloadData()
-            })
-            .store(in: &self.cancellables)
-        /// 푼 문제 수
-        self.showSolvedProblemsVM?.$scoringQueue
-            .receive(on: DispatchQueue.main)
-            .sink(receiveValue: { [weak self] scoringQueue in
-//                self?.checkingProblemsCountLabel.text = "\(scoringQueue.count) 문제"
-                self?.problems.reloadData()
-                self?.activeScoring()
             })
             .store(in: &self.cancellables)
     }
@@ -215,39 +168,23 @@ extension SelectProblemsVC {
 
 extension SelectProblemsVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch self.mode {
-        case .default: return self.selectProblemsVM?.problems.count ?? 0
-        case .practiceTest: return self.showSolvedProblemsVM?.problems.count ?? 0
-        default: return 0
-        }
+        return self.selectProblemsVM?.problems.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProblemSelectCell.identifier, for: indexPath) as? ProblemSelectCell else { return UICollectionViewCell() }
         
-        switch self.mode {
-        case .default:
-            guard let problem = self.selectProblemsVM?.problems[safe: indexPath.item] else { return cell }
-            guard let isChecked = self.selectProblemsVM?.isChecked(at: indexPath.item) else { return cell }
-            
-            cell.configure(problem: problem, isChecked: isChecked)
-            return cell
-        case .practiceTest:
-            guard let problem = self.showSolvedProblemsVM?.problems[safe: indexPath.item] else { return cell }
-            guard let isSolved = self.showSolvedProblemsVM?.isSolved(at: indexPath.item) else { return cell }
-            
-            cell.configure(problem: problem, isChecked: isSolved)
-            return cell
-        default:
-            return cell
-        }
+        guard let problem = self.selectProblemsVM?.problems[safe: indexPath.item] else { return cell }
+        guard let isChecked = self.selectProblemsVM?.isChecked(at: indexPath.item) else { return cell }
+        
+        cell.configure(problem: problem, isChecked: isChecked)
+        return cell
     }
 }
 
 
 extension SelectProblemsVC: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard self.mode == .default else { return }
         self.selectProblemsVM?.toggle(at: indexPath.item)
     }
 }
