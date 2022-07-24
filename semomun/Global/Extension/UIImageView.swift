895f64b855c59454660639e64adfa36672dee286
이성열 <type101315@gmail.com>
2022-07-24 21:42:01 +0900

    [Feat] ProblemTimerView 디자인 적용

--- semomun/Global/Extension/UIImageView.swift
+++ semomun/Global/Extension/UIImageView.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 extension UIImageView {
+    func setSVGTintColor(_ semomunColor: SemomunColor) {
+        self.image = self.image?.withRenderingMode(.alwaysTemplate)
+        self.tintColor = UIColor.getSemomunColor(semomunColor)
+    }
     func setSVGTintColor(to color: UIColor) {
         self.image = self.image?.withRenderingMode(.alwaysTemplate)
         self.tintColor = color
