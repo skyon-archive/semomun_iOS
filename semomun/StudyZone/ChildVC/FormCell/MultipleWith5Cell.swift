74f6c1b34a70503070ef3fcfe1e2dcc63d4d727c
이성열 <type101315@gmail.com>
2022-07-25 18:08:40 +0900

    [Refactor] 필요없는 코드 제거

--- semomun/StudyZone/ChildVC/FormCell/MultipleWith5Cell.swift
+++ semomun/StudyZone/ChildVC/FormCell/MultipleWith5Cell.swift
@@ -26,11 +26,6 @@ final class MultipleWith5Cell: FormCell, CellLayoutable {
         return imageView
     }()
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.delegate = delegate
-    }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
