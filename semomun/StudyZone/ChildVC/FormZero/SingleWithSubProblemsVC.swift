aafad22fe81791604d942bd72cd1cc494ff9c74a
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-26 17:37:23 +0900

    [Feat] subProblems keyboard 로직 수정

--- semomun/StudyZone/ChildVC/FormZero/SingleWithSubProblemsVC.swift
+++ semomun/StudyZone/ChildVC/FormZero/SingleWithSubProblemsVC.swift
@@ -16,6 +16,7 @@ final class SingleWithSubProblemsVC: FormZero {
     var viewModel: SingleWithSubProblemsVM?
     /* private */
     private let answerView = StudySubProblemsAnswerView()
+    private var isTextFieldEditing: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,6 +65,7 @@ final class SingleWithSubProblemsVC: FormZero {
     
     private func configureNotification() {
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillChangeFrame), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisappear), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
 
@@ -132,23 +134,17 @@ extension SingleWithSubProblemsVC: TimeRecordControllable {
     }
     
     @objc func keyboardWillChangeFrame(notification: Notification) {
+        guard self.isTextFieldEditing == false else { return }
+        self.isTextFieldEditing = true
         guard let userInfo = notification.userInfo,
               let frame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
             return
         }
-        
-        guard let terminated = self.viewModel?.problem?.terminated,
-              let problemCount = self.viewModel?.problem?.subProblemsCount else { return }
-        let wrongCount = self.answerView.wrongCount
-        
-        let bottomPoint = CGPoint(self.view.frame.maxX, self.view.frame.maxY)
-        let size = StudySubProblemsAnswerView.size(terminated: terminated, problemCount: Int(problemCount), wrongCount: wrongCount)
-        let defaultAnswerViewFrame = CGRect(origin: CGPoint(bottomPoint.x - 16 - size.width, bottomPoint.y - 16 - size.height), size: size)
-        
-        if defaultAnswerViewFrame.origin.y == self.answerView.frame.origin.y {
-            self.answerView.frame.origin.y -= frame.height
-        } else {
-            self.answerView.frame = defaultAnswerViewFrame
-        }
+        self.answerView.frame.origin.y -= frame.height
+    }
+    
+    @objc func keyboardWillDisappear() {
+        self.updateAnswerViewFrame()
+        self.isTextFieldEditing = false
     }
 }
