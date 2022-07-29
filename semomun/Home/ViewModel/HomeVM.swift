43450b0f63c53fd37191c44a5333f5ca78110393
FreeDeveloper97 <freedeveloper97@gmail.com>
2022-07-29 12:54:23 +0900

    [Feat] WorkbookDetailVM: getTags 로직 구현

--- semomun/Home/ViewModel/HomeVM.swift
+++ semomun/Home/ViewModel/HomeVM.swift
@@ -35,7 +35,7 @@ final class HomeVM {
         self.networkUsecase = networkUsecase
         self.configureObservation()
         self.popularCategoryContents = .init(
-            repeating: (CategoryOfDB(cid: 1, name: ""), []),
+            repeating: (CategoryOfDB(cid: 1, name: "", TagCount: nil), []),
             count: self.popularTagSectionCount
         )
     }
